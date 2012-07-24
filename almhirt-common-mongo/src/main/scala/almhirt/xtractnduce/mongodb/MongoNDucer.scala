b90553e60fa24c5478067912064743237e0ae45d
Christian Douven <douven@dial.de>
2012-07-24 14:48:57 +0200

    MongoNDucer did not respect keymapper

--- almhirt-common-mongo/src/main/scala/almhirt/xtractnduce/mongodb/MongoNDucer.scala
+++ almhirt-common-mongo/src/main/scala/almhirt/xtractnduce/mongodb/MongoNDucer.scala
@@ -26,45 +26,45 @@ object MongoNDucer {
   private def addToBuilder(scriptOp: NDuceScriptOp, builder: Builder[(String, Any), DBObject], mapKey: String => String): Unit =
     scriptOp match {
       case SetString(key, value) =>
-        builder += key -> value
+        builder += mapKey(key) -> value
       case SetStringOpt(key, value) =>
-        value foreach {v => builder += key -> v}
+        value foreach {v => builder += mapKey(key) -> v}
       case SetInt(key, value) =>
-        builder += key -> value
+        builder += mapKey(key) -> value
       case SetIntOpt(key, value) =>
-        value foreach {v => builder += key -> v}
+        value foreach {v => builder += mapKey(key) -> v}
       case SetLong(key, value) =>
-        builder += key -> value
+        builder += mapKey(key) -> value
       case SetLongOpt(key, value) =>
-        value foreach {v => builder += key -> v}
+        value foreach {v => builder += mapKey(key) -> v}
       case SetDouble(key, value) =>
-        builder += key -> value
+        builder += mapKey(key) -> value
       case SetDoubleOpt(key, value) =>
-        value foreach {v => builder += key -> v}
+        value foreach {v => builder += mapKey(key) -> v}
       case SetFloat(key, value) =>
-        builder += key -> value
+        builder += mapKey(key) -> value
       case SetFloatOpt(key, value) =>
-        value foreach {v => builder += key -> v}
+        value foreach {v => builder += mapKey(key) -> v}
       case SetBoolean(key, value) =>
-        builder += key -> value
+        builder += mapKey(key) -> value
       case SetBooleanOpt(key, value) =>
-        value foreach {v => builder += key -> v}
+        value foreach {v => builder += mapKey(key) -> v}
       case SetDecimal(key, value) =>
-        builder += key -> value
+        builder += mapKey(key) -> value
       case SetDecimalOpt(key, value) =>
-        value foreach {v => builder += key -> v}
+        value foreach {v => builder += mapKey(key) -> v}
       case SetDateTime(key, value) =>
-        builder += key -> value
+        builder += mapKey(key) -> value
       case SetDateTimeOpt(key, value) =>
-        value foreach {v => builder += key -> v}
+        value foreach {v => builder += mapKey(key) -> v}
       case SetBytes(key, value) =>
-        builder += key -> value
+        builder += mapKey(key) -> value
       case SetBytesOpt(key, value) =>
-        value foreach {v => builder += key -> v}
+        value foreach {v => builder += mapKey(key) -> v}
       case SetElement(key, value) =>
-        builder += key -> apply(value)
+        builder += mapKey(key) -> apply(value)
       case SetElementOpt(key, value) =>
-        value foreach {v => builder += key -> mongoObjectFromScript(v, identity)}
+        value foreach {v => builder += mapKey(key) -> mongoObjectFromScript(v, identity)}
       case SetElements(key, elements) =>
         val children = elements map {mongoObjectFromScript(_, identity)}
         builder += key -> MongoDBList(children: _*)
