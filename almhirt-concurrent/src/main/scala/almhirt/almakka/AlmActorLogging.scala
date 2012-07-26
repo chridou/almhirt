dd437d199a0692fab40792a43aad0e7154567f2d
Christian Douven <douven@dial.de>
2012-07-26 06:31:58 +0200

    AlmActorLogging respects MinSeverity which is Minor per default

--- almhirt-concurrent/src/main/scala/almhirt/almakka/AlmActorLogging.scala
+++ almhirt-concurrent/src/main/scala/almhirt/almakka/AlmActorLogging.scala
@@ -3,33 +3,33 @@ package almhirt.almakka
 import scalaz.{Success, Failure}
 import akka.event._
 import almhirt.validation._
-import almhirt.validation.Problem
 import almhirt.concurrent._
 
 trait AlmActorLogging { self: akka.actor.Actor =>
   val log = Logging(context.system, this)
   
-  private def logProblem(prob: Problem) {
-    prob.severity match {
-      case NoProblem =>
-        ()
-      case Minor =>
-        log.warning(prob.toString)
-      case Major =>
-        log.error(prob.toString)
-      case Critical =>
-        log.error(prob.toString)
-    }
+  private def logProblem(prob: Problem, minSeverity: Severity) {
+    if(prob.severity >= minSeverity)
+	  prob.severity match {
+	    case NoProblem =>
+	      ()
+	    case Minor =>
+	      log.warning(prob.toString)
+	    case Major =>
+	      log.error(prob.toString)
+	    case Critical =>
+	      log.error(prob.toString)
+	    }
   }
   
   implicit def almValidation2AlmValidationLoggingW[T](validation: AlmValidation[T]) = new AlmValidationLoggingW[T](validation)
   final class AlmValidationLoggingW[T](validation: AlmValidation[T]) {
-    def logFailure(): AlmValidation[T] = {
+    def logFailure(minSeverity: Severity = Minor): AlmValidation[T] = {
       validation match {
         case Success(_) => 
           validation
         case Failure(problem) =>
-          logProblem(problem)
+          logProblem(problem, minSeverity)
           validation
       }
     }
@@ -37,8 +37,8 @@ trait AlmActorLogging { self: akka.actor.Actor =>
   
   implicit def almFuture2AlmValidationLoggingW[T](future: AlmFuture[T]) = new AlmFutureLoggingW[T](future)
   final class AlmFutureLoggingW[T](future: AlmFuture[T]) {
-    def logFailure(): AlmFuture[T] = {
-       future.onFailure(logProblem(_))
+    def logFailure(minSeverity: Severity = Minor): AlmFuture[T] = {
+       future.onFailure(logProblem(_, Minor))
     }
   }
   
