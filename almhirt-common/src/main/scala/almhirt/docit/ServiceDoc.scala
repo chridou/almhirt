f43c7063c4a88eb36f35af4f3bcdfa0fde1297e8
Christian Douven <douven@dial.de>
2012-08-15 15:57:28 +0200

    service doc

---
+++ almhirt-common/src/main/scala/almhirt/docit/ServiceDoc.scala
@@ -0,0 +1,52 @@
+package almhirt.docit
+
+case class ServiceDoc(
+  name: String,
+  description: String,
+  resources: List[ResourceDoc])
+  
+case class ResourceDoc(
+  name: String,
+  parameter: Option[String],
+  requiresAuthentication: Boolean,
+  title: String,
+  description: String,
+  resources: List[ResourceDoc],
+  methods: List[MethodDoc])
+  
+sealed trait MethodDoc {
+  def description: String
+  def methodParams: List[MethodParamDoc]
+  def contentTypes: List[ContentTypeDoc]
+}
+
+case class GET(description: String, methodParams: List[MethodParamDoc], contentTypes: List[ContentTypeDoc]) extends MethodDoc
+case class PUT(description: String, methodParams: List[MethodParamDoc], contentTypes: List[ContentTypeDoc]) extends MethodDoc
+case class POST(description: String, methodParams: List[MethodParamDoc], contentTypes: List[ContentTypeDoc]) extends MethodDoc
+case class DELETE(description: String, methodParams: List[MethodParamDoc], contentTypes: List[ContentTypeDoc]) extends MethodDoc
+
+sealed trait MethodParamDoc
+
+sealed trait ContentTypeDoc{
+  def headerString: String
+  def docLink: Option[String]
+}
+
+case class AppXml() extends ContentTypeDoc {
+  val docLink = Some("""http://www.iana.org/assignments/media-types/application/index.html""")
+  val headerString = "application/xml"
+}
+case class AppJson() extends ContentTypeDoc {
+  val docLink = Some("""http://www.iana.org/assignments/media-types/application/index.html""")
+  val headerString = "application/json"
+}
+case class ImgJpeg() extends ContentTypeDoc {
+  val docLink = Some("""http://www.iana.org/assignments/media-types/image/index.html""")
+  val headerString = "image/jpeg"
+}
+case class ImgPng() extends ContentTypeDoc {
+  val docLink = Some("""http://www.iana.org/assignments/media-types/image/index.html""")
+  val headerString = "image/png"
+}
+
+case class CustomContentType(headerString: String, docLink: Option[String]) extends ContentTypeDoc
