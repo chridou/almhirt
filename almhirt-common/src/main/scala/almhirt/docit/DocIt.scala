27014a4b08ded073c26cc21082d31c7e6c550b07
chridou <chridou@github.com>
2012-08-16 22:48:43 +0200

    DocIt

--- almhirt-common/src/main/scala/almhirt/docit/DocIt.scala
+++ almhirt-common/src/main/scala/almhirt/docit/DocIt.scala
@@ -1,22 +1,17 @@
 package almhirt.docit
 
+import scala.xml.Node
 import scalaz._, Scalaz._
 
 case class DocItHtmlSettingss(css: Option[String] = None, styleClassMap: Map[String, String] = Map.empty)
 
-class DocItHtml private(tree: Tree[PathPartElement], settings: DocItHtmlSettingss) {
-  
-}
-
-object DocItHtml {
-  def apply(aDocTree: DocTree, css: Option[String] = None, styleClassMap: Map[String, String] = Map.empty): DocItHtml = {
+object DocIt {
+  def apply(aDocTree: DocTree): Tree[PathPartElement] = {
     def nodeFromDocTree(docTree: DocTree): Tree[PathPartElement] =
       if(docTree.children.isEmpty)
         docTree.payload.leaf
       else
         docTree.payload.node(docTree.children.map(nodeFromDocTree(_)): _*)      
-      
-    
-    new DocItHtml(nodeFromDocTree(aDocTree), DocItHtmlSettingss(css, styleClassMap))
+    nodeFromDocTree(aDocTree)
   }
 }
