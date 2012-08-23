1d6b8cf98210c0f6e4359850591f7e3f37be67a1
chridou <chridou@github.com>
2012-08-23 21:45:40 +0200

    extended canxtract type class

--- almhirt-common/src/main/scala/almhirt/xtractnduce/CanXtract.scala
+++ almhirt-common/src/main/scala/almhirt/xtractnduce/CanXtract.scala
@@ -1,7 +1,17 @@
 package almhirt.xtractnduce
 
+import scalaz.syntax.validation._
 import almhirt.validation._
+import almhirt.validation.AlmValidation._
+import almhirt.validation.Problem._
 
-trait CanXtract[T] {
+/** Use as a type class or mix into an object*/
+trait CanXTract[T] {
   def xtract(xtractor: XTractor): AlmValidationMBD[T]
+  def tryXtractFrom(xtractor: XTractor, key: String): AlmValidationMBD[Option[T]] =
+    xtractor.tryGetXTractor(key).toMBD.bind(opt => opt.map(x => xtract(x)).validationOut)
+  def xtractFrom(xtractor: XTractor, key: String): AlmValidationMBD[T] = 
+    tryXtractFrom(xtractor, key) bind {
+      case Some(x) => x.success 
+      case None => SingleBadDataProblem("Key not found.", key = key).toMBD.failure } 
 }
--- almhirt-common/src/main/scala/almhirt/xtractnduce/Scribbler.scala
+++ almhirt-common/src/main/scala/almhirt/xtractnduce/Scribbler.scala
@@ -1,5 +1,6 @@
 package almhirt.xtractnduce
 
+/** Use as a type class */
 trait Scribbler[A] {
   def scribble(a: A): NDuceScript
 }
