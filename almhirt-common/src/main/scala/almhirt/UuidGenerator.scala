ffc5149c77fcc998acfaf168abc9046f7f2c341d
Christian Douven <douven@dial.de>
2012-08-27 09:58:20 +0200

    UuidGenerator adde

--- almhirt-common/src/main/scala/almhirt/UuidGenerator.scala
+++ almhirt-common/src/main/scala/almhirt/UuidGenerator.scala
@@ -2,10 +2,15 @@ package almhirt
 
 import java.util.UUID
 
+/** Generates a [[java.util.UUID]] */
 trait UuidGenerator {
+  /** Generate a new [[java.util.UUID]] 
+   * @return A new [[java.util.UUID]]
+   */
   def generate: UUID
 }
 
+/** Generates a new [[java.util.UUID]] by using [[java.util.UUID]].randomUUID */
 class JavaUtilUuidGenerator extends UuidGenerator {
   def generate = UUID.randomUUID
 }
