3c5f47fcc26b62ca28138d7faf94885e4c56ac77
Christian Douven <douven@dial.de>
2012-08-29 13:02:51 +0200

    added generators for generic problems

--- almhirt-common/src/main/scala/almhirt/validation/ProblemFunctions.scala
+++ almhirt-common/src/main/scala/almhirt/validation/ProblemFunctions.scala
@@ -3,6 +3,10 @@ package almhirt.validation
 trait ProblemFunctions {
   def badData(key: String, message: String) =
     SingleBadDataProblem(message, key)
+  def systemProblem(message: String, severity: Severity = Major, exception: Option[Throwable] = None, args: Map[String, Any] = Map(), causes: List[Problem] = Nil) =
+    UnspecifiedProblem(message, severity, SystemProblem, exception, args)
+  def applicationProblem(message: String, severity: Severity = Major, exception: Option[Throwable] = None, args: Map[String, Any] = Map(), causes: List[Problem] = Nil) =
+    UnspecifiedProblem(message, severity, ApplicationProblem, exception, args)
 }
 
 object ProblemFunctions extends ProblemFunctions
