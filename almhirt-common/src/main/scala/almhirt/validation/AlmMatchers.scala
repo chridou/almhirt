9a525f9b55d4a98294de2bfe8875bd913ff6633c
chridou <chridou>
2012-09-03 21:21:05 +0200

    removedcout commented code

--- almhirt-common/src/main/scala/almhirt/validation/AlmMatchers.scala
+++ almhirt-common/src/main/scala/almhirt/validation/AlmMatchers.scala
@@ -8,16 +8,6 @@ sealed trait AlmMatcher[T]
 case class AlmSuccess[T](succ: T) extends AlmMatcher[T]
 case class AlmFailure[T](p: Problem) extends AlmMatcher[T]
 
-//object AlmSuccess {
-//  def unapply[R](validation: AlmValidation[R]): Option[R] = 
-//    validation fold (f => None, succ => Some(succ))
-//}
-
-//object AlmFailure {
-//  def unapply[P <: Problem, R](validation: Validation[P,R]): Option[P] = 
-//    validation fold (f => Some(f), succ => None)
-//}
-
 object AlmSeverity {
   def unapply[R](validation: AlmValidation[R]): Option[Severity] = 
     validation fold (f => Some(f.severity), succ => None)
