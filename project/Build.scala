98618b71ab9f7b518883392cb0fb0f8bf7150144
Christian Douven <douven@dial.de>
2012-09-05 07:19:13 +0200

    bumped to 0.0.3-SNAPSHOT

--- project/Build.scala
+++ project/Build.scala
@@ -3,7 +3,7 @@ import Keys._
 
 object BuildSettings {
   val buildOrganization = "org.almhirt"
-  val buildVersion      = "0.0.2-SNAPSHOT"
+  val buildVersion      = "0.0.3-SNAPSHOT"
   val buildScalaVersion = "2.9.2"
 
   val buildSettings = Defaults.defaultSettings ++ Seq (
