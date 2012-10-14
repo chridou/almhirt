c98fd8778b3c939d029017858c65091266676014
chridou <chridou@github.com>
2012-10-14 21:13:31 +0200

    Some comments

--- almhirt-common/src/main/scala/almhirt/messaging/MessageStream.scala
+++ almhirt-common/src/main/scala/almhirt/messaging/MessageStream.scala
@@ -20,15 +20,19 @@ import almhirt._
 /** Publishes messages to its subscribers. 
  * This is the weakest contract a channel must fulfill:
  * A Channel does
- * * guarantee that all message have been published by someone
+ * * guarantee that all message received by handlers have been published by someone
  * * not guarantee that messages arrive in the same order as they were published  
  * * not guarantee that all messages will be published
  * * not guarantee that all handlers will be called on the same thread
  * * not guarantee that handlers won't be called concurrently
  */
 trait MessageStream extends SubscribableForMessages with almhirt.MightBeRegisteredSomewhere with almhirt.Disposable {
+  /** The message stream might be limited to some topic
+   */
   def topicPattern: Option[String]
   
+  /** Ceases activity of this message stream and unregisteres it if it was registered somewhere
+   */
   def dispose() =
     registration.foreach(_.dispose())
 }
