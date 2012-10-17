4b8d627300ce1bb6cc91b6ead0042fd18c442de3
Christian Douven <douven@dial.de>
2012-10-17 10:49:59 +0200

    Added ActorBasedMessageHubSpecs

---
+++ almhirt-common/src/test/scala/almhirt/messaging/ActorBasedMessageHubSpecs.scala
@@ -0,0 +1,5 @@
+package almhirt.messaging
+
+class ActorBasedMessageHubSpecs {
+
+}
--- almhirt-common/src/test/scala/almhirt/messaging/ActorBasedTypedMessageChannelSpecs.scala
+++ almhirt-common/src/test/scala/almhirt/messaging/ActorBasedTypedMessageChannelSpecs.scala
@@ -120,6 +120,91 @@ class ActorBasedTypedMessageChannelSpecs extends Specification with AlmAkkaConte
         hitB === false
       }
     }
+    
+    """not trigger the handler on the parent for an A posted on the parent when the classifier is not met""" in {
+      inOwnContext { ctx =>
+        val channel = getChannel[A](ctx)
+        val subChannel = channel.createSubChannel[B].result(Duration.Inf).forceResult
+        var hitA = false
+        var hitB = false
+        val subscriptionA = (channel <-* (x => hitA = true, x => x.payload.propa == 0)).result(Duration.Inf).forceResult
+        val subscriptionB = (subChannel <-* (x => hitB = true)).result(Duration.Inf).forceResult
+        channel.post(Message(new A(1)))
+        subscriptionA.dispose()
+        subscriptionB.dispose()
+        hitA === false
+      }
+    }
+    """trigger the handler on the parent for an A posted on the parent when the classifier is met""" in {
+      inOwnContext { ctx =>
+        val channel = getChannel[A](ctx)
+        val subChannel = channel.createSubChannel[B].result(Duration.Inf).forceResult
+        var hitA = false
+        var hitB = false
+        val subscriptionA = (channel <-* (x => hitA = true, x => x.payload.propa == 1)).result(Duration.Inf).forceResult
+        val subscriptionB = (subChannel <-* (x => hitB = true)).result(Duration.Inf).forceResult
+        channel.post(Message(new A(1)))
+        subscriptionA.dispose()
+        subscriptionB.dispose()
+        hitA === true
+      }
+    }
+    """not trigger the handler on the parent or the subchannelfor an B posted on the parent when the classifier is not met""" in {
+      inOwnContext { ctx =>
+        val channel = getChannel[A](ctx)
+        val subChannel = channel.createSubChannel[B].result(Duration.Inf).forceResult
+        var hitA = false
+        var hitB = false
+        val subscriptionA = (channel <-* (x => hitA = true, x => x.payload.propa == 0)).result(Duration.Inf).forceResult
+        val subscriptionB = (subChannel <-* (x => hitB = true, x => x.payload.propb == "A")).result(Duration.Inf).forceResult
+        channel.post(Message(new B(1, "B")))
+        subscriptionA.dispose()
+        subscriptionB.dispose()
+        hitA === false && hitB === false
+      }
+    }
+    """trigger the handler on the parent and the subchannel for an B posted on the parent when the classifier is met""" in {
+      inOwnContext { ctx =>
+        val channel = getChannel[A](ctx)
+        val subChannel = channel.createSubChannel[B].result(Duration.Inf).forceResult
+        var hitA = false
+        var hitB = false
+        val subscriptionA = (channel <-* (x => hitA = true, x => x.payload.propa == 1)).result(Duration.Inf).forceResult
+        val subscriptionB = (subChannel <-* (x => hitB = true, x => x.payload.propb == "B")).result(Duration.Inf).forceResult
+        channel.post(Message(new B(1, "B")))
+        subscriptionA.dispose()
+        subscriptionB.dispose()
+        hitA === true && hitB === true
+      }
+    }
+    """trigger the handler on the parent and not on the subchannel for an B posted on the parent when the classifier is met only in the parent""" in {
+      inOwnContext { ctx =>
+        val channel = getChannel[A](ctx)
+        val subChannel = channel.createSubChannel[B].result(Duration.Inf).forceResult
+        var hitA = false
+        var hitB = false
+        val subscriptionA = (channel <-* (x => hitA = true, x => x.payload.propa == 1)).result(Duration.Inf).forceResult
+        val subscriptionB = (subChannel <-* (x => hitB = true, x => x.payload.propb == "A")).result(Duration.Inf).forceResult
+        channel.post(Message(new B(1, "B")))
+        subscriptionA.dispose()
+        subscriptionB.dispose()
+        hitA === true && hitB === false
+      }
+    }
+    """not trigger the handler on the parent but on the subchannel for an B posted on the subchannel when the classifier is met only in the subchannel""" in {
+      inOwnContext { ctx =>
+        val channel = getChannel[A](ctx)
+        val subChannel = channel.createSubChannel[B].result(Duration.Inf).forceResult
+        var hitA = false
+        var hitB = false
+        val subscriptionA = (channel <-* (x => hitA = true, x => x.payload.propa == 0)).result(Duration.Inf).forceResult
+        val subscriptionB = (subChannel <-* (x => hitB = true, x => x.payload.propb == "B")).result(Duration.Inf).forceResult
+        channel.post(Message(new B(1, "B")))
+        subscriptionA.dispose()
+        subscriptionB.dispose()
+        hitA === false && hitB === true
+      }
+    }
   }
   
 }
