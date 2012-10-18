0382be0d89deaf089fb4342ebdc485620f25ab0e
Christian Douven <douven@dial.de>
2012-10-18 12:28:57 +0200

    renamed to hasrepositories"

--- almhirt-core/src/main/scala/almhirt/domain/HasRepositories.scala
+++ almhirt-core/src/main/scala/almhirt/domain/HasRepositories.scala
@@ -2,6 +2,7 @@ package almhirt.domain
 
 import almhirt._
 
-trait RegistersRepositories {
+trait HasRepositories {
   def get[T <: AggregateRootRepository[_,_]](implicit m: Manifest[T]): AlmValidation[T]
+  def register[T <: AggregateRootRepository[_,_]](repo: T)(implicit m: Manifest[T]): Unit
 }
