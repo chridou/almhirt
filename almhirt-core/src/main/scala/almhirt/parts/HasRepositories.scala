b050ea2ba21b3d63994c739d84aac2f81695d32d
Christian Douven <douven@dial.de>
2012-10-18 13:20:06 +0200

    Implemented UnsafeRepositoryRegistry

--- almhirt-core/src/main/scala/almhirt/parts/HasRepositories.scala
+++ almhirt-core/src/main/scala/almhirt/parts/HasRepositories.scala
@@ -5,5 +5,7 @@ import almhirt.domain.AggregateRootRepository
 
 trait HasRepositories {
   def get[T <: AggregateRootRepository[_,_]](implicit m: Manifest[T]): AlmValidation[T]
+  /** Registers a new repository. Has replace semantics. 
+   */
   def register[T <: AggregateRootRepository[_,_]](repo: T)(implicit m: Manifest[T]): Unit
 }
---
+++ almhirt-core/src/main/scala/almhirt/parts/impl/UnsafeRepositoryRegistry.scala
@@ -0,0 +1,22 @@
+package almhirt.parts.impl
+
+import scalaz.syntax.validation._
+import almhirt._
+import almhirt.domain.AggregateRootRepository
+import almhirt.parts.HasRepositories
+
+/** A repository registry that is __NOT__ thread safe. Do not mutate, once almhirt is running!
+ */
+class UnsafeRepositoryRegistry extends HasRepositories {
+  private val repos = scala.collection.mutable.Map[String, AnyRef]()
+  
+  def get[T <: AggregateRootRepository[_,_]](implicit m: Manifest[T]): AlmValidation[T] = {
+    repos.get(m.erasure.getName) match {
+      case Some(r) => r.asInstanceOf[T].success
+      case None => NotFoundProblem("Repository of type '%s' not found".format(m.erasure.getName)).failure
+    }
+  }
+  def register[T <: AggregateRootRepository[_,_]](repo: T)(implicit m: Manifest[T]) {
+    repos + ((m.erasure.getName, repo))
+  }
+}
