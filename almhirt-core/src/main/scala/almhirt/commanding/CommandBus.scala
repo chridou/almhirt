88317460de93c750236f7152bf1c4a82cda5e4ce
Christian Douven <douven@dial.de>
2012-10-25 07:34:09 +0200

    removed command bus

--- almhirt-core/src/main/scala/almhirt/commanding/CommandBus.scala
+++
@@ -1,5 +0,0 @@
-package almhirt.commanding
-
-trait CommandBus {
-
-}
--- almhirt-core/src/main/scala/almhirt/commanding/ExecutesCommands.scala
+++ almhirt-core/src/main/scala/almhirt/commanding/ExecutesCommands.scala
@@ -2,4 +2,5 @@ package almhirt.commanding
 
 trait ExecutesCommands {
   def executeCommand(com: DomainCommand, ticket: Option[String]): Unit
+  def executeCommand(commandEnvelope: CommandEnvelope) { executeCommand(commandEnvelope.command, commandEnvelope.ticket) }
 }
