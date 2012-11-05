cb3908eae17ec2fed324342bd7208a7412092231
Christian Douven <douven@dial.de>
2012-11-05 09:18:51 +0100

    refactored error handling in almfuture

--- almhirt-common/src/main/scala/almhirt/AlmFuture.scala
+++ almhirt-common/src/main/scala/almhirt/AlmFuture.scala
@@ -15,121 +15,113 @@
 package almhirt
 
 import java.util.concurrent.TimeoutException
-import scala.{Left, Right}
-import scalaz.{Validation, Success, Failure}
+import scala.{ Left, Right }
+import scala.collection.generic.CanBuildFrom
+import scala.collection.mutable.Builder
+import scalaz.{ Validation, Success, Failure }
 import scalaz.syntax.validation._
-import akka.dispatch.{Future, Promise, Await, ExecutionContext}
+import akka.dispatch.{ Future, Promise, Await, ExecutionContext }
 import almhirt._
 import akka.util.Duration
-import scala.collection.generic.CanBuildFrom
 
-/** A future based on [[akka.dispatch.Future]].
- * 
+/**
+ * A future based on [[akka.dispatch.Future]].
+ *
  * The intention is to have a future that doesn't rely on the Either type where Left[Throwable] identifies an error.
- * Instead a result should always be in a [[almhirt.validation.AlmValidation]] which is in fact a [[scalaz.Validation]] 
+ * Instead a result should always be in a [[almhirt.validation.AlmValidation]] which is in fact a [[scalaz.Validation]]
  * based on [[almhirt.validation.Problem]] as the error type
- * 
+ *
  * Errors which would end in a Throwable end in a SystemProblem whereas a TimeoutException ends in a TimeoutProblem.
  */
-class AlmFuture[+R](val underlying: Future[AlmValidation[R]])(implicit executionContext: akka.dispatch.ExecutionContext)  {
+class AlmFuture[+R](val underlying: Future[AlmValidation[R]])(implicit executionContext: akka.dispatch.ExecutionContext) {
   import almfuture.all._
   def map[T](compute: R => T): AlmFuture[T] =
     new AlmFuture[T](underlying map { validation => validation map compute })
-    
+
   def flatMap[T](compute: R => AlmFuture[T]): AlmFuture[T] =
-    new AlmFuture(underlying flatMap { validation => 
+    new AlmFuture(underlying flatMap { validation =>
       validation fold (
         f => Promise.successful(f.failure[T]),
-        r => compute(r).underlying) } )
+        r => compute(r).underlying)
+    })
 
   def mapV[T](compute: R => AlmValidation[T]): AlmFuture[T] =
     new AlmFuture[T](underlying map { validation => validation bind compute })
-  
+
   def fold[T](failure: Problem => T = identity[Problem] _, success: R => T = identity[R] _): Future[T] =
-    underlying map { validation => validation fold (failure, success)}
-  
+    underlying map { validation => validation fold (failure, success) }
+
   def onComplete(handler: AlmValidation[R] => Unit): AlmFuture[R] = {
-    underlying onComplete({
+    underlying onComplete ({
       case Right(validation) => handler(validation)
-      case Left(err) => {
-        val prob = err match {
-          case tout: TimeoutException => OperationTimedOutProblem("A future operation timed out.", cause = Some(CauseIsThrowable(tout))) 
-          case exn => UnspecifiedProblem(message = exn.getMessage, severity = Major, category = SystemProblem, cause = Some(CauseIsThrowable(exn))) 
-        }
-        handler(prob.failure[R])
-      }
+      case Left(err) => handler(throwableToProblem(err).failure)
     })
   }
 
   def onComplete(fail: Problem => Unit, succ: R => Unit): AlmFuture[R] = {
-    underlying onComplete({
-      case Right(validation) => validation fold(fail, succ)
-      case Left(err) => {
-        val prob = err match {
-          case tout: TimeoutException => OperationTimedOutProblem("A future operation timed out.", cause = Some(CauseIsThrowable(tout))) 
-          case exn => UnspecifiedProblem(message = exn.getMessage, severity = Major, category = SystemProblem, cause = Some(CauseIsThrowable(exn))) 
-        }
-        fail(prob)
-      }
+    underlying onComplete ({
+      case Right(validation) => validation fold (fail, succ)
+      case Left(err) => fail(throwableToProblem(err))
     })
   }
 
   def sideEffect(fail: Problem => Unit, succ: R => Unit): Unit = {
-    underlying onComplete({
-      case Right(validation) => validation fold(fail, succ)
-      case Left(err) => {
-        val prob = err match {
-          case tout: TimeoutException => OperationTimedOutProblem("A future operation timed out.", cause = Some(CauseIsThrowable(tout))) 
-          case exn => UnspecifiedProblem(message = exn.getMessage, severity = Major, category = SystemProblem, cause = Some(CauseIsThrowable(exn))) 
-        }
-        fail(prob)
-      }
+    underlying onComplete ({
+      case Right(validation) => validation fold (fail, succ)
+      case Left(err) => fail(throwableToProblem(err))
     })
   }
-  
-  def onSuccess(onRes: R => Unit): AlmFuture[R] = 
-    underlying onSuccess({
-      case x => x fold(_ => (), onRes(_))
+
+  def onSuccess(onRes: R => Unit): AlmFuture[R] =
+    underlying onSuccess ({
+      case x => x fold (_ => (), onRes(_))
     })
 
-  def onFailure(onProb: Problem => Unit): AlmFuture[R] = 
-    onComplete(_ fold(onProb(_), _ => ()))
- 
+  def onFailure(onProb: Problem => Unit): AlmFuture[R] =
+    onComplete(_ fold (onProb(_), _ => ()))
+
   def andThen(effect: AlmValidation[R] => Unit): AlmFuture[R] = {
-    new AlmFuture(underlying andThen{
+    new AlmFuture(underlying andThen {
       case Right(r) => effect(r)
-      case Left(err) => effect(UnspecifiedProblem(err.getMessage, severity = Major, category = SystemProblem, cause = Some(CauseIsThrowable(err))).failure[R])
+      case Left(err) => effect(throwableToProblem(err).failure)
     })
   }
-  
+
   def isCompleted = underlying.isCompleted
-  
+
   @deprecated("Use awaitResult", "0.0.1")
   def result(implicit atMost: Duration): AlmValidation[R] = awaitResult
 
-  def awaitResult(implicit atMost: Duration): AlmValidation[R] = 
+  def awaitResult(implicit atMost: Duration): AlmValidation[R] =
     try {
       Await.result(underlying, atMost)
     } catch {
-      case tout: TimeoutException => OperationTimedOutProblem("A future operation timed out.", cause = Some(CauseIsThrowable(tout))).failure[R] 
-      case exn => UnspecifiedProblem(exn.getMessage, severity = Major, category = SystemProblem, cause = Some(CauseIsThrowable(exn))).failure[R] 
+      case exn => throwableToProblem(exn).failure
     }
 
+  private def throwableToProblem(throwable: Throwable): Problem =
+    throwable match {
+      case tout: TimeoutException => OperationTimedOutProblem("A future operation timed out: %s".format(tout.getMessage), cause = Some(CauseIsThrowable(tout)))
+      case exn => UnspecifiedProblem(exn.getMessage, severity = Major, category = SystemProblem, cause = Some(CauseIsThrowable(exn)))
+    }
 }
 
 object AlmFuture {
-  def apply[T](compute: => AlmValidation[T])(implicit executor: akka.dispatch.ExecutionContext) = new AlmFuture[T](Future{compute})
+  def apply[T](compute: => AlmValidation[T])(implicit executor: akka.dispatch.ExecutionContext) = new AlmFuture[T](Future { compute })
   @deprecated("Use AlmPromise.apply", "0.0.1")
-  def promise[T](compute: => AlmValidation[T])(implicit executor: akka.dispatch.ExecutionContext) = new AlmFuture[T](Promise.successful{compute})
+  def promise[T](compute: => AlmValidation[T])(implicit executor: akka.dispatch.ExecutionContext) = new AlmFuture[T](Promise.successful { compute })
   @deprecated("Use apply", "0.0.1")
   def future[T](compute: => AlmValidation[T])(implicit executor: akka.dispatch.ExecutionContext) = apply(compute)(executor)
 
-//  def sequence[A, M[_] <: Traversable[_]](in: M[AlmFuture[A]])(implicit cbf: CanBuildFrom[M[AlmFuture[A]], A, M[A]], executor: ExecutionContext): AlmFuture[M[A]] =
-//    in.foldLeft(Promise.successful(cbf(in)): Future[Builder[A, M[A]]])((fr, fa) ⇒ for (r ← fr; a ← fa.asInstanceOf[Future[A]]) yield (r += a)).map(_.result)
-    
+//  def sequence[A, M[_] <: Traversable[_]](in: M[AlmFuture[A]])(implicit cbf: CanBuildFrom[M[AlmFuture[A]], A, M[A]], executor: ExecutionContext): AlmFuture[M[A]] = {
+    //in.foldLeft(Promise.successful(cbf(in)): Future[Builder[A, M[A]]])((fr, fa) ⇒ for (r ← fr; a ← fa.asInstanceOf[Future[A]]) yield (r += a)).map(_.result)
+//    val x = in.foldLeft(Promise.successful(cbf(in)): Future[Builder[A, M[A]]])((fr, fa) ⇒ for (r ← fr; a ← fa.asInstanceOf[Future[A]]) yield (r += a)).map(_.result)
+//    val fut = in.foldLeft(Promise.successful(cbf(in)): Future[Builder[A, M[A]]])((facc, felem) => sys.error(""))
+//    sys.error("")
+//  }
 }
 
-object AlmPromise{
-  def apply[T](compute: => AlmValidation[T])(implicit executor: akka.dispatch.ExecutionContext) = new AlmFuture[T](Promise.successful{compute})
-  
+object AlmPromise {
+  def apply[T](compute: => AlmValidation[T])(implicit executor: akka.dispatch.ExecutionContext) = new AlmFuture[T](Promise.successful { compute })
+
 }
