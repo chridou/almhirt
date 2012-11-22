231fe562c6d6b2ee9d32ad583bdf807f998cf93e
chridou <chridou@github.com>
2012-11-22 10:59:50 +0100

    added a maximum call duration for direct method calls instead of using the actor

--- almhirt-core/src/main/scala/almhirt/eventlog/impl/DomainEventLogActorHull.scala
+++ almhirt-core/src/main/scala/almhirt/eventlog/impl/DomainEventLogActorHull.scala
@@ -12,19 +12,19 @@ import almhirt.domain._
 import almhirt.eventlog._
 import akka.util.Duration
 
-class DomainEventLogActorHull(val actor: ActorRef, onClose: () => Unit)(implicit almhirtContext: AlmhirtContext) extends DomainEventLog {
-  private implicit def atMost = Duration(120, "seconds")
+class DomainEventLogActorHull(val actor: ActorRef, onClose: () => Unit, maximumDirectCallDuration: Duration)(implicit almhirtContext: AlmhirtContext) extends DomainEventLog {
   private implicit def executionContext = almhirtContext.system.futureDispatcher
 
-  def this(actor: ActorRef)(implicit almhirtContext: AlmhirtContext) = this(actor, () => ())
+  def this(actor: ActorRef, maximumDirectCallDuration: Duration)(implicit almhirtContext: AlmhirtContext) = this(actor, () => (), maximumDirectCallDuration)
+  def this(actor: ActorRef)(implicit almhirtContext: AlmhirtContext) = this(actor, Duration(5, "seconds"))
   
-  def storeEvents(events: List[DomainEvent]) = (actor ? LogEventsQry(events, None))(atMost).mapTo[CommittedDomainEventsRsp].map(_.events)
+  def storeEvents(events: List[DomainEvent]) = (actor ? LogEventsQry(events, None))(maximumDirectCallDuration).mapTo[CommittedDomainEventsRsp].map(_.events)
   def purgeEvents(aggRootId: java.util.UUID) = AlmPromise { Nil.success }
 
-  def getAllEvents() = (actor ? GetAllEventsQry)(atMost).mapTo[AllEventsRsp].map(x => x.chunk.events)
-  def getEvents(id: UUID) = (actor ? GetEventsQry(id))(atMost).mapTo[EventsForAggregateRootRsp].map(x => x.chunk.events)
-  def getEvents(id: UUID, fromVersion: Long) = (actor ? GetEventsFromQry(id, fromVersion))(atMost).mapTo[EventsForAggregateRootRsp].map(x => x.chunk.events)
-  def getEvents(id: UUID, fromVersion: Long, toVersion: Long) = (actor ? GetEventsFromToQry(id, fromVersion, toVersion))(atMost).mapTo[EventsForAggregateRootRsp].map(x => x.chunk.events)
-  def getRequiredNextEventVersion(id: UUID): AlmFuture[Long] = (actor ? GetRequiredNextEventVersionQry(id))(atMost).mapTo[RequiredNextEventVersionRsp].map(x => x.nextVersion)
+  def getAllEvents() = (actor ? GetAllEventsQry)(maximumDirectCallDuration).mapTo[AllEventsRsp].map(x => x.chunk.events)
+  def getEvents(id: UUID) = (actor ? GetEventsQry(id))(maximumDirectCallDuration).mapTo[EventsForAggregateRootRsp].map(x => x.chunk.events)
+  def getEvents(id: UUID, fromVersion: Long) = (actor ? GetEventsFromQry(id, fromVersion))(maximumDirectCallDuration).mapTo[EventsForAggregateRootRsp].map(x => x.chunk.events)
+  def getEvents(id: UUID, fromVersion: Long, toVersion: Long) = (actor ? GetEventsFromToQry(id, fromVersion, toVersion))(maximumDirectCallDuration).mapTo[EventsForAggregateRootRsp].map(x => x.chunk.events)
+  def getRequiredNextEventVersion(id: UUID): AlmFuture[Long] = (actor ? GetRequiredNextEventVersionQry(id))(maximumDirectCallDuration).mapTo[RequiredNextEventVersionRsp].map(x => x.nextVersion)
   def close() { onClose() }
 }
