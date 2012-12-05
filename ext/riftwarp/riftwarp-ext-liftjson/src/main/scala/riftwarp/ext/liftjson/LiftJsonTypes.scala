85c2248f549575447a9ee564394b58621498e12b
chridou <chridou@github.com>
2012-12-05 21:49:57 +0100

    added converter to json lib

--- ext/riftwarp/riftwarp-ext-liftjson/src/main/scala/riftwarp/ext/liftjson/LiftJsonTypes.scala
+++ ext/riftwarp/riftwarp-ext-liftjson/src/main/scala/riftwarp/ext/liftjson/LiftJsonTypes.scala
@@ -5,7 +5,6 @@ import net.liftweb.json._
 
 case class DimensionLiftJsonAst(manifestation: JValue) extends RiftDimension
 
-
 class ToolGroupLiftJson extends ToolGroup { val name = "tool_liftjson" }
 object ToolGroupLiftJson {
   private val theInstance = new ToolGroupLiftJson()
@@ -15,3 +14,39 @@ object ToolGroupLiftJson {
 object LiftJson {
   val toolGroup = ToolGroupLiftJson()
 }
+
+import scalaz.syntax.validation._
+import almhirt.common._
+
+object DimensionLiftJsonAstToString extends DimensionConverter[DimensionLiftJsonAst, DimensionString] {
+  import scalaz.Cord
+  val tSource = classOf[DimensionLiftJsonAst]
+  val tTarget = classOf[DimensionString]
+  def convert(source: DimensionLiftJsonAst): AlmValidation[DimensionString] =
+    DimensionString(compact(render(source.manifestation))).success
+}
+
+object DimensionLiftJsonAstToNiceString extends DimensionConverter[DimensionLiftJsonAst, DimensionNiceString] {
+  import scalaz.Cord
+  val tSource = classOf[DimensionLiftJsonAst]
+  val tTarget = classOf[DimensionNiceString]
+  def convert(source: DimensionLiftJsonAst): AlmValidation[DimensionNiceString] =
+    DimensionNiceString(pretty(render(source.manifestation))).success
+}
+
+object DimensionLiftJsonAstToCord extends DimensionConverter[DimensionLiftJsonAst, DimensionCord] {
+  import scalaz.Cord
+  val tSource = classOf[DimensionLiftJsonAst]
+  val tTarget = classOf[DimensionCord]
+  def convert(source: DimensionLiftJsonAst): AlmValidation[DimensionCord] =
+    DimensionCord(compact(render(source.manifestation))).success
+}
+
+object DimensionLiftJsonAstToNiceCord extends DimensionConverter[DimensionLiftJsonAst, DimensionNiceCord] {
+  import scalaz.Cord
+  val tSource = classOf[DimensionLiftJsonAst]
+  val tTarget = classOf[DimensionNiceCord]
+  def convert(source: DimensionLiftJsonAst): AlmValidation[DimensionNiceCord] =
+    DimensionNiceCord(pretty(render(source.manifestation))).success
+}
+
