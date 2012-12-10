8de7b05eb8c6e27546b161e37b896f87bc7eead5
chridou <chridou@github.com>
2012-12-10 21:14:59 +0100

    schema files

--- ext/eventlogs/almhirt-ext-anormeventlog/src/test/resources/conf/h2jsonddl_blob.sql
+++ ext/eventlogs/almhirt-ext-anormeventlog/src/test/resources/conf/h2jsonddl_blob.sql
@@ -1,11 +1,3 @@
-CREATE TABLE IF NOT EXISTS %tblname%
-(	internalId BIGINT IDENTITY,
-    id UUID UNIQUE,
-	aggId UUID NOT NULL,
-	aggVersion BIGINT NOT NULL,
-	channel VARCHAR(16) NOT NULL,
-	timestamp TIMESTAMP NOT NULL,
-	payload CLOB NOT NULL);
-	
-CREATE INDEX IF NOT EXISTS idx_aggId ON %tblname%(aggId);
-CREATE INDEX IF NOT EXISTS idx_aggIdAggVersion ON %tblname%(aggId, aggVersion);
+CREATE TABLE IF NOT EXISTS %tblname%_blobs
+(	id UUID PRIMARY KEY,
+	data BLOB NOT NULL);
