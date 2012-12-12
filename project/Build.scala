2dc75bb85f4585ac822ac93b14582584c54ca1ae
Christian Douven <douven@dial.de>
2012-12-12 09:22:03 +0100

    version 0.0.24

--- project/Build.scala
+++ project/Build.scala
@@ -3,7 +3,7 @@ import Keys._
 
 object BuildSettings {
   val buildOrganization = "org.almhirt"
-  val buildVersion      = "0.0.23-SNAPSHOT"
+  val buildVersion      = "0.0.24-SNAPSHOT"
   val buildScalaVersion = "2.9.2"
 
   val buildSettings = Defaults.defaultSettings ++ Seq (
