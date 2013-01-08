009be73bda7d9b9e6a154be1d4457e525e00dd0f
Christian Douven <douven@dial.de>
2013-01-08 17:24:01 +0100

    fixed commandhandlers

--- almhirt-core/src/test/scala/test/TestPersonCommands.scala
+++ almhirt-core/src/test/scala/test/TestPersonCommands.scala
@@ -23,7 +23,7 @@ abstract class TestPersonCreatorUnitOfWork[TCom <: TestPersonCommand](implicit a
 abstract class TestPersonMutatorUnitOfWork[TCom <: TestPersonCommand](implicit almhirt: Almhirt) extends TestPersonUnitOfWork[TCom] with MutatorUnitOfWorkStyleFuture[TestPerson, TestPersonEvent, TCom]
 
 object TestPersonHandlerFactory {
-  def newTestPersonUnitOfWork(implicit theAlmhirt: Almhirt) = BoundUnitOfWork.createCreatorStyleFuture[TestPerson, TestPersonEvent, NewTestPerson]((cmd: NewTestPerson, passedAlmhirt: Almhirt) => AlmFuture { TestPerson(cmd.id, cmd.name).recordings }(theAlmhirt))
+  def newTestPersonUnitOfWork(implicit theAlmhirt: Almhirt) = BoundUnitOfWork.createCreatorStyleFuture[TestPerson, TestPersonEvent, NewTestPerson]((cmd: NewTestPerson, passedAlmhirt: Almhirt) => AlmFuture { TestPerson(cmd.id, cmd.name).recordings }(passedAlmhirt))
   def changeTestPersonNameUnitOfWork(implicit theAlmhirt: Almhirt) = BoundUnitOfWork.createMutatorStyleFuture[TestPerson, TestPersonEvent, ChangeTestPersonName]((cmd: ChangeTestPersonName, person: TestPerson, passedAlmhirt: Almhirt) => AlmFuture { person.changeName(cmd.newName).recordings }(passedAlmhirt))
   def setTestPersonAdressUnitOfWork(implicit theAlmhirt: Almhirt) = BoundUnitOfWork.createMutatorStyleFuture[TestPerson, TestPersonEvent, SetTestPersonAddress]((cmd: SetTestPersonAddress, person: TestPerson, passedAlmhirt: Almhirt) => AlmFuture { person.addressAquired(cmd.aquiredAddress).recordings }(passedAlmhirt))
   def moveTestPersonNameUnitOfWork(implicit theAlmhirt: Almhirt) = BoundUnitOfWork.createMutatorStyleFuture[TestPerson, TestPersonEvent, MoveTestPerson]((cmd: MoveTestPerson, person: TestPerson, passedAlmhirt: Almhirt) => AlmFuture { person.move(cmd.newAddress).recordings }(theAlmhirt))
