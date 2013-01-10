0fa2337935a8ee3f25fd029bbef6b7a89305aae2
chridou <at.at@at.at>
2013-01-10 18:57:01 +0100

    problems can be tagged with a string

--- almhirt-common/src/main/scala/almhirt/problem/ProblemOps.scala
+++ almhirt-common/src/main/scala/almhirt/problem/ProblemOps.scala
@@ -36,7 +36,7 @@ trait ProblemOps0 extends Ops[NonEmptyList[Problem]] {
 }
 
 trait ProblemOps1[T <: Problem] extends Ops[T] {
-  
+
   def toAggregate: AggregateProblem =
     AggregateProblem(self.message, severity = self.severity, category = self.category, problems = self :: Nil)
 }
@@ -50,10 +50,10 @@ trait ProblemOps2[T <: Problem] extends Ops[T] {
 
   def markLogged(): T = self.withArg("isLogged", true).asInstanceOf[T]
   def isLogged(): Boolean = self.args.contains("isLogged") && self.args("isLogged") == true
-  
+
   def setTag(tag: String): T = self.withArg("tag", tag).asInstanceOf[T]
   def isTagged(): Boolean = self.args.contains("tag") && self.args("tag").isInstanceOf[String]
-  def tryGetTag(): Option[String] = if(isTagged) Some(self.args("tag").asInstanceOf[String]) else None
+  def tryGetTag(): Option[String] = if (isTagged) Some(self.args("tag").asInstanceOf[String]) else None
 }
 
 trait ToProblemOps {
