03984adf98131dc69bd4abb8fd9fff83149e8d8c
Christian Douven <at.at@at.at>
2013-01-18 13:18:52 +0100

    macros

--- ext/riftwarp/riftwarp-automatic/src/main/scala/riftwarp/automatic/GeneratedDecomposer.scala
+++ ext/riftwarp/riftwarp-automatic/src/main/scala/riftwarp/automatic/GeneratedDecomposer.scala
@@ -1,18 +1,16 @@
 package riftwarp.automatic
 
-import scala.language.experimental.macros
 
 import almhirt.common._
 import riftwarp._
 
 trait GeneratedDecomposer[TToDecompose <: AnyRef] extends Decomposer[TToDecompose] {
   def addTypeDescriptor: Boolean
-  def decompose[TDimension <: RiftDimension](what: TToDecompose)(into: Dematerializer[TDimension]): AlmValidation[Dematerializer[TDimension]] = 
+  def decompose[TDimension <: RiftDimension](what: TToDecompose)(into: Dematerializer[TDimension]): AlmValidation[Dematerializer[TDimension]] = { 
+    def decomposeUncurried(what: TToDecompose, into: Dematerializer[TDimension]) = GeneratedDecomposerImpl.decomposeUncurried[TToDecompose, TDimension](what, into)
     if(addTypeDescriptor)
-      into.addTypeDescriptor(this.typeDescriptor).flatMap(decomposeUncurried(what, _))
+      into.addTypeDescriptor(this.typeDescriptor).flatMap(demat => decomposeUncurried(what, demat))
     else
-      decomposeUncurried[TToDecompose, TDimension](what, into)
-      
-  private def decomposeUncurried[TTWhat <: AnyRef, TTDimension <: RiftDimension](what: TTWhat, into: Dematerializer[TTDimension]): AlmValidation[Dematerializer[TTDimension]] = 
-    macro GeneratedDecomposerImpl.decomposeUncurried[TTWhat, TTDimension]
+      decomposeUncurried(what, into)
+  }
 }
--- riftwarp/src/main/scala/riftwarp/automatic/GeneratedDecomposer.scala
+++ riftwarp/src/main/scala/riftwarp/automatic/GeneratedDecomposer.scala
@@ -1,26 +1,32 @@
 package riftwarp.automatic
 
+import scala.language.experimental.macros
+
 import scala.reflect.macros.Context
 import almhirt.common._
 import riftwarp._
 
 object GeneratedDecomposerImpl {
-  
+  final def decomposeUncurried[TTWhat <: AnyRef, TTDimension <: RiftDimension](what: TTWhat, into: Dematerializer[TTDimension]): AlmValidation[Dematerializer[TTDimension]] = macro decomposeUncurriedImpl[TTWhat, TTDimension]
 
-  
-  final def decomposeUncurried[TTWhat <: AnyRef: c.WeakTypeTag, TTDimension <: RiftDimension](c: Context)(what: c.Expr[TTWhat], into: c.Expr[Dematerializer[TTDimension]]): c.Expr[AlmValidation[Dematerializer[TTDimension]]] = {
+  final def decomposeUncurriedImpl[TTWhat <: AnyRef: c.WeakTypeTag, TTDimension <: RiftDimension](c: Context)(what: c.Expr[TTWhat], into: c.Expr[Dematerializer[TTDimension]]): c.Expr[AlmValidation[Dematerializer[TTDimension]]] = {
     import c.universe._
     import scala.collection.mutable.Queue
-    
+
     val tWhat = c.weakTypeOf[TTWhat]
-    
+
     //val im = what.m
-    
-    val getterSymbols = 
-      Queue(tWhat.members.filter(m => m.isTerm && !m.isMethod && m.isPublic).map(sym => sym.asTerm.getter).filterNot(_ == NoSymbol))
 
+   
+    val fields = Queue(
+      tWhat.members
+        .filter(m => m.isTerm && !m.isMethod && m.isPublic)
+        .map(sym => sym.asTerm.getter)
+        .filterNot(getter => getter == NoSymbol /*|| getter.name. == "typeDescriptor"*/).toSeq: _*)
 
-   // private def getPri
+    // private def getPri
     ???
   }
+
+  //  final def getPrimitiveAdder[TTWhat <: AnyRef](c: Context)(what: c.Expr[TTWhat]
 }
