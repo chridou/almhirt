0697a31ecee6769b533b9cc2a7393ea31dddab78
Christian Douven <at.at@at.at>
2013-01-18 09:25:11 +0100

    Added posibillity to chose whether to add a type descriptor

--- ext/riftwarp/riftwarp-automatic/src/main/scala/riftwarp/automatic/GeneratedDecomposer.scala
+++ ext/riftwarp/riftwarp-automatic/src/main/scala/riftwarp/automatic/GeneratedDecomposer.scala
@@ -6,7 +6,13 @@ import almhirt.common._
 import riftwarp._
 
 trait GeneratedDecomposer[TToDecompose <: AnyRef] extends Decomposer[TToDecompose] {
-  def decompose[TDimension <: RiftDimension](what: TToDecompose)(into: Dematerializer[TDimension]): AlmValidation[Dematerializer[TDimension]] = decomposeUncurried[TToDecompose, TDimension](what, into)
+  def addTypeDescriptor: Boolean
+  def decompose[TDimension <: RiftDimension](what: TToDecompose)(into: Dematerializer[TDimension]): AlmValidation[Dematerializer[TDimension]] = 
+    if(addTypeDescriptor)
+      into.addTypeDescriptor(this.typeDescriptor).flatMap(decomposeUncurried(what, _))
+    else
+      decomposeUncurried[TToDecompose, TDimension](what, into)
+      
   private def decomposeUncurried[TTWhat <: AnyRef, TTDimension <: RiftDimension](what: TTWhat, into: Dematerializer[TTDimension]): AlmValidation[Dematerializer[TTDimension]] = 
     macro GeneratedDecomposerImpl.decomposeUncurried[TTWhat, TTDimension]
 }
--- riftwarp/src/main/scala/riftwarp/automatic/GeneratedDecomposer.scala
+++ riftwarp/src/main/scala/riftwarp/automatic/GeneratedDecomposer.scala
@@ -5,15 +5,22 @@ import almhirt.common._
 import riftwarp._
 
 object GeneratedDecomposerImpl {
-  def decomposeUncurried[TTWhat <: AnyRef: c.WeakTypeTag, TTDimension <: RiftDimension](c: Context)(what: c.Expr[TTWhat], into: c.Expr[Dematerializer[TTDimension]]): c.Expr[AlmValidation[Dematerializer[TTDimension]]] = {
+  
+
+  
+  final def decomposeUncurried[TTWhat <: AnyRef: c.WeakTypeTag, TTDimension <: RiftDimension](c: Context)(what: c.Expr[TTWhat], into: c.Expr[Dematerializer[TTDimension]]): c.Expr[AlmValidation[Dematerializer[TTDimension]]] = {
     import c.universe._
     import scala.collection.mutable.Queue
     
     val tWhat = c.weakTypeOf[TTWhat]
-    //val im = what.
     
-    val theFields = 
-      Queue(tWhat.members.filter(m => m.isTerm && !m.isMethod && m.isPublic).map(term => term))
+    //val im = what.m
+    
+    val getterSymbols = 
+      Queue(tWhat.members.filter(m => m.isTerm && !m.isMethod && m.isPublic).map(sym => sym.asTerm.getter).filterNot(_ == NoSymbol))
+
+
+   // private def getPri
     ???
   }
 }
