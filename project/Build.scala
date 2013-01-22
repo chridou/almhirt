a4d53122f2c4a56a83d10af4c3566416cbd6bffc
Christian Douven <at.at@at.at>
2013-01-22 13:27:25 +0100

    push to 0.34-SNAPSHOT

--- project/Build.scala
+++ project/Build.scala
@@ -3,7 +3,7 @@ import Keys._
 
 object BuildSettings {
   val buildOrganization = "org.almhirt"
-  val buildVersion      = "0.0.33-SNAPSHOT"
+  val buildVersion      = "0.0.34-SNAPSHOT"
   val buildScalaVersion = "2.10.0"
 
   val buildSettings = Defaults.defaultSettings ++ Seq (
