58591e1d80982254b576bc9d9e422419e8075ec1
Christian Douven <at.at@at.at>
2013-01-24 14:34:48 +0100

    Added more traits for HasExecutionContext with defaults

--- almhirt-common/src/main/scala/almhirt/common/HasExecutionContext.scala
+++ almhirt-common/src/main/scala/almhirt/common/HasExecutionContext.scala
@@ -4,18 +4,32 @@ trait HasExecutionContext {
   def executionContext: scala.concurrent.ExecutionContext
 }
 
+import java.util.concurrent._
+
+trait HasCachedExecutionContext extends HasExecutionContext {
+  val executionContext = scala.concurrent.ExecutionContext.fromExecutor(Executors.newCachedThreadPool())
+}
+
+trait HasSingleThreadedExecutionContext extends HasExecutionContext {
+  val executionContext = scala.concurrent.ExecutionContext.fromExecutor(Executors.newSingleThreadExecutor())
+}
+
+trait HasFixedSizeExecutionContext extends HasExecutionContext {
+  def nThreads: Int
+  val executionContext = scala.concurrent.ExecutionContext.fromExecutor(Executors.newFixedThreadPool(nThreads))
+}
+
 object HasExecutionContext {
-  import java.util.concurrent._
   def apply(): HasExecutionContext = cached
 
   def cached(): HasExecutionContext = new HasExecutionContext {
     val executionContext = scala.concurrent.ExecutionContext.fromExecutor(Executors.newCachedThreadPool())
   }
-  
+
   def single(): HasExecutionContext = new HasExecutionContext {
     val executionContext = scala.concurrent.ExecutionContext.fromExecutor(Executors.newSingleThreadExecutor())
   }
-  
+
   def fixed(nThreads: Int): HasExecutionContext = new HasExecutionContext {
     val executionContext = scala.concurrent.ExecutionContext.fromExecutor(Executors.newFixedThreadPool(nThreads))
   }
