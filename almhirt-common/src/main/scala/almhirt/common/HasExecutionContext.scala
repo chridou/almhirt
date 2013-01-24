ddbe9b1ad1ce9edecf510ecaf944ab6f7739fd78
Christian Douven <at.at@at.at>
2013-01-24 14:28:27 +0100

    default HasExecuitionContext implementations

--- almhirt-common/src/main/scala/almhirt/common/HasExecutionContext.scala
+++ almhirt-common/src/main/scala/almhirt/common/HasExecutionContext.scala
@@ -3,3 +3,20 @@ package almhirt.common
 trait HasExecutionContext {
   def executionContext: scala.concurrent.ExecutionContext
 }
+
+object HasExecutionContext {
+  import java.util.concurrent._
+  def apply(): HasExecutionContext = cached
+
+  def cached(): HasExecutionContext = new HasExecutionContext {
+    val executionContext = scala.concurrent.ExecutionContext.fromExecutor(Executors.newCachedThreadPool())
+  }
+  
+  def single(): HasExecutionContext = new HasExecutionContext {
+    val executionContext = scala.concurrent.ExecutionContext.fromExecutor(Executors.newSingleThreadExecutor())
+  }
+  
+  def fixed(nThreads: Int): HasExecutionContext = new HasExecutionContext {
+    val executionContext = scala.concurrent.ExecutionContext.fromExecutor(Executors.newFixedThreadPool(nThreads))
+  }
+}
