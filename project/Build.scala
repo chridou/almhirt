38a47ead38abae09c7315e8f251ca749a88dceac
Christian Douven <at.at@at.at>
2013-02-08 07:07:12 +0100

    push to v 0.0.48

--- project/Build.scala
+++ project/Build.scala
@@ -3,7 +3,7 @@ import Keys._
 
 object BuildSettings {
   val buildOrganization = "org.almhirt"
-  val buildVersion      = "0.0.47"
+  val buildVersion      = "0.0.48"
   val buildScalaVersion = "2.10.0"
 
   val buildSettings = Defaults.defaultSettings ++ Seq (
