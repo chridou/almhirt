50b81f791038f02c9198588c79fd1e6a0d4ddfcc
Christian Douven <at.at@at.at>
2013-02-11 08:26:08 +0100

    a decomposer is contravariant

--- riftwarp/src/main/scala/riftwarp/Decomposer.scala
+++ riftwarp/src/main/scala/riftwarp/Decomposer.scala
@@ -10,7 +10,7 @@ trait RawDecomposer extends HasAlternativeRiftDescriptors {
 /**
  * instance -> Atoms
  */
-trait Decomposer[T <: AnyRef] extends RawDecomposer {
+trait Decomposer[-T <: AnyRef] extends RawDecomposer {
   def decomposeRaw[TDimension <: RiftDimension](what: AnyRef)(into: Dematerializer[TDimension]): AlmValidation[Dematerializer[TDimension]] =
     almhirt.almvalidation.funs.computeSafely(decompose[TDimension](what.asInstanceOf[T])(into))
   def decompose[TDimension <: RiftDimension](what: T)(into: Dematerializer[TDimension]): AlmValidation[Dematerializer[TDimension]]
