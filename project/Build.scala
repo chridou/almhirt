bf47f7b28af01520d657d16f77b4a576f3d7b69f
Christian Douven <at.at@at.at>
2013-02-12 08:17:41 +0100

    increased version

--- project/Build.scala
+++ project/Build.scala
@@ -3,7 +3,7 @@ import Keys._
 
 object BuildSettings {
   val buildOrganization = "org.almhirt"
-  val buildVersion      = "0.0.55"
+  val buildVersion      = "0.0.56"
   val buildScalaVersion = "2.10.0"
 
   val buildSettings = Defaults.defaultSettings ++ Seq (
