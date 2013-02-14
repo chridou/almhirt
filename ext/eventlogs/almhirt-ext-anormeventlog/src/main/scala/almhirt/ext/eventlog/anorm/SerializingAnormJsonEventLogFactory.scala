b10841bc027b2da8b2fda5cb83ef0cd3b7a26a17
Christian Douven <at.at@at.at>
2013-02-14 08:47:15 +0100

    register the correct terimantion hook

--- ext/eventlogs/almhirt-ext-anormeventlog/src/main/scala/almhirt/ext/eventlog/anorm/SerializingAnormJsonEventLogFactory.scala
+++ ext/eventlogs/almhirt-ext-anormeventlog/src/main/scala/almhirt/ext/eventlog/anorm/SerializingAnormJsonEventLogFactory.scala
@@ -55,7 +55,7 @@ class SerializingAnormJsonEventLogFactory extends DomainEventLogFactory {
             }
             ()
           }
-          theAlmhirt.actorSystem.registerOnTermination(dropOnClose)
+          theAlmhirt.actorSystem.registerOnTermination(dropTable)
         }
         actor
       })
