f3191c04d635e15c228c37dad40f2ae81620f801
chridou <at.at@at.at>
2013-02-25 20:05:36 +0100

    added traits to reflect decompose and decompose async

--- riftwarp/src/main/scala/riftwarp/Decomposer.scala
+++ riftwarp/src/main/scala/riftwarp/Decomposer.scala
@@ -7,15 +7,21 @@ trait RawDecomposer extends HasAlternativeRiftDescriptors {
   def decomposeRaw[TDimension <: RiftDimension](what: AnyRef, into: Dematerializer[TDimension]): AlmValidation[Dematerializer[TDimension]]
 }
 
-//trait Decomposes extends Function[]
+trait Decomposes[-T]{
+  def decompose[TDimension <: RiftDimension](what: T, into: Dematerializer[TDimension]): AlmValidation[Dematerializer[TDimension]]
+}
+
+trait DecomposesAsync[-T]{
+  def decomposeAsync[TDimension <: RiftDimension](what: T, into: Dematerializer[TDimension])(implicit hasExecContext: HasExecutionContext): AlmFuture[Dematerializer[TDimension]]
+}
 
 /**
  * instance -> Atoms
  */
-trait Decomposer[-T <: AnyRef] extends RawDecomposer {
+trait Decomposer[-T <: AnyRef] extends RawDecomposer with Decomposes[T] with DecomposesAsync[T] {
   def decomposeRaw[TDimension <: RiftDimension](what: AnyRef, into: Dematerializer[TDimension]): AlmValidation[Dematerializer[TDimension]] =
     almhirt.almvalidation.funs.computeSafely(decompose[TDimension](what.asInstanceOf[T], into))
   def decompose[TDimension <: RiftDimension](what: T, into: Dematerializer[TDimension]): AlmValidation[Dematerializer[TDimension]]
-  def decomposeAsync[TDimension <: RiftDimension](what: T)(into: Dematerializer[TDimension])(implicit hasExecContext: HasExecutionContext): AlmFuture[Dematerializer[TDimension]] =
+  override def decomposeAsync[TDimension <: RiftDimension](what: T, into: Dematerializer[TDimension])(implicit hasExecContext: HasExecutionContext): AlmFuture[Dematerializer[TDimension]] =
     AlmFuture { decompose[TDimension](what, into) }
 }
