7c204c561f83b50ea744c4669f960c2c05b66bda
Christian Douven <douven@dial.de>
2013-03-18 17:11:27 +0100

    Data access components

--- ext/core/almhirt-ext-core-slick/src/main/scala/almhirt/ext/core/slick/eventlogs/DomainEventLogDataAccess.scala
+++ ext/core/almhirt-ext-core-slick/src/main/scala/almhirt/ext/core/slick/eventlogs/DomainEventLogDataAccess.scala
@@ -5,10 +5,10 @@ import scalaz.syntax.validation._
 import almhirt.common._
 import almhirt.almvalidation.kit._
 
-class TextEventLogDataAccess(override val eventlogtablename: String, override val blobtablename: String, override val profile: scala.slick.driver.ExtendedProfile) extends BlobStoreComponent with TextEventLogStoreComponent with Profile {
+class TextDomainEventLogDataAccess(override val eventlogtablename: String, override val blobtablename: String, override val profile: scala.slick.driver.ExtendedProfile) extends BlobStoreComponent with TextDomainEventLogStoreComponent with Profile {
   import profile.simple._
 
-  private val ddl = BlobRows.ddl ++ TextEventLogRows.ddl
+  private val ddl = BlobRows.ddl ++ TextDomainEventLogRows.ddl
 
   def create(implicit session: Session): AlmValidation[Unit] =
     inTryCatchM { ddl.create }("Could not create schema for TextEventLogDataAccessLayer")
