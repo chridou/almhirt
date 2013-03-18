18353721e72fe4d2c24f5241a1d0dfa8efd47d3a
Christian Douven <douven@dial.de>
2013-03-18 18:38:57 +0100

    Some work

--- ext/core/almhirt-ext-core-slick/src/main/scala/almhirt/ext/core/slick/eventlogs/DomainEventLogDataAccess.scala
+++ ext/core/almhirt-ext-core-slick/src/main/scala/almhirt/ext/core/slick/eventlogs/DomainEventLogDataAccess.scala
@@ -11,10 +11,10 @@ class TextDomainEventLogDataAccess(override val eventlogtablename: String, overr
   private val ddl = BlobRows.ddl ++ TextDomainEventLogRows.ddl
 
   def create(implicit session: Session): AlmValidation[Unit] =
-    inTryCatchM { ddl.create }("Could not create schema for TextEventLogDataAccessLayer")
+    inTryCatchM { ddl.create }("Could not create schema for TextDomainEvent")
 
   def drop(implicit session: Session): AlmValidation[Unit] =
-    inTryCatchM { ddl.drop }("Could not drop schema for TextEventLogDataAccessLayer")
+    inTryCatchM { ddl.drop }("Could not drop schema for TextDomainEvent")
 }
 
 class BinaryDomainEventLogDataAccess(override val eventlogtablename: String, override val blobtablename: String, override val profile: scala.slick.driver.ExtendedProfile) extends BlobStoreComponent with BinaryDomainEventLogStoreComponent with Profile {
@@ -23,8 +23,10 @@ class BinaryDomainEventLogDataAccess(override val eventlogtablename: String, ove
   private val ddl = BlobRows.ddl ++ BinaryDomainEventLogRows.ddl
 
   def create(implicit session: Session): AlmValidation[Unit] =
-    inTryCatchM { ddl.create }("Could not create schema for TextEventLogDataAccessLayer")
+    inTryCatchM { ddl.create }("Could not create schema for BinaryEventLog")
 
   def drop(implicit session: Session): AlmValidation[Unit] =
-    inTryCatchM { ddl.drop }("Could not drop schema for TextEventLogDataAccessLayer")
+    inTryCatchM { ddl.drop }("Could not drop schema for BinaryEventLog")
 }
+
+
--- ext/core/almhirt-ext-core-slick/src/main/scala/almhirt/ext/core/slick/eventlogs/Profile.scala
+++ ext/core/almhirt-ext-core-slick/src/main/scala/almhirt/ext/core/slick/eventlogs/Profile.scala
@@ -1,7 +1,24 @@
 package almhirt.ext.core.slick.eventlogs
 
 import scala.slick.driver.ExtendedProfile
+import scala.slick.driver.H2Driver
+import almhirt.common._
+import almhirt.almvalidation.kit._
 
 trait Profile {
   val profile: ExtendedProfile
+}
+
+final case class ProfileSettings(driver: String, slickDriver: ExtendedProfile)
+
+object Profiles {
+  val profiles: Map[String, ProfileSettings] =
+    Map(
+      "h2" ->  ProfileSettings("org.h2.Driver", H2Driver) )
+      
+  def createTextDomainEventLogAccess(profile: String, eventlogtablename: String, blobtablename: String): AlmValidation[TextDomainEventLogDataAccess] =
+    for {
+      profile <- (profiles.lift >? profile)
+    } yield TextDomainEventLogDataAccess()
+    
 }
