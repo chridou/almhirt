6493db78fce29a07e7b975e8c4c47e2b266af7e8
Christian Douven <douven@dial.de>
2013-03-18 07:24:37 +0100

    Split trait into read and write side

--- almhirt-core/src/main/scala/almhirt/eventlog/EventLog.scala
+++ almhirt-core/src/main/scala/almhirt/eventlog/EventLog.scala
@@ -5,11 +5,16 @@ import org.joda.time.DateTime
 import almhirt.core.Event
 import almhirt.common.AlmFuture
 
-trait EventLog {
+trait CanStoreEvents {
   def storeEvent(event: Event): AlmFuture[Event]
+}
+
+trait HasEvents {
   def getAllEvents: AlmFuture[Iterable[Event]]
   def getEvent(eventId: JUUID): AlmFuture[Event]
   def getEventsFrom(from: DateTime): AlmFuture[Iterable[Event]]
   def getEventsUntil(until: DateTime): AlmFuture[Iterable[Event]]
   def getEventsFromUntil(from: DateTime, until: DateTime): AlmFuture[Iterable[Event]]
 }
+
+trait EventLog extends CanStoreEvents with HasEvents with almhirt.almakka.ActorBased
