6d0fbd317dd28f98f0944a6c78a255f5f821b8f8
Christian Douven <douven@dial.de>
2013-03-19 13:53:16 +0100

    Added an incomplete trait for asyncdomaineventstorage

--- almhirt-core/src/main/scala/almhirt/eventlog/util/DomainEventStorage.scala
+++ almhirt-core/src/main/scala/almhirt/eventlog/util/DomainEventStorage.scala
@@ -14,3 +14,14 @@ trait SyncDomainEventStorage {
   def getAllEventsForTo(aggId: JUUID, toVersion: Long): AlmValidation[Iterable[DomainEvent]]
   def getAllEventsForFromTo(aggId: JUUID, fromVersion: Long, toVersion: Long): AlmValidation[Iterable[DomainEvent]]
 }
+
+trait AsyncDomainEventStorage {
+  def storeEvent(eventLogRow: DomainEvent): AlmFuture[DomainEvent]
+  def storeManyEvents(eventLogRows: Seq[DomainEvent]): AlmFuture[(IndexedSeq[DomainEvent], Option[(Problem, IndexedSeq[DomainEvent])])]
+  def getEventById(id: JUUID): AlmFuture[DomainEvent]
+  def getAllEvents(): AlmFuture[Iterable[DomainEvent]]
+  def getAllEventsFor(aggId: JUUID): AlmFuture[Iterable[DomainEvent]]
+  def getAllEventsForFrom(aggId: JUUID, fromVersion: Long): AlmFuture[Iterable[DomainEvent]]
+  def getAllEventsForTo(aggId: JUUID, toVersion: Long): AlmFuture[Iterable[DomainEvent]]
+  def getAllEventsForFromTo(aggId: JUUID, fromVersion: Long, toVersion: Long): AlmFuture[Iterable[DomainEvent]]
+}
