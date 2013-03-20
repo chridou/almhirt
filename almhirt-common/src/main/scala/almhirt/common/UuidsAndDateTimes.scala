42c334bf7459ae09d7cfe1eef70844ae87f3639e
Christian Douven <douven@dial.de>
2013-03-20 06:47:18 +0100

    Added factory in companion, therefore default implementations should be removed

--- almhirt-common/src/main/scala/almhirt/common/UuidsAndDateTimes.scala
+++ almhirt-common/src/main/scala/almhirt/common/UuidsAndDateTimes.scala
@@ -2,8 +2,15 @@ package almhirt.common
 
 import org.joda.time.DateTime
 
-trait CanCreateUuid{ def getUuid(): java.util.UUID = java.util.UUID.randomUUID() }
+trait CanCreateUuid { def getUuid(): java.util.UUID = java.util.UUID.randomUUID() }
 
-trait CanCreateDateTime{ def getDateTime(): DateTime = org.joda.time.DateTime.now() }
+trait CanCreateDateTime { def getDateTime(): DateTime = org.joda.time.DateTime.now() }
 
 trait CanCreateUuidsAndDateTimes extends CanCreateUuid with CanCreateDateTime
+
+object CanCreateUuidsAndDateTimes {
+  def apply(): CanCreateUuidsAndDateTimes = new CanCreateUuidsAndDateTimes {
+     override def getUuid(): java.util.UUID = java.util.UUID.randomUUID()
+     override def getDateTime(): DateTime = org.joda.time.DateTime.now()
+  }
+}
