5b0e00ae93ecc7cc39b29ac4ee71c61e3ed47c11
Christian Douven <douven@dial.de>
2013-03-27 07:09:01 +0100

    some doc

--- almhirt-core/src/main/scala/almhirt/environment/configuration/bootstrappers/RegistersServiceRegistry.scala
+++ almhirt-core/src/main/scala/almhirt/environment/configuration/bootstrappers/RegistersServiceRegistry.scala
@@ -5,6 +5,9 @@ import akka.event.LoggingAdapter
 import almhirt.core.HasServiceRegistry
 import almhirt.core.ServiceRegistry
 
+/**
+ * This one registers the service registry to itself.
+ */
 trait RegistersServiceRegistry extends PreInitBootstrapperPhase { self: HasServiceRegistry =>
   override def preInit(startUpLogger: LoggingAdapter): BootstrapperPhaseResult =
     super.preInit(startUpLogger).andThen {
