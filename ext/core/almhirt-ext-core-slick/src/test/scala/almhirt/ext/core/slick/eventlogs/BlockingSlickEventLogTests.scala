76f59214e424aeb272d7da30cdba6f606d7c590b
Christian Douven <douven@dial.de>
2013-03-27 15:22:46 +0100

    Slick event log tests

--- ext/core/almhirt-ext-core-slick/src/test/scala/almhirt/ext/core/slick/eventlogs/BlockingSlickEventLogTests.scala
+++ ext/core/almhirt-ext-core-slick/src/test/scala/almhirt/ext/core/slick/eventlogs/BlockingSlickEventLogTests.scala
@@ -2,8 +2,169 @@ package almhirt.ext.core.slick.eventlogs
 
 import org.scalatest._
 import org.scalatest.matchers.MustMatchers
+import scala.concurrent.duration._
+import scalaz.syntax.validation._
+import almhirt.common._
+import almhirt.core.Almhirt
+import almhirt.almvalidation.kit._
+import almhirt.environment._
+import almhirt.eventlog._
+import almhirt.domain._
+import almhirt.core.test.BlockingRepoCoreBootstrapper
+import almhirt.ext.core.riftwarp.RiftWarpBootstrapper
+import almhirt.core.riftwarp.test.WithTestDecomposersAndRecomposersBootstrapper
+import almhirt.core.test._
+import almhirt.domain.AggregateRootRef
+import com.typesafe.config.ConfigFactory
+import almhirt.environment.configuration.Bootstrapper
+import org.joda.time.DateTime
+import almhirt.messaging.Message
 
+class BlockingSlickEventLogOnAlmhirtTests extends FunSuite with MustMatchers with AlmhirtTestKit {
+  implicit object ccuad extends CanCreateUuidsAndDateTimes
+  implicit val atMost = FiniteDuration(5, "s")
+  val bootstrapper =
+    new Bootstrapper with RiftWarpBootstrapper with BlockingRepoCoreBootstrapper with WithTestDecomposersAndRecomposersBootstrapper {
+      val config = ConfigFactory.load
 
-class BlockingSlickEventLogTests extends FunSuite with MustMatchers{
+    }
 
+  def inLocalTestAlmhirt[T](compute: AlmhirtForExtendedTesting => T) =
+    inExtendedTestAlmhirt(bootstrapper)(compute)
+
+  val baseDateTime = new DateTime(2013, 1, 1, 0, 0)
+  val events = (for (i <- 0 until 100) yield {
+    new TestPersonCreated(DomainEventHeader(ccuad.getUuid, AggregateRootRef(ccuad.getUuid, i), baseDateTime.plusHours(i)), s"testEvent_$i")
+  }).toVector
+
+  val idsVector = events.map(_.header.id)
+
+  def withIsolatedFilledEventLog[T](f: EventLog => AlmValidation[T]): AlmValidation[T] = {
+    inLocalTestAlmhirt { almhirt =>
+      for {
+        eventlog <- almhirt.getService[EventLog]
+        _ <- inTryCatch { events.foreach(eventlog.storeEvent(_)) }
+        res <- f(eventlog)
+      } yield res
+    }
+  }
+
+  test("The eventlog must store the events") {
+    withIsolatedFilledEventLog(eventlog => true.success).isSuccess must be(true)
+  }
+
+  test("The eventlog must store and read the events") {
+    withIsolatedFilledEventLog { eventlog =>
+      val res = eventlog.getAllEvents.awaitResult
+      res.map(events => events.map(_.header.id).toVector)
+    }.forceResult must equal(idsVector)
+  }
+
+  test("The eventlog must return the first event when queried with its id") {
+    withIsolatedFilledEventLog { eventlog =>
+      val res = eventlog.getEvent(events.head.header.id).awaitResult
+      res
+    }.forceResult.header.id must equal(idsVector.head)
+  }
+
+  test("The eventlog fail when queried for an unknown id") {
+    withIsolatedFilledEventLog { eventlog =>
+      val res = eventlog.getEvent(ccuad.getUuid).awaitResult
+      res
+    }.isFailure must be(true)
+  }
+  
+  test("The eventlog must return all events when from is before the first events timestamp") {
+    withIsolatedFilledEventLog { eventlog =>
+      val res = eventlog.getEventsFrom(events.head.header.timestamp.minusMillis(1)).awaitResult
+      res.map(events => events.map(_.header.id).toVector)
+    }.forceResult must equal(idsVector)
+  }
+  
+  test("The eventlog must return all events when from is equal the first events timestamp") {
+    withIsolatedFilledEventLog { eventlog =>
+      val res = eventlog.getEventsFrom(events.head.header.timestamp).awaitResult
+      res.map(events => events.map(_.header.id).toVector)
+    }.forceResult must equal(idsVector)
+  }
+  
+  test("The eventlog must return no events when from is after the last events timestamp") {
+    withIsolatedFilledEventLog { eventlog =>
+      val res = eventlog.getEventsFrom(events.last.header.timestamp.plusMillis(1)).awaitResult
+      res.map(events => events.map(_.header.id).toVector)
+    }.forceResult must equal(Vector.empty)
+  }
+
+  test("The eventlog must return the event when from is equal to the last events timestamp") {
+    withIsolatedFilledEventLog { eventlog =>
+      val res = eventlog.getEventsFrom(events.last.header.timestamp).awaitResult
+      res.map(events => events.map(_.header.id).toVector)
+    }.forceResult must equal(Vector(idsVector.last))
+  }
+
+  test("The eventlog must return all events when until is after the last events timestamp") {
+    withIsolatedFilledEventLog { eventlog =>
+      val res = eventlog.getEventsUntil(events.last.header.timestamp.plusMillis(1)).awaitResult
+      res.map(events => events.map(_.header.id).toVector)
+    }.forceResult must equal(idsVector)
+  }
+
+  test("The eventlog must return all events except the last when until is equal to the last events timestamp") {
+    withIsolatedFilledEventLog { eventlog =>
+      val res = eventlog.getEventsUntil(events.last.header.timestamp).awaitResult
+      res.map(events => events.map(_.header.id).toVector)
+    }.forceResult must equal(idsVector.take(idsVector.length-1))
+  }
+
+  test("The eventlog must return no events when until is equal the first events timestamp") {
+    withIsolatedFilledEventLog { eventlog =>
+      val res = eventlog.getEventsUntil(events.head.header.timestamp).awaitResult
+      res.map(events => events.map(_.header.id).toVector)
+    }.forceResult must equal(Vector.empty)
+  }
+  
+  test("The eventlog must return no events when until is before the first events timestamp") {
+    withIsolatedFilledEventLog { eventlog =>
+      val res = eventlog.getEventsUntil(events.head.header.timestamp.minusMillis(1)).awaitResult
+      res.map(events => events.map(_.header.id).toVector)
+    }.forceResult must equal(Vector.empty)
+  }
+
+  test("The eventlog must return all events when from is the first events timestamp and until is after the last events timestamp") {
+    withIsolatedFilledEventLog { eventlog =>
+      val res = eventlog.getEventsFromUntil(events.head.header.timestamp, events.last.header.timestamp.plusMillis(1)).awaitResult
+      res.map(events => events.map(_.header.id).toVector)
+    }.forceResult must equal(idsVector)
+  }
+
+  test("The eventlog must return all events except the last when from is the first events timestamp and until the last events timestamp") {
+    withIsolatedFilledEventLog { eventlog =>
+      val res = eventlog.getEventsFromUntil(events.head.header.timestamp, events.last.header.timestamp).awaitResult
+      res.map(events => events.map(_.header.id).toVector)
+    }.forceResult must equal(idsVector.take(idsVector.length-1))
+  }
+  
+  test("The eventlog must return all events except the first and last when from after the first events timestamp and until the last events timestamp") {
+    withIsolatedFilledEventLog { eventlog =>
+      val res = eventlog.getEventsFromUntil(events.head.header.timestamp.plusMillis(1), events.last.header.timestamp).awaitResult
+      res.map(events => events.map(_.header.id).toVector)
+    }.forceResult must equal(idsVector.take(idsVector.length-1).drop(1))
+  }
+
+  test("The eventlog must return no events  when from is the last events events timestamp and until the first events timestamp") {
+    withIsolatedFilledEventLog { eventlog =>
+      val res = eventlog.getEventsFromUntil(events.last.header.timestamp, events.head.header.timestamp).awaitResult
+      res.map(events => events.map(_.header.id).toVector)
+    }.forceResult must equal(Vector.empty)
+  }
+  
+  ignore("The eventlog must store the events via the messagehub and the return all events") {
+    (inLocalTestAlmhirt { almhirt =>
+      for {
+        eventlog <- almhirt.getService[EventLog]
+        _ <- inTryCatch { events.foreach(event => almhirt.publish(Message(event))) }
+        res <- eventlog.getAllEvents.awaitResult.map(events => events.map(event => event.header.id).toVector)
+      } yield res
+    }).forceResult must equal(idsVector)
+  }
 }
--- ext/core/almhirt-ext-core-slick/src/test/scala/almhirt/ext/core/slick/eventlogs/SerializingSlickJsonDomainEventLogSpecs.scala
+++ ext/core/almhirt-ext-core-slick/src/test/scala/almhirt/ext/core/slick/eventlogs/SerializingSlickJsonDomainEventLogSpecs.scala
@@ -48,134 +48,134 @@ class SerializingSlickJsonDomainEventLogSpecs extends FlatSpec with ShouldMatche
         val res = eventLog.storeEvents(IndexedSeq(event)).awaitResult(Duration(2, "s")).forceResult
       }
     }
-//  it should "accept an event and return exactly 1 event as the committed events" in {
-//    withEmptyEventLog { (eventLog, almhirt) =>
-//      val event = TestPersonCreated(AggregateRootRef(aggIdForEvent), "test")
-//      val res = eventLog.storeEvents(IndexedSeq(event)).awaitResult(Duration(2, "s"))
-//      res.forceResult._1 should have size 1
-//    }
-//  }
-//  it should "accept an event and return exactly 1 event when queried for all events" in {
-//    withEmptyEventLog { (eventLog, almhirt) =>
-//      val event = TestPersonCreated(AggregateRootRef(aggIdForEvent), "test")
-//      val resCommit = eventLog.storeEvents(IndexedSeq(event)).awaitResult(Duration(1, "s")).forceResult
-//      val res = eventLog.getEvents(aggIdForEvent).awaitResult(Duration(2, "s"))
-//      println(res)
-//      res.forceResult should have size 1
-//    }
-//  }
-//  it should "accept an event and return exactly the same event when queried for all events" in {
-//    withEmptyEventLog { (eventLog, almhirt) =>
-//      val event = TestPersonCreated(AggregateRootRef(aggIdForEvent), "test")
-//      val resCommit = eventLog.storeEvents(IndexedSeq(event)).awaitResult(Duration(1, "s")).forceResult
-//      val res = eventLog.getEvents(aggIdForEvent).awaitResult(Duration(2, "s")).forceResult
-//      res.headOption should equal(Some(event))
-//    }
-//  }
-//  it should "accept 100 events with the same aggId and return them in the same order (getEvents(aggId))" in {
-//    withEmptyEventLog { (eventLog, almhirt) =>
-//      val aggId = almhirt.getUuid
-//      val firstEvent = TestPersonCreated(AggregateRootRef(aggId), "testEvent0")
-//      val events = firstEvent +: (for (i <- 1 until 100) yield TestPersonNameChanged(AggregateRootRef(aggId, i.toLong), "testEvent%d".format(i)))
-//      val resCommit = eventLog.storeEvents(events).awaitResult(Duration(2, "s")).forceResult
-//      val res = eventLog.getEvents(aggId).awaitResult(Duration(2, "s")).withFailEffect(p => println(p))
-//      res.forceResult should equal(events)
-//    }
-//  }
-//  it should "accept 100 events with the same aggId shuffled with 100 other events and return the events for a specific aggId in the same order(getEvents(aggId))" in {
-//    withEmptyEventLog { (eventLog, almhirt) =>
-//      val aggId = almhirt.getUuid
-//      val firstEvent = TestPersonCreated(AggregateRootRef(aggId), "testEvent0")
-//      val events = firstEvent :: (for (i <- 1 until 100) yield TestPersonNameChanged(AggregateRootRef(aggId, i.toLong), "testEvent%d".format(i)).asInstanceOf[DomainEvent]).toList
-//      val eventsToShuffleIn = (for (i <- 0 until 100) yield TestPersonCreated(AggregateRootRef(almhirt.getUuid), "shuffle%d".format(i)).asInstanceOf[DomainEvent]).toList
-//      val shuffled = events.reverse.zip(eventsToShuffleIn.reverse).foldLeft(List.empty[DomainEvent])((acc, elem) => elem._1 :: elem._2 :: acc)
-//      val resCommit = eventLog.storeEvents(shuffled.toVector).awaitResult(Duration(2, "s")).forceResult
-//      val res = eventLog.getEvents(aggId).awaitResult(Duration(2, "s")).withFailEffect(p => println(p)).forceResult
-//      res should equal(events)
-//    }
-//  }
-//  it should "accept 100 events with the same aggId shuffled with 100 other events and return the last 10 events for a specific aggId in the same order(getEvents(aggId, from))" in {
-//    withEmptyEventLog { (eventLog, almhirt) =>
-//      val aggId = almhirt.getUuid
-//      val firstEvent = TestPersonCreated(AggregateRootRef(aggId), "testEvent0")
-//      val events = firstEvent :: (for (i <- 1 until 100) yield TestPersonNameChanged(AggregateRootRef(aggId, i.toLong), "testEvent%d".format(i)).asInstanceOf[DomainEvent]).toList
-//      val eventsToShuffleIn = (for (i <- 0 until 100) yield TestPersonCreated(AggregateRootRef(almhirt.getUuid), "shuffle%d".format(i)).asInstanceOf[DomainEvent]).toList
-//      val shuffled = events.reverse.zip(eventsToShuffleIn.reverse).foldLeft(List.empty[DomainEvent])((acc, elem) => elem._1 :: elem._2 :: acc)
-//      val resCommit = eventLog.storeEvents(shuffled.toVector).awaitResult(Duration(2, "s")).forceResult
-//      val res = eventLog.getEvents(aggId, 90).awaitResult(Duration(2, "s")).withFailEffect(p => println(p)).forceResult
-//      res should equal(events.drop(90))
-//    }
-//  }
-//  it should "accept 100 events with the same aggId shuffled with 100 other events and return the first 10 events for a specific aggId in the same order(getEvents(aggId, from, to))" in {
-//    withEmptyEventLog { (eventLog, almhirt) =>
-//      val aggId = almhirt.getUuid
-//      val firstEvent = TestPersonCreated(AggregateRootRef(aggId), "testEvent0")
-//      val events = firstEvent :: (for (i <- 1 until 100) yield TestPersonNameChanged(AggregateRootRef(aggId, i.toLong), "testEvent%d".format(i)).asInstanceOf[DomainEvent]).toList
-//      val eventsToShuffleIn = (for (i <- 0 until 100) yield TestPersonCreated(AggregateRootRef(almhirt.getUuid), "shuffle%d".format(i)).asInstanceOf[DomainEvent]).toList
-//      val shuffled = events.reverse.zip(eventsToShuffleIn.reverse).foldLeft(List.empty[DomainEvent])((acc, elem) => elem._1 :: elem._2 :: acc)
-//      val resCommit = eventLog.storeEvents(shuffled.toVector).awaitResult(Duration(1, "s")).forceResult
-//      val res = eventLog.getEvents(aggId, 0, 9).awaitResult(Duration(2, "s")).withFailEffect(p => println(p)).forceResult
-//      res should equal(events.take(10))
-//    }
-//  }
-//  it should "accept 100 events with the same aggId shuffled with 100 other events and return the events from 40 to 59 for a specific aggId in the same order(getEvents(aggId, from, to))" in {
-//    withEmptyEventLog { (eventLog, almhirt) =>
-//      val aggId = almhirt.getUuid
-//      val firstEvent = TestPersonCreated(AggregateRootRef(aggId), "testEvent0")
-//      val events = firstEvent :: (for (i <- 1 until 100) yield TestPersonNameChanged(AggregateRootRef(aggId, i.toLong), "testEvent%d".format(i)).asInstanceOf[DomainEvent]).toList
-//      val eventsToShuffleIn = (for (i <- 0 until 100) yield TestPersonCreated(AggregateRootRef(almhirt.getUuid), "shuffle%d".format(i)).asInstanceOf[DomainEvent]).toList
-//      val shuffled = events.reverse.zip(eventsToShuffleIn.reverse).foldLeft(List.empty[DomainEvent])((acc, elem) => elem._1 :: elem._2 :: acc)
-//      val resCommit = eventLog.storeEvents(shuffled.toVector).awaitResult(Duration(2, "s")).forceResult
-//      val res = eventLog.getEvents(aggId, 40, 59).awaitResult(Duration(2, "s")).withFailEffect(p => println(p)).forceResult
-//      res should equal(events.drop(40).take(20))
-//    }
-//  }
-//
-//  "An anorm SerializingAnormEventLogwhen already containing an event with version 0L when nothing else is added" should
-//    "return exactly 1 event when queried for all events for the given id(the one that was already present)" in {
-//      withEventLogWithOneTestEventA { (eventLog, almhirt) =>
-//        val res = eventLog.getEvents(testEventA.aggId).awaitResult(Duration(2, "s")).forceResult
-//        res should have size 1
-//      }
-//    }
-//
-//  "An anorm SerializingAnormEventLogwhen already containing an event with version 0L after adding another event" should
-//    "have accepted an event with a different id" in {
-//      withEmptyEventLog { (eventLog, almhirt) =>
-//        val event = TestPersonCreated(AggregateRootRef(almhirt.getUuid), "test")
-//        val res = eventLog.storeEvents(IndexedSeq(event)).awaitResult(Duration(2, "s")).forceResult
-//      }
-//    }
-//  it should "return exactly 2 events when queried for all events" in {
-//    withEventLogWithOneTestEventA { (eventLog, almhirt) =>
-//      val event = TestPersonCreated(AggregateRootRef(almhirt.getUuid), "test")
-//      eventLog.storeEvents(IndexedSeq(event)).awaitResult(Duration(2, "s")).forceResult
-//      val res = eventLog.getAllEvents.awaitResult(Duration(2, "s")).forceResult
-//      res should have size 2
-//    }
-//  }
-//  it should "return exactly 1 event after adding another event when queried for all events for the given id(the one that was already present)" in {
-//    withEventLogWithOneTestEventA { (eventLog, almhirt) =>
-//      val event = TestPersonCreated(AggregateRootRef(almhirt.getUuid), "test")
-//      eventLog.storeEvents(IndexedSeq(event)).awaitResult(Duration(2, "s")).forceResult
-//      val res = eventLog.getEvents(testEventA.aggId).awaitResult(Duration(2, "s")).forceResult
-//      res should have size 1
-//    }
-//  }
-//  it should "return exactly 1 event after adding another event when queried for all events for the given id(the added one)" in {
-//    withEventLogWithOneTestEventA { (eventLog, almhirt) =>
-//      val event = TestPersonCreated(AggregateRootRef(almhirt.getUuid), "test")
-//      eventLog.storeEvents(IndexedSeq(event)).awaitResult(Duration(2, "s")).forceResult
-//      val res = eventLog.getEvents(event.aggId).awaitResult(Duration(2, "s")).forceResult
-//      res should have size 1
-//    }
-//  }
-//  it should "return the 2 events in the order they have been added" in {
-//    withEventLogWithOneTestEventA { (eventLog, almhirt) =>
-//      val event = TestPersonCreated(AggregateRootRef(almhirt.getUuid), "test")
-//      eventLog.storeEvents(IndexedSeq(event)).awaitResult(Duration(2, "s")).forceResult
-//      val res = eventLog.getAllEvents.awaitResult(Duration(2, "s")).forceResult
-//      res.toList should equal(List(testEventA, event))
-//    }
-//  }
+  it should "accept an event and return exactly 1 event as the committed events" in {
+    withEmptyEventLog { (eventLog, almhirt) =>
+      val event = TestPersonCreated(AggregateRootRef(aggIdForEvent), "test")
+      val res = eventLog.storeEvents(IndexedSeq(event)).awaitResult(Duration(2, "s"))
+      res.forceResult._1 should have size 1
+    }
+  }
+  it should "accept an event and return exactly 1 event when queried for all events" in {
+    withEmptyEventLog { (eventLog, almhirt) =>
+      val event = TestPersonCreated(AggregateRootRef(aggIdForEvent), "test")
+      val resCommit = eventLog.storeEvents(IndexedSeq(event)).awaitResult(Duration(1, "s")).forceResult
+      val res = eventLog.getEvents(aggIdForEvent).awaitResult(Duration(2, "s"))
+      println(res)
+      res.forceResult should have size 1
+    }
+  }
+  it should "accept an event and return exactly the same event when queried for all events" in {
+    withEmptyEventLog { (eventLog, almhirt) =>
+      val event = TestPersonCreated(AggregateRootRef(aggIdForEvent), "test")
+      val resCommit = eventLog.storeEvents(IndexedSeq(event)).awaitResult(Duration(1, "s")).forceResult
+      val res = eventLog.getEvents(aggIdForEvent).awaitResult(Duration(2, "s")).forceResult
+      res.headOption should equal(Some(event))
+    }
+  }
+  it should "accept 100 events with the same aggId and return them in the same order (getEvents(aggId))" in {
+    withEmptyEventLog { (eventLog, almhirt) =>
+      val aggId = almhirt.getUuid
+      val firstEvent = TestPersonCreated(AggregateRootRef(aggId), "testEvent0")
+      val events = firstEvent +: (for (i <- 1 until 100) yield TestPersonNameChanged(AggregateRootRef(aggId, i.toLong), "testEvent%d".format(i)))
+      val resCommit = eventLog.storeEvents(events).awaitResult(Duration(2, "s")).forceResult
+      val res = eventLog.getEvents(aggId).awaitResult(Duration(2, "s")).withFailEffect(p => println(p))
+      res.forceResult should equal(events)
+    }
+  }
+  it should "accept 100 events with the same aggId shuffled with 100 other events and return the events for a specific aggId in the same order(getEvents(aggId))" in {
+    withEmptyEventLog { (eventLog, almhirt) =>
+      val aggId = almhirt.getUuid
+      val firstEvent = TestPersonCreated(AggregateRootRef(aggId), "testEvent0")
+      val events = firstEvent :: (for (i <- 1 until 100) yield TestPersonNameChanged(AggregateRootRef(aggId, i.toLong), "testEvent%d".format(i)).asInstanceOf[DomainEvent]).toList
+      val eventsToShuffleIn = (for (i <- 0 until 100) yield TestPersonCreated(AggregateRootRef(almhirt.getUuid), "shuffle%d".format(i)).asInstanceOf[DomainEvent]).toList
+      val shuffled = events.reverse.zip(eventsToShuffleIn.reverse).foldLeft(List.empty[DomainEvent])((acc, elem) => elem._1 :: elem._2 :: acc)
+      val resCommit = eventLog.storeEvents(shuffled.toVector).awaitResult(Duration(2, "s")).forceResult
+      val res = eventLog.getEvents(aggId).awaitResult(Duration(2, "s")).withFailEffect(p => println(p)).forceResult
+      res should equal(events)
+    }
+  }
+  it should "accept 100 events with the same aggId shuffled with 100 other events and return the last 10 events for a specific aggId in the same order(getEvents(aggId, from))" in {
+    withEmptyEventLog { (eventLog, almhirt) =>
+      val aggId = almhirt.getUuid
+      val firstEvent = TestPersonCreated(AggregateRootRef(aggId), "testEvent0")
+      val events = firstEvent :: (for (i <- 1 until 100) yield TestPersonNameChanged(AggregateRootRef(aggId, i.toLong), "testEvent%d".format(i)).asInstanceOf[DomainEvent]).toList
+      val eventsToShuffleIn = (for (i <- 0 until 100) yield TestPersonCreated(AggregateRootRef(almhirt.getUuid), "shuffle%d".format(i)).asInstanceOf[DomainEvent]).toList
+      val shuffled = events.reverse.zip(eventsToShuffleIn.reverse).foldLeft(List.empty[DomainEvent])((acc, elem) => elem._1 :: elem._2 :: acc)
+      val resCommit = eventLog.storeEvents(shuffled.toVector).awaitResult(Duration(2, "s")).forceResult
+      val res = eventLog.getEvents(aggId, 90).awaitResult(Duration(2, "s")).withFailEffect(p => println(p)).forceResult
+      res should equal(events.drop(90))
+    }
+  }
+  it should "accept 100 events with the same aggId shuffled with 100 other events and return the first 10 events for a specific aggId in the same order(getEvents(aggId, from, to))" in {
+    withEmptyEventLog { (eventLog, almhirt) =>
+      val aggId = almhirt.getUuid
+      val firstEvent = TestPersonCreated(AggregateRootRef(aggId), "testEvent0")
+      val events = firstEvent :: (for (i <- 1 until 100) yield TestPersonNameChanged(AggregateRootRef(aggId, i.toLong), "testEvent%d".format(i)).asInstanceOf[DomainEvent]).toList
+      val eventsToShuffleIn = (for (i <- 0 until 100) yield TestPersonCreated(AggregateRootRef(almhirt.getUuid), "shuffle%d".format(i)).asInstanceOf[DomainEvent]).toList
+      val shuffled = events.reverse.zip(eventsToShuffleIn.reverse).foldLeft(List.empty[DomainEvent])((acc, elem) => elem._1 :: elem._2 :: acc)
+      val resCommit = eventLog.storeEvents(shuffled.toVector).awaitResult(Duration(1, "s")).forceResult
+      val res = eventLog.getEvents(aggId, 0, 9).awaitResult(Duration(2, "s")).withFailEffect(p => println(p)).forceResult
+      res should equal(events.take(10))
+    }
+  }
+  it should "accept 100 events with the same aggId shuffled with 100 other events and return the events from 40 to 59 for a specific aggId in the same order(getEvents(aggId, from, to))" in {
+    withEmptyEventLog { (eventLog, almhirt) =>
+      val aggId = almhirt.getUuid
+      val firstEvent = TestPersonCreated(AggregateRootRef(aggId), "testEvent0")
+      val events = firstEvent :: (for (i <- 1 until 100) yield TestPersonNameChanged(AggregateRootRef(aggId, i.toLong), "testEvent%d".format(i)).asInstanceOf[DomainEvent]).toList
+      val eventsToShuffleIn = (for (i <- 0 until 100) yield TestPersonCreated(AggregateRootRef(almhirt.getUuid), "shuffle%d".format(i)).asInstanceOf[DomainEvent]).toList
+      val shuffled = events.reverse.zip(eventsToShuffleIn.reverse).foldLeft(List.empty[DomainEvent])((acc, elem) => elem._1 :: elem._2 :: acc)
+      val resCommit = eventLog.storeEvents(shuffled.toVector).awaitResult(Duration(2, "s")).forceResult
+      val res = eventLog.getEvents(aggId, 40, 59).awaitResult(Duration(2, "s")).withFailEffect(p => println(p)).forceResult
+      res should equal(events.drop(40).take(20))
+    }
+  }
+
+  "An anorm SerializingAnormEventLogwhen already containing an event with version 0L when nothing else is added" should
+    "return exactly 1 event when queried for all events for the given id(the one that was already present)" in {
+      withEventLogWithOneTestEventA { (eventLog, almhirt) =>
+        val res = eventLog.getEvents(testEventA.aggId).awaitResult(Duration(2, "s")).forceResult
+        res should have size 1
+      }
+    }
+
+  "An anorm SerializingAnormEventLogwhen already containing an event with version 0L after adding another event" should
+    "have accepted an event with a different id" in {
+      withEmptyEventLog { (eventLog, almhirt) =>
+        val event = TestPersonCreated(AggregateRootRef(almhirt.getUuid), "test")
+        val res = eventLog.storeEvents(IndexedSeq(event)).awaitResult(Duration(2, "s")).forceResult
+      }
+    }
+  it should "return exactly 2 events when queried for all events" in {
+    withEventLogWithOneTestEventA { (eventLog, almhirt) =>
+      val event = TestPersonCreated(AggregateRootRef(almhirt.getUuid), "test")
+      eventLog.storeEvents(IndexedSeq(event)).awaitResult(Duration(2, "s")).forceResult
+      val res = eventLog.getAllEvents.awaitResult(Duration(2, "s")).forceResult
+      res should have size 2
+    }
+  }
+  it should "return exactly 1 event after adding another event when queried for all events for the given id(the one that was already present)" in {
+    withEventLogWithOneTestEventA { (eventLog, almhirt) =>
+      val event = TestPersonCreated(AggregateRootRef(almhirt.getUuid), "test")
+      eventLog.storeEvents(IndexedSeq(event)).awaitResult(Duration(2, "s")).forceResult
+      val res = eventLog.getEvents(testEventA.aggId).awaitResult(Duration(2, "s")).forceResult
+      res should have size 1
+    }
+  }
+  it should "return exactly 1 event after adding another event when queried for all events for the given id(the added one)" in {
+    withEventLogWithOneTestEventA { (eventLog, almhirt) =>
+      val event = TestPersonCreated(AggregateRootRef(almhirt.getUuid), "test")
+      eventLog.storeEvents(IndexedSeq(event)).awaitResult(Duration(2, "s")).forceResult
+      val res = eventLog.getEvents(event.aggId).awaitResult(Duration(2, "s")).forceResult
+      res should have size 1
+    }
+  }
+  it should "return the 2 events in the order they have been added" in {
+    withEventLogWithOneTestEventA { (eventLog, almhirt) =>
+      val event = TestPersonCreated(AggregateRootRef(almhirt.getUuid), "test")
+      eventLog.storeEvents(IndexedSeq(event)).awaitResult(Duration(2, "s")).forceResult
+      val res = eventLog.getAllEvents.awaitResult(Duration(2, "s")).forceResult
+      res.toList should equal(List(testEventA, event))
+    }
+  }
 }
