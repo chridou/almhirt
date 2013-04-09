f87c662d58820a43229975497463eaf4829a31f5
Christian Douven <douven@dial.de>
2013-04-09 15:24:15 +0200

    Removed BLOBS from common

--- almhirt-common/src/main/scala/almhirt/serialization/Serializer.scala
+++ almhirt-common/src/main/scala/almhirt/serialization/Serializer.scala
@@ -18,8 +18,6 @@ trait CanSerialize[-TIn] extends WorksWithSerializedRepresentation {
   // (type, serialized)
   def serialize(channel: String)(what: TIn, typeHint: Option[String]): AlmValidation[(Option[String], SerializedRepr)]
   def serializeAsync(channel: String)(what: TIn, typeHint: Option[String]): AlmFuture[(Option[String], SerializedRepr)]
-  def serializeBlobSeparating(blobPolicy: BlobSerializationPolicy)(channel: String)(what: TIn, typeHint: Option[String]): AlmValidation[(Option[String], SerializedRepr, Vector[ExtractedBlobReference])]
-  def serializeBlobSeparatingAsync(blobPolicy: BlobSerializationPolicy)(channel: String)(what: TIn, typeHint: Option[String]): AlmFuture[(Option[String], SerializedRepr, Vector[ExtractedBlobReference])]
 }
 
 trait CanSerializeToFixedChannel[-TIn] extends WorksWithSerializedRepresentation {
@@ -27,22 +25,16 @@ trait CanSerializeToFixedChannel[-TIn] extends WorksWithSerializedRepresentation
   def channel: String
   def serialize(what: TIn, typeHint: Option[String]): AlmValidation[(Option[String], SerializedRepr)]
   def serializeAsync(what: TIn, typeHint: Option[String]): AlmFuture[(Option[String], SerializedRepr)]
-  def serializeBlobSeparating(blobPolicy: BlobSerializationPolicy)(what: TIn, typeHint: Option[String]): AlmValidation[(Option[String], SerializedRepr, Vector[ExtractedBlobReference])]
-  def serializeBlobSeparatingAsync(blobPolicy: BlobSerializationPolicy)(what: TIn, typeHint: Option[String]): AlmFuture[(Option[String], SerializedRepr, Vector[ExtractedBlobReference])]
 }
 
 trait CanDeserialize[+TOut] extends WorksWithSerializedRepresentation {
   def deserialize(channel: String)(what: SerializedRepr, typeHint: Option[String]): AlmValidation[TOut]
   def deserializeAsync(channel: String)(what: SerializedRepr, typeHint: Option[String]): AlmFuture[TOut]
-  def deserializeBlobIntegrating(blobPolicy: BlobDeserializationPolicy)(channel: String)(what: SerializedRepr, typeHint: Option[String]): AlmValidation[TOut]
-  def deserializeBlobIntegratingAsync(blobPolicy: BlobDeserializationPolicy)(channel: String)(what: SerializedRepr, typeHint: Option[String]): AlmFuture[TOut]
 }
 
 trait CanDeserializeFromFixedChannel[+TOut] extends WorksWithSerializedRepresentation {
   def deserialize(what: SerializedRepr, typeHint: Option[String]): AlmValidation[TOut]
   def deserializeAsync(what: SerializedRepr, typeHint: Option[String]): AlmFuture[TOut]
-  def deserializeBlobIntegrating(blobPolicy: BlobDeserializationPolicy)(what: SerializedRepr, typeHint: Option[String]): AlmValidation[TOut]
-  def deserializeBlobIntegratingAsync(blobPolicy: BlobDeserializationPolicy)(what: SerializedRepr, typeHint: Option[String]): AlmFuture[TOut]
 }
 
 object CanSerialize {
@@ -53,8 +45,6 @@ object CanSerialize {
         val channel = fixToThisChannel
         def serialize(what: TIn, typeHint: Option[String]) = self.serialize(channel)(what, typeHint)
         def serializeAsync(what: TIn, typeHint: Option[String]) = self.serializeAsync(channel)(what, typeHint)
-        def serializeBlobSeparating(blobPolicy: BlobSerializationPolicy)(what: TIn, typeHint: Option[String]) = self.serializeBlobSeparating(blobPolicy)(channel)(what, typeHint)
-        def serializeBlobSeparatingAsync(blobPolicy: BlobSerializationPolicy)(what: TIn, typeHint: Option[String]) = self.serializeBlobSeparatingAsync(blobPolicy)(channel)(what, typeHint)
       }
   }
 }
@@ -75,12 +65,8 @@ object StringSerializing {
         val channel = fixToThisChannel
         def serialize(what: TIn, typeHint: Option[String]) = self.serialize(channel)(what, typeHint)
         def serializeAsync(what: TIn, typeHint: Option[String]) = self.serializeAsync(channel)(what, typeHint)
-        def serializeBlobSeparating(blobPolicy: BlobSerializationPolicy)(what: TIn, typeHint: Option[String]) = self.serializeBlobSeparating(blobPolicy)(channel)(what, typeHint)
-        def serializeBlobSeparatingAsync(blobPolicy: BlobSerializationPolicy)(what: TIn, typeHint: Option[String]) = self.serializeBlobSeparatingAsync(blobPolicy)(channel)(what, typeHint)
         def deserialize(channel: String)(what: String, typeHint: Option[String]) = self.deserialize(channel)(what, typeHint)
         def deserializeAsync(channel: String)(what: String, typeHint: Option[String]) = self.deserializeAsync(channel)(what, typeHint)
-        def deserializeBlobIntegrating(blobPolicy: BlobDeserializationPolicy)(channel: String)(what: SerializedRepr, typeHint: Option[String]) = self.deserializeBlobIntegrating(blobPolicy)(channel)(what, typeHint)
-        def deserializeBlobIntegratingAsync(blobPolicy: BlobDeserializationPolicy)(channel: String)(what: SerializedRepr, typeHint: Option[String]): AlmFuture[TOut] = self.deserializeBlobIntegratingAsync(blobPolicy)(channel)(what, typeHint)
       }
   }
 }
@@ -92,12 +78,8 @@ object BinarySerializing {
         val channel = fixToThisChannel
         def serialize(what: TIn, typeHint: Option[String]) = self.serialize(channel)(what, typeHint)
         def serializeAsync(what: TIn, typeHint: Option[String]) = self.serializeAsync(channel)(what, typeHint)
-        def serializeBlobSeparating(blobPolicy: BlobSerializationPolicy)(what: TIn, typeHint: Option[String]) = self.serializeBlobSeparating(blobPolicy)(channel)(what, typeHint)
-        def serializeBlobSeparatingAsync(blobPolicy: BlobSerializationPolicy)(what: TIn, typeHint: Option[String]) = self.serializeBlobSeparatingAsync(blobPolicy)(channel)(what, typeHint)
         def deserialize(channel: String)(what: Array[Byte], typeHint: Option[String]) = self.deserialize(channel)(what, typeHint)
         def deserializeAsync(channel: String)(what: Array[Byte], typeHint: Option[String]) = self.deserializeAsync(channel)(what, typeHint)
-        def deserializeBlobIntegrating(blobPolicy: BlobDeserializationPolicy)(channel: String)(what: SerializedRepr, typeHint: Option[String]) = self.deserializeBlobIntegrating(blobPolicy)(channel)(what, typeHint)
-        def deserializeBlobIntegratingAsync(blobPolicy: BlobDeserializationPolicy)(channel: String)(what: SerializedRepr, typeHint: Option[String]): AlmFuture[TOut] = self.deserializeBlobIntegratingAsync(blobPolicy)(channel)(what, typeHint)
       }
   }
 }
