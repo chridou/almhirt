71b12a272a0c1c8a467783556364d7ab14627297
Christian Douven <douven@dial.de>
2013-04-12 11:12:51 +0200

    Bump to 0.0.130

--- project/Build.scala
+++ project/Build.scala
@@ -3,7 +3,7 @@ import Keys._
 
 object BuildSettings {
   val buildOrganization = "org.almhirt"
-  val buildVersion      = "0.0.129"
+  val buildVersion      = "0.0.130"
   val buildScalaVersion = "2.10.1"
 
   val buildSettings = Defaults.defaultSettings ++ Seq (
