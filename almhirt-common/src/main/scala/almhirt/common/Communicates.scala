96709b15554947825468bc86c58dc1be9411bcb3
chridou <ch.douven@web.de>
2013-04-21 20:35:36 +0200

    Splitted trait

--- almhirt-common/src/main/scala/almhirt/common/Communicates.scala
+++ almhirt-common/src/main/scala/almhirt/common/Communicates.scala
@@ -1,9 +1,23 @@
 package almhirt.common
 
+trait HasCommunicationTimeout {
+  def communicationTimeout: scala.concurrent.duration.Duration
+}
+
 trait CommunicatesOneWay[-A] {
   def send(what: A): Unit
 }
 
-trait Communicates[-A, +B] extends CommunicatesOneWay[A]{
+trait CommunicatesTwoWay[-A, +B] {
   def ask(what: A): AlmFuture[B]
+  def askForReply(what: A, replyTo: AlmValidation[B] => Unit): Unit
 }
+
+trait Communicates[-A, +B] extends CommunicatesOneWay[A] with CommunicatesTwoWay[A, B]
+
+//trait DefaultTwoWayCommunicator[-A, +B] extends CommunicatesTwoWay[A, B] {
+//  
+//  override def askForReply(what: A, replyTo: AlmValidation[B] => Unit) {
+//    ask(what).onComplete(replyTo)
+//  }
+//}
