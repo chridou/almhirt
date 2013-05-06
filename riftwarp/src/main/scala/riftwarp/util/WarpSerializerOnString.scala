baccea3bda5319a6447e082fdcdef3ced831aa25
Christian Douven <douven@dial.de>
2013-05-06 12:58:03 +0200

    implemented string serialzer

---
+++ riftwarp/src/main/scala/riftwarp/util/WarpSerializerOnString.scala
@@ -0,0 +1,31 @@
+package riftwarp.util
+
+import java.util.{ UUID => JUUID }
+import scalaz.syntax.validation._
+import almhirt.common._
+import almhirt.serialization._
+import riftwarp._
+import scala.reflect.ClassTag
+
+class WarpSerializerToString[TIn <: AnyRef](riftWarp: RiftWarp)(implicit support: HasExecutionContext) extends CanSerialize[TIn] {
+  type SerializedRepr = String
+
+  private def serializeWithRiftWarp(what: TIn, channel: String, options: Map[String, Any]): AlmValidation[String] =
+    riftWarp.departureTyped[String](channel, what, options)
+
+  override def serialize(channel: String)(what: TIn, options: Map[String, Any] = Map.empty): AlmValidation[(Option[String], SerializedRepr)] =
+    serializeWithRiftWarp(what, channel, options).map(x => (Some(WarpDescriptor(what.getClass()).toParsableString()), x))
+
+  override def serializeAsync(channel: String)(what: TIn, options: Map[String, Any] = Map.empty): AlmFuture[(Option[String], SerializedRepr)] =
+    AlmFuture { serialize(channel)(what, options) }
+}
+
+class RiftDeserializerFromStrings[TOut <: AnyRef](riftWarp: RiftWarp)(implicit support: HasExecutionContext, tag: ClassTag[TOut]) extends CanDeserialize[TOut] {
+  type SerializedRepr = String
+
+  override def deserialize(channel: String)(what: String, options: Map[String, Any] = Map.empty): AlmValidation[TOut] =
+    riftWarp.arrivalTyped[String, TOut](channel, what, options)
+
+  override def deserializeAsync(channel: String)(what: SerializedRepr, options: Map[String, Any] = Map.empty): AlmFuture[TOut] =
+    AlmFuture { deserialize(channel)(what, options) }
+}
