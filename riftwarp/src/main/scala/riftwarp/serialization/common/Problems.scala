fb78a25e1f67d4854986a6aaaae5e4b9c7007c79
Christian Douven <douven@dial.de>
2013-05-07 05:44:56 +0200

    simplified creation of problem packers and unpackers

--- riftwarp/src/main/scala/riftwarp/serialization/common/Problems.scala
+++ riftwarp/src/main/scala/riftwarp/serialization/common/Problems.scala
@@ -1,5 +1,6 @@
 package riftwarp.serialization.common
 
+import scala.reflect.ClassTag
 import scalaz.std._
 import scalaz.syntax.validation._
 import almhirt.common._
@@ -9,10 +10,10 @@ import riftwarp.std.kit._
 object Problems {
   type DefaultProblemCreator[T <: Problem] = ((String, Severity, ProblemCategory, Map[String, Any], Option[ProblemCause])) => T
 
-  def createDefaultPacker[T <: Problem](aWarpDescriptor: WarpDescriptor): WarpPacker[T] with RegisterableWarpPacker = {
+  def createDefaultPacker[T <: Problem](clazz: Class[_]): WarpPacker[T] with RegisterableWarpPacker = {
     new WarpPacker[T] with RegisterableWarpPacker {
-      val warpDescriptor = aWarpDescriptor
-      val alternativeWarpDescriptors = Nil
+      val warpDescriptor = WarpDescriptor(clazz.getSimpleName())
+      val alternativeWarpDescriptors = WarpDescriptor(clazz.getName()) :: Nil
       def pack(what: T)(implicit packers: WarpPackers): AlmValidation[WarpPackage] =
         warpDescriptor ~>
           P("message", what.message) ~>
@@ -23,10 +24,10 @@ object Problems {
     }
   }
 
-  def createDefaultUnpacker[T <: Problem](aWarpDescriptor: WarpDescriptor, creator: DefaultProblemCreator[T]): RegisterableWarpUnpacker[T] = {
+  def createDefaultUnpacker[T <: Problem](clazz: Class[_], creator: DefaultProblemCreator[T]): RegisterableWarpUnpacker[T] = {
     new RegisterableWarpUnpacker[T] {
-      val warpDescriptor = aWarpDescriptor
-      val alternativeWarpDescriptors = Nil
+      val warpDescriptor = WarpDescriptor(clazz.getSimpleName())
+      val alternativeWarpDescriptors = WarpDescriptor(clazz.getName()) :: Nil
       def unpack(from: WarpPackage)(implicit unpackers: WarpUnpackers): AlmValidation[T] = {
         withFastLookUp(from) { lookup =>
           for {
@@ -41,10 +42,10 @@ object Problems {
     }
   }
 
-  def createAggregateProblemPacker(aWarpDescriptor: WarpDescriptor): WarpPacker[AggregateProblem] with RegisterableWarpPacker = {
+  def createAggregateProblemPacker(): WarpPacker[AggregateProblem] with RegisterableWarpPacker = {
     new WarpPacker[AggregateProblem] with RegisterableWarpPacker {
-      val warpDescriptor = aWarpDescriptor
-      val alternativeWarpDescriptors = Nil
+      val warpDescriptor = WarpDescriptor(classOf[AggregateProblem].getSimpleName())
+      val alternativeWarpDescriptors = WarpDescriptor(classOf[AggregateProblem].getName()) :: Nil
       def pack(what: AggregateProblem)(implicit packers: WarpPackers): AlmValidation[WarpPackage] =
         warpDescriptor ~>
           P("message", what.message) ~>
@@ -56,10 +57,10 @@ object Problems {
     }
   }
 
-  def createAggregateProblemUnpacker(aWarpDescriptor: WarpDescriptor): RegisterableWarpUnpacker[AggregateProblem] = {
+  def createAggregateProblemUnpacker(): RegisterableWarpUnpacker[AggregateProblem] = {
     new RegisterableWarpUnpacker[AggregateProblem] {
-      val warpDescriptor = aWarpDescriptor
-      val alternativeWarpDescriptors = Nil
+      val warpDescriptor = WarpDescriptor(classOf[AggregateProblem].getSimpleName())
+      val alternativeWarpDescriptors = WarpDescriptor(classOf[AggregateProblem].getName()) :: Nil
       def unpack(from: WarpPackage)(implicit unpackers: WarpUnpackers): AlmValidation[AggregateProblem] = {
         withFastLookUp(from) { lookup =>
           for {
@@ -75,57 +76,57 @@ object Problems {
     }
   }
 
-  def createDefaultDecomposerAndRecomposer[T <: Problem](warpDescriptor: WarpDescriptor, creator: DefaultProblemCreator[T]) =
-    (createDefaultPacker[T](warpDescriptor), createDefaultUnpacker[T](warpDescriptor, creator))
+  def createDefaultDecomposerAndRecomposer[T <: Problem](creator: DefaultProblemCreator[T])(implicit tag: ClassTag[T]) =
+    (createDefaultPacker[T](tag.runtimeClass), createDefaultUnpacker[T](tag.runtimeClass, creator))
 
-  def createAndRegisterDefaultPackerAndUnpacker[T <: Problem](packers: WarpPackers, unpackers: WarpUnpackers)(warpDescriptor: WarpDescriptor, creator: DefaultProblemCreator[T]) {
-    val (packer, unpacker) = createDefaultDecomposerAndRecomposer(warpDescriptor, creator)
+  def createAndRegisterDefaultPackerAndUnpacker[T <: Problem : ClassTag](packers: WarpPackers, unpackers: WarpUnpackers)(creator: DefaultProblemCreator[T]) {
+    val (packer, unpacker) = createDefaultDecomposerAndRecomposer(creator)
     packers.addTyped[T](packer)
     unpackers.addTyped[T](unpacker)
   }
 
   def createAndRegisterAllDefaultDecomposersAndRecomposers(packers: WarpPackers, unpackers: WarpUnpackers) {
-    createAndRegisterDefaultPackerAndUnpacker[Problem](packers, unpackers)(WarpDescriptor(classOf[Problem].getName), UnspecifiedProblem.tupled)
-    createAndRegisterDefaultPackerAndUnpacker[UnspecifiedProblem](packers, unpackers)(WarpDescriptor(classOf[UnspecifiedProblem].getName), UnspecifiedProblem.tupled)
-    createAndRegisterDefaultPackerAndUnpacker[ExceptionCaughtProblem](packers, unpackers)(WarpDescriptor(classOf[ExceptionCaughtProblem].getName), ExceptionCaughtProblem.tupled)
-    createAndRegisterDefaultPackerAndUnpacker[RegistrationProblem](packers, unpackers)(WarpDescriptor(classOf[RegistrationProblem].getName), RegistrationProblem.tupled)
-    createAndRegisterDefaultPackerAndUnpacker[ServiceNotFoundProblem](packers, unpackers)(WarpDescriptor(classOf[ServiceNotFoundProblem].getName), ServiceNotFoundProblem.tupled)
-    createAndRegisterDefaultPackerAndUnpacker[NoConnectionProblem](packers, unpackers)(WarpDescriptor(classOf[NoConnectionProblem].getName), NoConnectionProblem.tupled)
-    createAndRegisterDefaultPackerAndUnpacker[OperationTimedOutProblem](packers, unpackers)(WarpDescriptor(classOf[OperationTimedOutProblem].getName), OperationTimedOutProblem.tupled)
-    createAndRegisterDefaultPackerAndUnpacker[OperationAbortedProblem](packers, unpackers)(WarpDescriptor(classOf[OperationAbortedProblem].getName), OperationAbortedProblem.tupled)
-    createAndRegisterDefaultPackerAndUnpacker[IllegalOperationProblem](packers, unpackers)(WarpDescriptor(classOf[IllegalOperationProblem].getName), IllegalOperationProblem.tupled)
-    createAndRegisterDefaultPackerAndUnpacker[OperationNotSupportedProblem](packers, unpackers)(WarpDescriptor(classOf[OperationNotSupportedProblem].getName), OperationNotSupportedProblem.tupled)
-    createAndRegisterDefaultPackerAndUnpacker[ArgumentProblem](packers, unpackers)(WarpDescriptor(classOf[ArgumentProblem].getName), ArgumentProblem.tupled)
-    createAndRegisterDefaultPackerAndUnpacker[EmptyCollectionProblem](packers, unpackers)(WarpDescriptor(classOf[EmptyCollectionProblem].getName), EmptyCollectionProblem.tupled)
-    createAndRegisterDefaultPackerAndUnpacker[InvalidCastProblem](packers, unpackers)(WarpDescriptor(classOf[InvalidCastProblem].getName), InvalidCastProblem.tupled)
-    createAndRegisterDefaultPackerAndUnpacker[PersistenceProblem](packers, unpackers)(WarpDescriptor(classOf[PersistenceProblem].getName), PersistenceProblem.tupled)
-    createAndRegisterDefaultPackerAndUnpacker[NotSupportedProblem](packers, unpackers)(WarpDescriptor(classOf[NotSupportedProblem].getName), NotSupportedProblem.tupled)
-    createAndRegisterDefaultPackerAndUnpacker[MappingProblem](packers, unpackers)(WarpDescriptor(classOf[MappingProblem].getName), MappingProblem.tupled)
-    createAndRegisterDefaultPackerAndUnpacker[MappingProblem](packers, unpackers)(WarpDescriptor(classOf[MappingProblem].getName), MappingProblem.tupled)
-    createAndRegisterDefaultPackerAndUnpacker[SerializationProblem](packers, unpackers)(WarpDescriptor(classOf[SerializationProblem].getName), SerializationProblem.tupled)
-    createAndRegisterDefaultPackerAndUnpacker[StartupProblem](packers, unpackers)(WarpDescriptor(classOf[StartupProblem].getName), StartupProblem.tupled)
-    createAndRegisterDefaultPackerAndUnpacker[IndexOutOfBoundsProblem](packers, unpackers)(WarpDescriptor(classOf[IndexOutOfBoundsProblem].getName), IndexOutOfBoundsProblem.tupled)
-    createAndRegisterDefaultPackerAndUnpacker[KeyNotFoundProblem](packers, unpackers)(WarpDescriptor(classOf[KeyNotFoundProblem].getName), KeyNotFoundProblem.tupled)
-    createAndRegisterDefaultPackerAndUnpacker[ConstraintViolatedProblem](packers, unpackers)(WarpDescriptor(classOf[ConstraintViolatedProblem].getName), ConstraintViolatedProblem.tupled)
-    createAndRegisterDefaultPackerAndUnpacker[ParsingProblem](packers, unpackers)(WarpDescriptor(classOf[ParsingProblem].getName), ParsingProblem.tupled)
-    createAndRegisterDefaultPackerAndUnpacker[BadDataProblem](packers, unpackers)(WarpDescriptor(classOf[BadDataProblem].getName), BadDataProblem.tupled)
-    createAndRegisterDefaultPackerAndUnpacker[CollisionProblem](packers, unpackers)(WarpDescriptor(classOf[CollisionProblem].getName), CollisionProblem.tupled)
-    createAndRegisterDefaultPackerAndUnpacker[NotAuthorizedProblem](packers, unpackers)(WarpDescriptor(classOf[NotAuthorizedProblem].getName), NotAuthorizedProblem.tupled)
-    createAndRegisterDefaultPackerAndUnpacker[NotAuthenticatedProblem](packers, unpackers)(WarpDescriptor(classOf[NotAuthenticatedProblem].getName), NotAuthenticatedProblem.tupled)
-    createAndRegisterDefaultPackerAndUnpacker[AlreadyExistsProblem](packers, unpackers)(WarpDescriptor(classOf[AlreadyExistsProblem].getName), AlreadyExistsProblem.tupled)
-    createAndRegisterDefaultPackerAndUnpacker[OperationCancelledProblem](packers, unpackers)(WarpDescriptor(classOf[OperationCancelledProblem].getName), OperationCancelledProblem.tupled)
-    createAndRegisterDefaultPackerAndUnpacker[BusinessRuleViolatedProblem](packers, unpackers)(WarpDescriptor(classOf[BusinessRuleViolatedProblem].getName), BusinessRuleViolatedProblem.tupled)
-    createAndRegisterDefaultPackerAndUnpacker[LocaleNotSupportedProblem](packers, unpackers)(WarpDescriptor(classOf[LocaleNotSupportedProblem].getName), LocaleNotSupportedProblem.tupled)
-    createAndRegisterDefaultPackerAndUnpacker[NoSuchElementProblem](packers, unpackers)(WarpDescriptor(classOf[NoSuchElementProblem].getName), NoSuchElementProblem.tupled)
+    createAndRegisterDefaultPackerAndUnpacker[Problem](packers, unpackers)(UnspecifiedProblem.tupled)
+    createAndRegisterDefaultPackerAndUnpacker[UnspecifiedProblem](packers, unpackers)(UnspecifiedProblem.tupled)
+    createAndRegisterDefaultPackerAndUnpacker[ExceptionCaughtProblem](packers, unpackers)(ExceptionCaughtProblem.tupled)
+    createAndRegisterDefaultPackerAndUnpacker[RegistrationProblem](packers, unpackers)(RegistrationProblem.tupled)
+    createAndRegisterDefaultPackerAndUnpacker[ServiceNotFoundProblem](packers, unpackers)(ServiceNotFoundProblem.tupled)
+    createAndRegisterDefaultPackerAndUnpacker[NoConnectionProblem](packers, unpackers)(NoConnectionProblem.tupled)
+    createAndRegisterDefaultPackerAndUnpacker[OperationTimedOutProblem](packers, unpackers)(OperationTimedOutProblem.tupled)
+    createAndRegisterDefaultPackerAndUnpacker[OperationAbortedProblem](packers, unpackers)(OperationAbortedProblem.tupled)
+    createAndRegisterDefaultPackerAndUnpacker[IllegalOperationProblem](packers, unpackers)(IllegalOperationProblem.tupled)
+    createAndRegisterDefaultPackerAndUnpacker[OperationNotSupportedProblem](packers, unpackers)(OperationNotSupportedProblem.tupled)
+    createAndRegisterDefaultPackerAndUnpacker[ArgumentProblem](packers, unpackers)(ArgumentProblem.tupled)
+    createAndRegisterDefaultPackerAndUnpacker[EmptyCollectionProblem](packers, unpackers)(EmptyCollectionProblem.tupled)
+    createAndRegisterDefaultPackerAndUnpacker[InvalidCastProblem](packers, unpackers)(InvalidCastProblem.tupled)
+    createAndRegisterDefaultPackerAndUnpacker[PersistenceProblem](packers, unpackers)(PersistenceProblem.tupled)
+    createAndRegisterDefaultPackerAndUnpacker[NotSupportedProblem](packers, unpackers)(NotSupportedProblem.tupled)
+    createAndRegisterDefaultPackerAndUnpacker[MappingProblem](packers, unpackers)(MappingProblem.tupled)
+    createAndRegisterDefaultPackerAndUnpacker[MappingProblem](packers, unpackers)(MappingProblem.tupled)
+    createAndRegisterDefaultPackerAndUnpacker[SerializationProblem](packers, unpackers)(SerializationProblem.tupled)
+    createAndRegisterDefaultPackerAndUnpacker[StartupProblem](packers, unpackers)(StartupProblem.tupled)
+    createAndRegisterDefaultPackerAndUnpacker[IndexOutOfBoundsProblem](packers, unpackers)(IndexOutOfBoundsProblem.tupled)
+    createAndRegisterDefaultPackerAndUnpacker[KeyNotFoundProblem](packers, unpackers)(KeyNotFoundProblem.tupled)
+    createAndRegisterDefaultPackerAndUnpacker[ConstraintViolatedProblem](packers, unpackers)(ConstraintViolatedProblem.tupled)
+    createAndRegisterDefaultPackerAndUnpacker[ParsingProblem](packers, unpackers)(ParsingProblem.tupled)
+    createAndRegisterDefaultPackerAndUnpacker[BadDataProblem](packers, unpackers)(BadDataProblem.tupled)
+    createAndRegisterDefaultPackerAndUnpacker[CollisionProblem](packers, unpackers)(CollisionProblem.tupled)
+    createAndRegisterDefaultPackerAndUnpacker[NotAuthorizedProblem](packers, unpackers)(NotAuthorizedProblem.tupled)
+    createAndRegisterDefaultPackerAndUnpacker[NotAuthenticatedProblem](packers, unpackers)(NotAuthenticatedProblem.tupled)
+    createAndRegisterDefaultPackerAndUnpacker[AlreadyExistsProblem](packers, unpackers)(AlreadyExistsProblem.tupled)
+    createAndRegisterDefaultPackerAndUnpacker[OperationCancelledProblem](packers, unpackers)(OperationCancelledProblem.tupled)
+    createAndRegisterDefaultPackerAndUnpacker[BusinessRuleViolatedProblem](packers, unpackers)(BusinessRuleViolatedProblem.tupled)
+    createAndRegisterDefaultPackerAndUnpacker[LocaleNotSupportedProblem](packers, unpackers)(LocaleNotSupportedProblem.tupled)
+    createAndRegisterDefaultPackerAndUnpacker[NoSuchElementProblem](packers, unpackers)(NoSuchElementProblem.tupled)
 
-    createAndRegisterDefaultPackerAndUnpacker[RiftWarpProblem](packers, unpackers)(WarpDescriptor(classOf[RiftWarpProblem].getName), RiftWarpProblem.tupled)
+    createAndRegisterDefaultPackerAndUnpacker[RiftWarpProblem](packers, unpackers)(RiftWarpProblem.tupled)
   
     packers.addPredicated(x => x.isInstanceOf[Problem], createDefaultPacker[Problem](classOf[Problem]))
   }
 
   def registerAllCommonProblems(packers: WarpPackers, unpackers: WarpUnpackers) {
     createAndRegisterAllDefaultDecomposersAndRecomposers(packers, unpackers)
-    packers.addTyped(createAggregateProblemPacker(WarpDescriptor(classOf[AggregateProblem])))
-    unpackers.addTyped(createAggregateProblemUnpacker(WarpDescriptor(classOf[AggregateProblem])))
+    packers.addTyped(createAggregateProblemPacker())
+    unpackers.addTyped(createAggregateProblemUnpacker())
   }
 }
