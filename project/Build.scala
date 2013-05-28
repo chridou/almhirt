dbcdbb783d21116a1134627ad60543b78fcbdf00
Christian Douven <douven@dial.de>
2013-05-28 10:38:33 +0200

    Push to 0.1.1

--- project/Build.scala
+++ project/Build.scala
@@ -3,7 +3,7 @@ import Keys._
 
 object BuildSettings {
   val buildOrganization = "org.almhirt"
-  val buildVersion      = "0.1.0"
+  val buildVersion      = "0.1.1"
   val buildScalaVersion = "2.10.1"
 
   val buildSettings = Defaults.defaultSettings ++ Seq (
