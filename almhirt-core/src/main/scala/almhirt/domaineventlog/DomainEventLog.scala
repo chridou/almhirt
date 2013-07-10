9d645d3fe779625c5dfb080dd84e3a2ee27e34ea
Christian Douven <douven@dial.de>
2013-07-10 14:36:44 +0200

    Basic in memory domain event log

--- almhirt-core/src/main/scala/almhirt/domaineventlog/DomainEventLog.scala
+++ almhirt-core/src/main/scala/almhirt/domaineventlog/DomainEventLog.scala
@@ -3,41 +3,44 @@ package almhirt.domaineventlog
 import java.util.{ UUID => JUUID }
 import almhirt.common._
 import almhirt.domain.DomainEvent
+import akka.actor.Actor
 
-sealed trait FetchedDomainEvents {
-  def index: Int
-  def isLast: Boolean
+object DomainEventLog {
+  sealed trait FetchedDomainEventsPart {
+    def index: Int
+    def isLast: Boolean
+  }
+
+  final case class DomainEventsChunk(
+    /**
+     * Starts with Zero
+     */
+    index: Int,
+    isLast: Boolean,
+    events: Iterable[DomainEvent]) extends FetchedDomainEventsPart
+
+  final case class DomainEventsFailure(
+    /**
+     * Starts with Zero
+     */
+    index: Int,
+    problem: Problem) extends FetchedDomainEventsPart {
+    override def isLast = true
+  }
+
+  final case class LogDomainEvents(events: IndexedSeq[DomainEvent])
+  case object GetAllDomainEvents
+  final case class GetDomainEvent(eventId: JUUID)
+  final case class GetDomainEvents(aggId: JUUID)
+  final case class GetDomainEventsFrom(aggId: JUUID, fromVersion: Long)
+  final case class GetDomainEventsTo(aggId: JUUID, toVersion: Long)
+  final case class GetDomainEventsUntil(aggId: JUUID, untilVersion: Long)
+  final case class GetDomainEventsFromTo(aggId: JUUID, fromVersion: Long, toVersion: Long)
+  final case class GetDomainEventsFromUntil(aggId: JUUID, fromVersion: Long, untilVersion: Long)
+  
+  final case class CommittedDomainEvents(commited: Iterable[DomainEvent], uncommitted: Option[(Iterable[DomainEvent], Problem)])
 }
 
-final case class DomainEventsChunk(
-  /**
-   * Starts with Zero
-   */
-  index: Int,
-  isLast: Boolean,
-  events: Iterable[DomainEvent]) extends FetchedDomainEvents
-  
-final case class DomainEventsFailure(
-  /**
-   * Starts with Zero
-   */
-  index: Int,
-  isLast: Boolean,
-  problem: Problem) extends FetchedDomainEvents
-  
-object DomainEventLog {
-  final case class LogDomainEvents(events: IndexedSeq[DomainEvent])
-  final case class GetDomainEvent(eventId: JUUID)
-  final case class GetAllDomainEvents()
-  final case class GetDomainEvents(aggId: JUUID)
-  final case class GetDomainEventsFrom(aggId: JUUID, from: Long)
-  final case class GetDomainEventsTo(aggId: JUUID, to: Long)
-  final case class GetDomainEventsFromTo(aggId: JUUID, from: Long, to: Long) 
-
-//  sealed trait DomainEventLogRsp extends DomainEventLogMessage
-//  final case class DomainEventsForAggregateRootRsp(aggId: UUID, chunk: DomainEventsChunk, correlationId: Option[UUID]) extends DomainEventLogRsp
-//  final case class AllDomainEventsRsp(chunk: DomainEventsChunk, correlationId: Option[UUID]) extends DomainEventLogRsp
-//  final case class DomainEventByIdRsp(event: AlmValidation[DomainEvent], correlationId: Option[UUID]) extends DomainEventLogRsp
-//  final case class LoggedDomainEventsRsp(committedEvents: IndexedSeq[DomainEvent], uncommittedEvents: Option[(Problem, IndexedSeq[DomainEvent])], correlationId: Option[UUID]) extends DomainEventLogRsp
-
+trait DomainEventLog { self: Actor =>
+  protected def receiveDomainEventLogMsg: Receive
 }
