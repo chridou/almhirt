3a83e5dfde4247ac7b51003a90ee6090e37adf47
Christian Douven <douven@dial.de>
2013-07-18 14:37:12 +0200

    CammandHandlerRegistry(trait)

---
+++ almhirt-core/src/main/scala/almhirt/commanding/CommandHandlerRegistry.scala
@@ -0,0 +1,12 @@
+package almhirt.commanding
+
+import scala.reflect.ClassTag
+import almhirt.common._
+
+trait CommandHandlerRegistry {
+  def addGenericCommandHandler[TCommand <: Command](command: TCommand, handler: GenericCommandHandler { type TCom = TCommand })(implicit tag: ClassTag[TCommand])
+  def addCreatingDomainCommandHandler[TCommand <: DomainCommand](command: TCommand, handler: CreatingDomainCommandHandler { type TCom = TCommand })(implicit tag: ClassTag[TCommand])
+  def addMutatingDomainCommandHandler[TCommand <: DomainCommand](command: TCommand, handler: MutatingDomainCommandHandler { type TCom = TCommand })(implicit tag: ClassTag[TCommand])
+
+  def getCommandHandler(commandType: Class[_]): AlmValidation[CommandHandler]
+}
