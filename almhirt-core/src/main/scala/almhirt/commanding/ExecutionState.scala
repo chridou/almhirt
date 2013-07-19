220c333a5e5409f2de026fa94112d51a3c53ec6a
Christian Douven <douven@dial.de>
2013-07-19 11:52:44 +0200

    Fixed command state event

--- almhirt-core/src/main/scala/almhirt/commanding/ExecutionState.scala
+++ almhirt-core/src/main/scala/almhirt/commanding/ExecutionState.scala
@@ -19,24 +19,25 @@ object ExecutionInProcess {
     ExecutionInProcess(trackId, Map("timestamp" -> ccuad.getDateTime.toString()))
 }
 
-final case class ExecutionSuccessful(trackId: String, message: String, metaData: Map[String, String]) extends ExecutionState
+trait ExecutionFinishedState extends ExecutionState
+
+final case class ExecutionSuccessful(trackId: String, message: String, metaData: Map[String, String]) extends ExecutionFinishedState
 object ExecutionSuccessful {
   def apply(trackId: String, message: String)(implicit ccuad: CanCreateUuidsAndDateTimes): ExecutionSuccessful =
     ExecutionSuccessful(trackId, message, Map("timestamp" -> ccuad.getDateTime.toString()))
 }
 
-case class ExecutionFailed(trackId: String, problem: Problem, metaData: Map[String, String])
+case class ExecutionFailed(trackId: String, problem: Problem, metaData: Map[String, String]) extends ExecutionFinishedState
 object ExecutionFailed {
   def apply(trackId: String, problem: Problem)(implicit ccuad: CanCreateUuidsAndDateTimes): ExecutionFailed =
     ExecutionFailed(trackId, problem, Map("timestamp" -> ccuad.getDateTime.toString()))
 }
 
-final case class ExecutionStateEvent(header: EventHeader, executionState: ExecutionState) extends Event with EventTemplate[ExecutionStateEvent] {
-  override protected def changeHeader(newHeader: EventHeader): ExecutionStateEvent =
-    copy(header = newHeader)
+final case class ExecutionStateChanged(header: EventHeader, executionState: ExecutionState) extends Event {
+   override def changeMetadata(newMetadata: Map[String, String]): ExecutionStateChanged = copy(header = this.header.changeMetadata(newMetadata))
 }
 
-object ExecutionStateEvent {
-  def apply(executionState: ExecutionState)(implicit ccuad: CanCreateUuidsAndDateTimes): ExecutionStateEvent =
-    ExecutionStateEvent(EventHeader(), executionState)
+object ExecutionStateChanged {
+  def apply(executionState: ExecutionState)(implicit ccuad: CanCreateUuidsAndDateTimes): ExecutionStateChanged =
+    ExecutionStateChanged(EventHeader(), executionState)
 }
