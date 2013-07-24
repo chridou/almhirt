a11ccc9310fab5f78d165e7f5202ff00929a8e5e
Christian Douven <douven@dial.de>
2013-07-24 13:25:06 +0200

    Aggregate roots for testing

---
+++ almhirt-testkit/src/main/scala/almhirt/testkit/domain/AR1.scala
@@ -0,0 +1,118 @@
+package almhirt.testkit.domain
+
+import scalaz._, Scalaz._
+import almhirt.common._
+import almhirt.almvalidation.kit._
+import almhirt.commanding._
+import almhirt.domain._
+
+trait AR1Event extends DomainEvent
+
+case class AR1Created(header: DomainEventHeader, newA: String) extends AR1Event with CreatesNewAggregateRootEvent {
+  override def changeMetadata(newMetaData: Map[String, String]): AR1Created = copy(header = this.header.changeMetadata(newMetaData))
+}
+
+case class AR1AChanged(header: DomainEventHeader, newA: String) extends AR1Event {
+  override def changeMetadata(newMetaData: Map[String, String]): AR1AChanged = copy(header = this.header.changeMetadata(newMetaData))
+}
+
+case class AR1BChanged(header: DomainEventHeader, newB: Option[String]) extends AR1Event {
+  override def changeMetadata(newMetaData: Map[String, String]): AR1BChanged = copy(header = this.header.changeMetadata(newMetaData))
+}
+
+case class AR1Deleted(header: DomainEventHeader) extends AR1Event with CreatesNewAggregateRootEvent {
+  override def changeMetadata(newMetaData: Map[String, String]): AR1Deleted = copy(header = this.header.changeMetadata(newMetaData))
+}
+
+case class AR1UnhandableEvent(header: DomainEventHeader) extends AR1Event {
+  override def changeMetadata(newMetaData: Map[String, String]): AR1UnhandableEvent = copy(header = this.header.changeMetadata(newMetaData))
+}
+
+case class AR1(ref: AggregateRootRef, theA: String, theB: Option[String], isDeleted: Boolean)
+  extends AggregateRoot[AR1, AR1Event]
+  with AggregateRootWithHandlers[AR1, AR1Event]
+  with AggregateRootMutationHelpers[AR1, AR1Event] {
+
+  protected override def handlers = {
+    case AR1AChanged(_, newA) => set((ar: AR1, v: String) => ar.copy(theA = v), newA)
+    case AR1BChanged(_, newB) => setL(AR1Lenses.theBL, newB)
+    case AR1Deleted(_) => markDeleted((ar: AR1, b: Boolean) => ar.copy(isDeleted = b))
+  }
+
+  def changeA(newA: String)(implicit ccuad: CanCreateUuidsAndDateTimes): UpdateRecorder[AR1, AR1Event] =
+    newA.notEmptyAlm.fold(
+      fail => reject(fail),
+      succ => update(AR1AChanged(ref, succ)))
+
+  def changeB(newB: Option[String])(implicit ccuad: CanCreateUuidsAndDateTimes): UpdateRecorder[AR1, AR1Event] =
+    newB.map(_.notEmptyAlm).validationOut.fold(
+      fail => reject(fail),
+      succ => update(AR1BChanged(ref, succ)))
+
+  def delete()(implicit ccuad: CanCreateUuidsAndDateTimes): UpdateRecorder[AR1, AR1Event] =
+    update(AR1Deleted(ref))
+
+  protected override def updateRef(newRef: AggregateRootRef): AR1 = this.copy(ref = newRef)
+}
+
+object AR1 extends CanCreateAggragateRoot[AR1, AR1Event] {
+  protected override def creationHandler: PartialFunction[AR1Event, AR1] = {
+    case AR1Created(header, newA) =>
+      AR1(header.aggRef.inc, newA, None, false)
+  }
+
+  def fromScratch(id: java.util.UUID, a: String)(implicit ccuad: CanCreateUuidsAndDateTimes): UpdateRecorder[AR1, AR1Event] =
+    create(AR1Created(DomainEventHeader(id, 0L), a))
+
+  object Commanding {
+    import almhirt.core.Almhirt
+    import scala.concurrent.ExecutionContext
+    def addCommands(registry: CommandHandlerRegistry)(implicit theAlmhirt: Almhirt): CommandHandlerRegistry = {
+      val executionContext = theAlmhirt.futuresExecutor
+
+      val createTestArAdder =
+        CreatingDomainCommandHandler.createRegistryAdderFromSyncFun[AR1ComCreateAR1, AR1Event, AR1](
+          command => AR1.fromScratch(command.targettedAggregateRootId, command.newA).result,
+          executionContext)
+      val changeAAdder =
+        MutatingDomainCommandHandler.createRegistryAdderFromSyncFun[AR1ComChangeA, AR1Event, AR1](
+          (ar, command) => ar.changeA(command.newA).result,
+          executionContext)
+      val changeBAdder =
+        MutatingDomainCommandHandler.createRegistryAdderFromSyncFun[AR1ComChangeB, AR1Event, AR1](
+          (ar, command) => ar.changeB(command.newB).result,
+          executionContext)
+      val deleteTestArAdder =
+        MutatingDomainCommandHandler.createRegistryAdderFromSyncFun[AR1ComDeleteAR1, AR1Event, AR1](
+          (ar, command) => ar.delete.result,
+          executionContext)
+
+      registry nextAdder createTestArAdder nextAdder changeAAdder nextAdder changeBAdder nextAdder deleteTestArAdder
+    }
+  }
+}
+
+object AR1Lenses {
+  val theBL: AR1 @> Option[String] = Lens.lensu((a, b) => a.copy(theB = b), _.theB)
+}
+
+trait AR1Command extends DomainCommand
+final case class AR1ComCreateAR1(header: DomainCommandHeader, newA: String) extends AR1Command with CreatingDomainCommand {
+  override def changeMetadata(newMetadata: Map[String, String]): AR1ComCreateAR1 = copy(header = this.header.changeMetadata(newMetadata))
+}
+
+final case class AR1ComChangeA(header: DomainCommandHeader, newA: String) extends AR1Command {
+  override def changeMetadata(newMetadata: Map[String, String]): AR1ComChangeA = copy(header = this.header.changeMetadata(newMetadata))
+}
+
+final case class AR1ComChangeB(header: DomainCommandHeader, newB: Option[String]) extends AR1Command {
+  override def changeMetadata(newMetadata: Map[String, String]): AR1ComChangeB = copy(header = this.header.changeMetadata(newMetadata))
+}
+
+final case class AR1ComDeleteAR1(header: DomainCommandHeader) extends AR1Command {
+  override def changeMetadata(newMetadata: Map[String, String]): AR1ComDeleteAR1 = copy(header = this.header.changeMetadata(newMetadata))
+}
+
+final case class AR1ComUnregisteredCommand(header: DomainCommandHeader) extends AR1Command {
+  override def changeMetadata(newMetadata: Map[String, String]): AR1ComUnregisteredCommand = copy(header = this.header.changeMetadata(newMetadata))
+}
---
+++ almhirt-testkit/src/main/scala/almhirt/testkit/domain/AR2.scala
@@ -0,0 +1,107 @@
+package almhirt.testkit.domain
+
+import scalaz._, Scalaz._
+import almhirt.common._
+import almhirt.almvalidation.kit._
+import almhirt.domain._
+import almhirt.commanding._
+
+trait AR2Event extends DomainEvent
+
+case class AR2Created(header: DomainEventHeader) extends AR2Event with CreatesNewAggregateRootEvent {
+  override def changeMetadata(newMetaData: Map[String, String]): AR2Created = copy(header = this.header.changeMetadata(newMetaData))
+}
+
+case class AR2CChanged(header: DomainEventHeader, newC: Option[Int]) extends AR2Event {
+  override def changeMetadata(newMetaData: Map[String, String]): AR2CChanged = copy(header = this.header.changeMetadata(newMetaData))
+}
+
+case class AR2Deleted(header: DomainEventHeader) extends AR2Event with CreatesNewAggregateRootEvent {
+  override def changeMetadata(newMetaData: Map[String, String]): AR2Deleted = copy(header = this.header.changeMetadata(newMetaData))
+}
+
+case class AR2UnhandableEvent(header: DomainEventHeader) extends AR2Event {
+  override def changeMetadata(newMetaData: Map[String, String]): AR2UnhandableEvent = copy(header = this.header.changeMetadata(newMetaData))
+}
+
+case class AR2(ref: AggregateRootRef, theC: Option[Int], isDeleted: Boolean)
+  extends AggregateRoot[AR2, AR2Event]
+  with AggregateRootWithHandlers[AR2, AR2Event]
+  with AggregateRootMutationHelpers[AR2, AR2Event] {
+
+  protected override def handlers = {
+    case AR2CChanged(_, newC) => set((ar: AR2, v: Option[Int]) => ar.copy(theC = v), newC)
+    case AR2Deleted(_) => markDeleted((ar: AR2, b: Boolean) => ar.copy(isDeleted = b))
+  }
+
+  def setC(newC: Int)(implicit ccuad: CanCreateUuidsAndDateTimes): UpdateRecorder[AR2, AR2Event] =
+    update(AR2CChanged(ref, Some(newC)))
+
+  def unsetC()(implicit ccuad: CanCreateUuidsAndDateTimes): UpdateRecorder[AR2, AR2Event] =
+    update(AR2CChanged(ref, None))
+
+  def delete()(implicit ccuad: CanCreateUuidsAndDateTimes): UpdateRecorder[AR2, AR2Event] =
+    update(AR2Deleted(ref))
+
+  protected override def updateRef(newRef: AggregateRootRef): AR2 = this.copy(ref = newRef)
+}
+
+object AR2 extends CanCreateAggragateRoot[AR2, AR2Event] {
+  protected override def creationHandler: PartialFunction[AR2Event, AR2] = {
+    case AR2Created(header) =>
+      AR2(header.aggRef.inc, None, false)
+  }
+
+  def fromScratch(id: java.util.UUID)(implicit ccuad: CanCreateUuidsAndDateTimes): UpdateRecorder[AR2, AR2Event] =
+    create(AR2Created(DomainEventHeader(id, 0L)))
+
+  object Handlers {
+    import almhirt.core.Almhirt
+    import scala.concurrent.ExecutionContext
+    def addCommands(registry: CommandHandlerRegistry)(implicit theAlmhirt: Almhirt): CommandHandlerRegistry = {
+      val executionContext = theAlmhirt.futuresExecutor
+      val createAdder =
+        CreatingDomainCommandHandler.createRegistryAdderFromSyncFun[CreateAnotherTestAr, AR2Event, AR2](
+          command => AR2.fromScratch(command.targettedAggregateRootId).result,
+          executionContext)
+      val setCAdder =
+        MutatingDomainCommandHandler.createRegistryAdderFromSyncFun[SetC, AR2Event, AR2](
+          (ar, command) => ar.setC(command.newC).result,
+          executionContext)
+      val unssetCAdder =
+        MutatingDomainCommandHandler.createRegistryAdderFromSyncFun[UnsetC, AR2Event, AR2](
+          (ar, command) => ar.unsetC.result,
+          executionContext)
+      val deleteAdder =
+        MutatingDomainCommandHandler.createRegistryAdderFromSyncFun[DeleteAnotherTestAr, AR2Event, AR2](
+          (ar, command) => ar.delete.result,
+          executionContext)
+
+      registry nextAdder createAdder nextAdder setCAdder nextAdder unssetCAdder nextAdder deleteAdder
+    }
+  }
+}
+
+object AR2Lenses {
+  val theCL: AR2 @> Option[Int] = Lens.lensu((a, b) => a.copy(theC = b), _.theC)
+}
+
+trait AR2Command extends DomainCommand
+
+final case class CreateAnotherTestAr(header: DomainCommandHeader, newA: String) extends AR2Command with CreatingDomainCommand {
+  override def changeMetadata(newMetadata: Map[String, String]): CreateAnotherTestAr = copy(header = this.header.changeMetadata(newMetadata))
+}
+
+final case class SetC(header: DomainCommandHeader, newC: Int) extends AR2Command {
+  override def changeMetadata(newMetadata: Map[String, String]): SetC = copy(header = this.header.changeMetadata(newMetadata))
+}
+
+final case class UnsetC(header: DomainCommandHeader) extends AR2Command {
+  override def changeMetadata(newMetadata: Map[String, String]): UnsetC = copy(header = this.header.changeMetadata(newMetadata))
+}
+
+final case class DeleteAnotherTestAr(header: DomainCommandHeader) extends AR2Command {
+  override def changeMetadata(newMetadata: Map[String, String]): DeleteAnotherTestAr = copy(header = this.header.changeMetadata(newMetadata))
+}
+
+
