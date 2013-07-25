5e0718f1845946d74d959e2bd28c0ebdf945b222
Christian Douven <douven@dial.de>
2013-07-25 16:13:29 +0200

    use local date time since it is always utc

--- ext/almhirt-ext-core-slick/src/main/scala/almhirt/ext/core/slick/TypeMappers.scala
+++ ext/almhirt-ext-core-slick/src/main/scala/almhirt/ext/core/slick/TypeMappers.scala
@@ -1,7 +1,7 @@
 package almhirt.ext.core.slick
 
 import scalaz.syntax.validation._
-import org.joda.time.{DateTime, DateTimeZone}
+import org.joda.time.{LocalDateTime}
 import almhirt.common._
 import almhirt.almvalidation.kit._
 import almhirt.ext.core.slick.shared.Profile
@@ -10,13 +10,13 @@ import java.sql.Timestamp
 trait SlickTypeMappers { this: Profile =>
   import profile.simple._
   import java.sql.Timestamp
-  implicit val JodaTimeToSqlTimestampMapper: TypeMapper[DateTime] =
-    MappedTypeMapper.base[DateTime, Timestamp](
-      dateTime => new Timestamp(dateTime.getMillis),
-      timestamp => new DateTime(timestamp.getTime, DateTimeZone.UTC))
+  implicit val JodaTimeToSqlTimestampMapper: TypeMapper[LocalDateTime] =
+    MappedTypeMapper.base[LocalDateTime, Timestamp](
+      dateTime => new Timestamp(dateTime.toDateTime().getMillis()),
+      timestamp => new LocalDateTime(timestamp.getTime))
 }
 
 object TypeConversion {
-  def dateTimeToTimeStamp(dateTime: DateTime): Timestamp = new Timestamp(dateTime.getMillis)
-  def timestampToUtcDateTime(timestamp: Timestamp): DateTime = new DateTime(timestamp.getTime, DateTimeZone.UTC)
+  def dateTimeToTimeStamp(dateTime: LocalDateTime): Timestamp = new Timestamp(dateTime.toDateTime().getMillis())
+  def timestampToUtcDateTime(timestamp: Timestamp): LocalDateTime = new LocalDateTime(timestamp.getTime)
 }
