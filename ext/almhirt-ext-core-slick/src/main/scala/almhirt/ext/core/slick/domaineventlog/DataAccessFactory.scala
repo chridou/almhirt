0e9699ce181fd24eff3860df1c03fca9acd1baa6
Christian Douven <douven@dial.de>
2013-07-29 12:24:48 +0200

    Removed unused file

--- ext/almhirt-ext-core-slick/src/main/scala/almhirt/ext/core/slick/domaineventlog/DataAccessFactory.scala
+++
@@ -1,9 +0,0 @@
-package almhirt.ext.core.slick.domaineventlog
-
-import com.typesafe.config.Config
-
-final case class DomainEventLogDataAccessConfig()
-
-object DomainEventLogDataAccessFactoryConfigReader {
-	def create(config: Config)= ??? 
-}
