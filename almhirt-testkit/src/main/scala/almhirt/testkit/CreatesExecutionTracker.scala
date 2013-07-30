44a547d3e4e1cb5956d7c9092539165ab0470fc2
Christian Douven <douven@dial.de>
2013-07-30 10:43:18 +0200

    Trait to create in-memory execution state tracker

--- almhirt-testkit/src/main/scala/almhirt/testkit/CreatesExecutionTracker.scala
+++ almhirt-testkit/src/main/scala/almhirt/testkit/CreatesExecutionTracker.scala
@@ -1,11 +1,29 @@
 package almhirt.testkit
 
-import akka.actor.ActorRef
+import akka.actor._
+import almhirt.core.HasAlmhirt
+import almhirt.components.ExecutionStateTracker
+import almhirt.components.impl.{ ExecutionTrackerTemplate, InMemoryExecutionStateTracker }
+import almhirt.messaging.MessagePublisher
 
 trait CreatesExecutionTracker {
   def createExecutionTracker(testId: Int): (ActorRef, () => Unit)
+  def createExecutionTracker(testId: Int, publishTo: MessagePublisher): (ActorRef, () => Unit)
 }
 
-trait CreatesCreatesInMemoryExecutionTracker extends CreatesExecutionTracker {  self: akka.testkit.TestKit =>
-  
+trait CreatesCreatesInMemoryExecutionTracker extends CreatesExecutionTracker { self: akka.testkit.TestKit with HasAlmhirt =>
+  override def createExecutionTracker(testId: Int): (ActorRef, () => Unit) =
+    createExecutionTracker(testId, theAlmhirt.messageBus)
+
+  override def createExecutionTracker(testId: Int, messagePublisher: MessagePublisher): (ActorRef, () => Unit) = {
+    val props = Props(
+      new ExecutionStateTracker with ExecutionTrackerTemplate with InMemoryExecutionStateTracker with Actor with ActorLogging {
+        val publishTo = messagePublisher
+        val canCreateUuidsAndDateTimes = theAlmhirt
+        val executionContext = theAlmhirt.futuresExecutor
+        val secondLevelMaxAskDuration = scala.concurrent.duration.FiniteDuration(3, "s")
+        def receive: Receive = handleTrackingMessage
+      })
+    (system.actorOf(props, "execution_state_tracker_" + testId), () => ())
+  }
 }
