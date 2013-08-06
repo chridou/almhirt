b9ab2311e6a84c8a26aa286a0b569a32bd5427a9
Christian Douven <douven@dial.de>
2013-08-06 09:42:25 +0200

    Serializing

--- ext/almhirt-corex-spray/src/main/scala/almhirt/corex/spray/marshalling/MarshallingFactory.scala
+++ ext/almhirt-corex-spray/src/main/scala/almhirt/corex/spray/marshalling/MarshallingFactory.scala
@@ -4,42 +4,79 @@ import almhirt.common._
 import almhirt.almvalidation.kit._
 import spray.http._
 import spray.httpx.marshalling.Marshaller
-import spray.httpx.unmarshalling.Unmarshaller
+import spray.httpx.unmarshalling._
 
 trait MarshallingFactory[T] {
   import Helper._
   import spray.util._
 
   def marshaller(
-    stringSerializer: almhirt.serialization.StringBasedSerializer[T, T],
-    binarySerializer: almhirt.serialization.BinaryBasedSerializer[T, T],
+    stringSerializer: Option[almhirt.serialization.StringBasedSerializer[T, T]],
+    binarySerializer: Option[almhirt.serialization.BinaryBasedSerializer[T, T]],
     contentTypes: ContentType*): AlmValidation[Marshaller[T]] =
     validateMediaTypes(contentTypes.map(_.mediaType)).map { _ =>
       Marshaller.of[T](contentTypes: _*) { (value, contentType, ctx) =>
         val channel = extractChannel(contentType.mediaType)
         if (contentType.mediaType.binary) {
-          val (res, _) = binarySerializer.serialize(channel)(value).resultOrEscalate
-          ctx.marshalTo(HttpEntity(contentType, res))
+          binarySerializer match {
+            case Some(binSer) =>
+              val (res, _) = binSer.serialize(channel)(value).resultOrEscalate
+              ctx.marshalTo(HttpEntity(contentType, res))
+            case None =>
+              throw new IllegalArgumentException("Binary content is not supported")
+          }
         } else {
-          val (res, _) = stringSerializer.serialize(channel)(value).resultOrEscalate
-          ctx.marshalTo(HttpEntity(contentType, res))
+          stringSerializer match {
+            case Some(strSer) =>
+              val (res, _) = strSer.serialize(channel)(value).resultOrEscalate
+              ctx.marshalTo(HttpEntity(ContentType(contentType.mediaType, HttpCharsets.`UTF-8`), res))
+            case None =>
+              throw new IllegalArgumentException("String content is not supported")
+          }
         }
       }
     }
 
   def unmarshaller(
-    stringSerializer: almhirt.serialization.StringBasedSerializer[T, T],
-    binarySerializer: almhirt.serialization.BinaryBasedSerializer[T, T],
-    contentTypes: ContentType*): AlmValidation[Unmarshaller[T]] =
+    stringSerializer: Option[almhirt.serialization.StringBasedSerializer[T, T]],
+    binarySerializer: Option[almhirt.serialization.BinaryBasedSerializer[T, T]],
+    contentTypes: ContentType*): AlmValidation[Unmarshaller[T]] = {
     validateMediaTypes(contentTypes.map(_.mediaType)).map { _ =>
-      Unmarshaller[T](contentTypes: _*) {
-        case HttpBody(contentType, buffer) =>
-          val channel = extractChannel(contentType.mediaType)
-          if (contentType.mediaType.binary) {
-            binarySerializer.deserialize(channel)(buffer, Map.empty).resultOrEscalate
-          } else {
-            stringSerializer.deserialize(channel)(buffer.asString, Map.empty).resultOrEscalate
+      val supported = contentTypes.map(ct => if(ct.mediaType.binary) ct else ContentType(ct.mediaType, HttpCharsets.`UTF-8`)).toSet
+      new Unmarshaller[T] {
+        override def apply(entity: HttpEntity): Deserialized[T] =
+          entity match {
+            case HttpBody(contentType, buffer) =>
+              if (!supported.contains(contentType)) {
+                val msgSupp = supported.map(_.value).mkString("Expected '", "' or '", "'")
+                Left(UnsupportedContentType(s""""${contentType}" is not supported. $msgSupp"""))
+              }
+              else {
+                val channel = extractChannel(contentType.mediaType)
+                  if (contentType.mediaType.binary) {
+                    binarySerializer match {
+                      case Some(binSer) =>
+                        binSer.deserialize(channel)(buffer, Map.empty).fold(
+                            fail => Left(MalformedContent(fail.message, None)),
+                            succ => Right(succ))
+                      case None =>
+                        Left(UnsupportedContentType(s"""Binary content("${contentType.value}") is not supported"""))
+                    }
+                  } else {
+                    stringSerializer match {
+                      case Some(strSer) =>
+                        strSer.deserialize(channel)(buffer.asString, Map.empty).fold(
+                            fail => Left(MalformedContent(fail.message, None)),
+                            succ => Right(succ))
+                      case None =>
+                        Left(UnsupportedContentType(s"""String content("${contentType.value}") is not supported"""))
+                    }
+                  }
+              }
+            case EmptyEntity =>
+              Left(ContentExpected)
           }
       }
     }
+  }
 }
