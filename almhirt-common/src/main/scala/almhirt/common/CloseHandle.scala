fed326ab4ad91114c8c9e4f63cc3b312c290fffc
Christian Douven <douven@dial.de>
2013-08-07 06:20:56 +0200

    Added "andThen" and made CloseHandle derive from Function0[Unit]

--- almhirt-common/src/main/scala/almhirt/common/CloseHandle.scala
+++ almhirt-common/src/main/scala/almhirt/common/CloseHandle.scala
@@ -1,6 +1,10 @@
 package almhirt.common
 
-trait CloseHandle{
+trait CloseHandle extends Function0[Unit] { self =>
   final def apply() { close() }
   def close()
+  final def andThen(other: CloseHandle): CloseHandle =
+    new CloseHandle {
+      def close() { self.close(); other.close() }
+    }
 }
