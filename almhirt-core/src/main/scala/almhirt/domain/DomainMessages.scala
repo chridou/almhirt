ff63037b99e34699b79afda30c1695f97fa0a039
Christian Douven <douven@dial.de>
2013-08-09 10:49:28 +0200

    Grouped domain messages with traits

--- almhirt-core/src/main/scala/almhirt/domain/DomainMessages.scala
+++ almhirt-core/src/main/scala/almhirt/domain/DomainMessages.scala
@@ -3,12 +3,17 @@ package almhirt.domain
 object DomainMessages {
   sealed trait DomainMessage
   final case class GetAggregateRoot(arId: java.util.UUID) extends DomainMessage
-  final case class RequestedAggregateRoot(ar: IsAggregateRoot) extends DomainMessage
-  final case class AggregateRootNotFound(arId: java.util.UUID) extends DomainMessage
-  final case class AggregateRootFetchFailed(arId: java.util.UUID, problem: almhirt.common.Problem) extends DomainMessage
-  final case class AggregateRootUpdated(newState: IsAggregateRoot) extends DomainMessage
-  final case class AggregateRootUpdateFailed(problem: almhirt.common.Problem) extends DomainMessage
+
+  sealed trait AggregateRootGetResponse extends DomainMessage
+  final case class RequestedAggregateRoot(ar: IsAggregateRoot) extends AggregateRootGetResponse
+  final case class AggregateRootNotFound(arId: java.util.UUID) extends AggregateRootGetResponse
+  final case class AggregateRootFetchFailed(arId: java.util.UUID, problem: almhirt.common.Problem) extends AggregateRootGetResponse
+
   final case class UpdateAggregateRoot(newState: IsAggregateRoot, eventsToNewState: IndexedSeq[DomainEvent]) extends DomainMessage
+  sealed trait AggregateRootUpdateResponse extends DomainMessage
+  final case class AggregateRootUpdated(newState: IsAggregateRoot) extends AggregateRootUpdateResponse
+  final case class AggregateRootUpdateFailed(problem: almhirt.common.Problem) extends AggregateRootUpdateResponse
+
   final case class IncompatibleAggregateRoot(ar: IsAggregateRoot, expected: String) extends DomainMessage
   final case class IncompatibleDomainEvent(expected: String) extends DomainMessage
 
