79cf4ea04fe696c58a4c0c2e2da85905bf975fbe
Christian Douven <douven@dial.de>
2013-08-19 10:53:22 +0200

    bump to 0.5.23

--- project/Build.scala
+++ project/Build.scala
@@ -3,7 +3,7 @@ import Keys._
 
 object BuildSettings {
   val buildOrganization = "org.almhirt"
-  val buildVersion      = "0.5.22"
+  val buildVersion      = "0.5.23"
   val buildScalaVersion = "2.10.2"
 
   val akkaVersion = "2.2.+"
