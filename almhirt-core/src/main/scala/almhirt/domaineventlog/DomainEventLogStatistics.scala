76c6a7c974ae75fbf51c0f200ef74414b343d7e6
Christian Douven <douven@dial.de>
2013-09-12 12:52:28 +0200

    also count how many items have been written within 0ms

--- almhirt-core/src/main/scala/almhirt/domaineventlog/DomainEventLogStatistics.scala
+++ almhirt-core/src/main/scala/almhirt/domaineventlog/DomainEventLogStatistics.scala
@@ -1,27 +1,36 @@
 package almhirt.domaineventlog
 
 case class DomainEventLogWriteStatistics(
-	minMs: Long,
-	maxMs: Long,
-	avgMs: Long,
-	totalMs: Long,
-	count: Long
-) {
+  minMs: Long,
+  maxMs: Long,
+  avgMs: Long,
+  totalMs: Long,
+  count: Long,
+  count0Ms: Long) {
   def add(ms: Long) = {
     val newCount = count + 1L
-    val newTotal = totalMs + ms 
+    val newCount0Ms =
+      if (ms == 0L)
+        count0Ms + 1L
+      else
+        count0Ms
+    val newTotal = totalMs + ms
     val newAvg = newTotal / newCount
-    val newMin = 
-      if(count == 0L)
+    val newMin =
+      if (newCount == 1L)
         Math.min(ms, Long.MaxValue)
       else
         Math.min(ms, minMs)
     val newMax = Math.max(ms, maxMs)
-    DomainEventLogWriteStatistics(newMin, newMax, newAvg, newTotal, newCount)
+    DomainEventLogWriteStatistics(newMin, newMax, newAvg, newTotal, newCount, newCount0Ms)
+  }
+
+  override def toString() = {
+    s"""DomainEventLogWriteStatistics(minMs=$minMs, maxMs=$maxMs, avgMs=$avgMs, totalMs=$totalMs, count=$count, count0Ms=count0Ms)"""
   }
 }
 
 object DomainEventLogWriteStatistics {
   def apply(): DomainEventLogWriteStatistics =
-    DomainEventLogWriteStatistics(0, 0, 0, 0, 0)
+    DomainEventLogWriteStatistics(0L, 0L, 0L, 0L, 0L, 0L)
 }
