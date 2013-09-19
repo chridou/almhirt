59f6608e40b190d2df249c702dc8724103e9ceb8
Christian Douven <douven@dial.de>
2013-09-19 08:45:29 +0200

    bump to 0.5.48

--- project/Build.scala
+++ project/Build.scala
@@ -3,7 +3,7 @@ import Keys._
 
 object BuildSettings {
   val buildOrganization = "org.almhirt"
-  val buildVersion      = "0.5.47"
+  val buildVersion      = "0.5.48"
   val buildScalaVersion = "2.10.2"
 
   val akkaVersion = "2.2.+"
