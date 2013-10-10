cadbe34b4a764db49edb21418d4ddde406873be9
Christian Douven <douven@dial.de>
2013-10-10 10:32:41 +0200

    more valid media types

--- ext/almhirt-corex-spray/src/main/scala/almhirt/corex/spray/marshalling/Helper.scala
+++ ext/almhirt-corex-spray/src/main/scala/almhirt/corex/spray/marshalling/Helper.scala
@@ -9,24 +9,30 @@ private[marshalling] object Helper {
   def extractChannel(mediaType: MediaType): String =
     if (mediaType == MediaTypes.`text/html`)
       "html"
+    else if (mediaType == MediaTypes.`application/json`)
+      "json"
+    else if (mediaType == MediaTypes.`application/xml`)
+      "xml"
     else
       mediaType.value.split('+')(1)
 
   def isValidMediaType(mediaType: MediaType): Boolean =
     if (mediaType == MediaTypes.`text/html`)
       true
+    else if (mediaType == MediaTypes.`application/json`)
+      true
+    else if (mediaType == MediaTypes.`application/xml`)
+      true
     else
       mediaType.value.split('+').size == 2
-      
-      
+
   def validateMediaTypes(mediaTypes: Seq[MediaType]): AlmValidation[Seq[MediaType]] = {
     val res =
       mediaTypes.map(mediaType =>
         if (isValidMediaType(mediaType))
           mediaType.success
         else
-          UnspecifiedProblem(s"""Not a valid media type: "${mediaType.value}".""").failure
-        ).map(_.toAgg).toList
+          UnspecifiedProblem(s"""Not a valid media type: "${mediaType.value}".""").failure).map(_.toAgg).toList
     res.sequence[AlmValidationAP, MediaType]
   }
 }
