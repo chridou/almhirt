6106a4f2f341f89991976c6ef39648680c9f168f
Christian Douven <douven@dial.de>
2013-10-17 17:15:51 +0200

    first implementation of binary reader

--- almhirt-common/src/main/scala/almhirt/io/BinaryReader.scala
+++ almhirt-common/src/main/scala/almhirt/io/BinaryReader.scala
@@ -10,13 +10,26 @@ trait BinaryReader {
   def readLong: Long
   def readFloat: Float
   def readDouble: Double
-  def readBigInt: BigInt
   def readByteArray(size: Int): Array[Byte]
   final def readBytes(size: Int): Array[Byte] = readByteArray(size)
 }
 
 object BinaryReader {
   def apply(bytes: Array[Byte]): BinaryReader = {
-    ???
+    val byteBuffer = java.nio.ByteBuffer.wrap(bytes)
+    new BinaryReader {
+      def readByte: Byte = byteBuffer.get
+      def readUnsignedByte: Int = byteBuffer.get & 0xff
+      def readShort: Short = byteBuffer.getShort
+      def readUnsignedShort: Int = byteBuffer.getShort & 0xffff
+      def readInt: Int = byteBuffer.getInt
+      def readLong: Long = byteBuffer.getLong
+      def readFloat: Float = byteBuffer.getFloat
+      def readDouble: Double = byteBuffer.getDouble
+      def readByteArray(size: Int): Array[Byte] = {
+        val array = Array.ofDim[Byte](size)
+        byteBuffer.get(array)
+        array}
+    }
   }
 }
