243bfd55f40bfe3679a789c1c5b24c337240b4fa
Christian Douven <douven@dial.de>
2013-11-29 11:08:25 +0100

    bump tp 0.5.152

--- project/Build.scala
+++ project/Build.scala
@@ -3,7 +3,7 @@ import Keys._
 
 object BuildSettings {
   val buildOrganization = "org.almhirt"
-  val buildVersion      = "0.5.151"
+  val buildVersion      = "0.5.152"
   val buildScalaVersion = "2.10.3"
 
   val akkaVersion = "2.2.+"
