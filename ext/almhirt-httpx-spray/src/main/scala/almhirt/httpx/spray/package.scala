435b3bcb01bd71b9acce9294b58dfd6ed3300652
Christian Douven <douven@dial.de>
2013-12-17 12:41:05 +0100

    implicit conversion of SEQ[AlmMediaType] to spray media types

--- ext/almhirt-httpx-spray/src/main/scala/almhirt/httpx/spray/package.scala
+++ ext/almhirt-httpx-spray/src/main/scala/almhirt/httpx/spray/package.scala
@@ -8,4 +8,9 @@ package object spray {
   implicit def almMediaType2SprayMediaType(amt: AlmMediaType): MediaType = {
     MediaType.custom(amt.mainType, amt.subTypeValue, amt.compressible, amt.binary, amt.fileExtensions)
   }
+  
+  implicit def almMediaTypes2SprayMediaTypes(amts: Seq[AlmMediaType]): Seq[MediaType] = {
+    amts.map(almMediaType2SprayMediaType(_))
+  }
+  
 }
