9c6ef4ec6b51d8e42e11d3dee4213008cb2c1d16
Christian Douven <douven@dial.de>
2013-12-17 13:31:16 +0100

    More common media types

--- almhirt-common/src/main/scala/almhirt/http/AlmMediaTypes.scala
+++ almhirt-common/src/main/scala/almhirt/http/AlmMediaTypes.scala
@@ -3,48 +3,80 @@ package almhirt.http
 object AlmMediaTypes extends AlmMediaTypesRegistry {
   def registeredJsonStructuredMedia(vendor: MediaTypeVendorPart, content: String): AlmMediaType =
     AlmMediaType.applicationStructured(vendor, content, "json").registered
-    
+
   def registeredXmlStructuredMedia(vendor: MediaTypeVendorPart, content: String): AlmMediaType =
     AlmMediaType.applicationStructured(vendor, content, "xml").registered
 
   def registeredMessagePackStructuredMedia(vendor: MediaTypeVendorPart, content: String): AlmMediaType =
     AlmMediaType.applicationStructured(vendor, content, "msgpack").registered
-    
-  val `text/html` = 
+
+  val `text/html` =
     AlmMediaType.text("html")
-    	.makeIanaRegistered
-    	.addFileExtensions("htm", "html")
-    	.registered
+      .makeIanaRegistered
+      .addFileExtensions("htm", "html")
+      .registered
 
-  val `text/xml` = 
+  val `text/plain` =
+    AlmMediaType.text("plain")
+      .makeIanaRegistered
+      .addFileExtensions("txt", "text")
+      .registered
+
+  val `text/xml` =
     AlmMediaType.text("xml")
-    	.makeIanaRegistered
-    	.addFileExtensions("xml")
-    	.registered
+      .makeIanaRegistered
+      .addFileExtensions("xml")
+      .registered
 
-  val `text/json` = 
+  val `text/csv` =
+    AlmMediaType.text("csv")
+      .makeIanaRegistered
+      .addFileExtensions("csv")
+      .registered
+
+  val `text/json` =
     AlmMediaType.text("json")
-    	.makeCompressible
-    	.makeIanaRegistered
-    	.addFileExtensions("json")
-    	.registered
+      .makeCompressible
+      .makeIanaRegistered
+      .addFileExtensions("json")
+      .registered
 
-  val `application/xml` = 
+  val `application/xml` =
     AlmMediaType.applicationTextual("xml")
-    	.makeIanaRegistered
-    	.addFileExtensions("xml")
-    	.registered
+      .makeIanaRegistered
+      .addFileExtensions("xml")
+      .registered
 
-  val `application/json` = 
+  val `application/json` =
     AlmMediaType.applicationTextual("json")
-    	.makeIanaRegistered
-    	.addFileExtensions("json")
-    	.registered
-    	
-  val `application/x-msgpack` = 
+      .makeIanaRegistered
+      .addFileExtensions("json")
+      .registered
+
+  val `application/pdf` =
+    AlmMediaType.applicationBinary("pdf")
+      .makeIanaRegistered
+      .addFileExtensions("pdf")
+      .registered
+      
+  val `application/x-msgpack` =
     AlmMediaType.applicationBinary("x-msgpack")
-    	.makeCompressible
-    	.addFileExtensions("msgpack")
-    	.registered
-  
+      .makeCompressible
+      .addFileExtensions("msgpack")
+      .registered
+
+  val `image/jpeg` =
+    AlmMediaType.binaryImage("jpeg")
+      .makeIanaRegistered
+      .makeCompressible
+      .addFileExtensions("jpeg", "jpg")
+      .registered
+
+  val `image/png` =
+    AlmMediaType.binaryImage("png")
+      .makeIanaRegistered
+      .makeCompressible
+      .addFileExtensions("png")
+      .registered
+
 }
