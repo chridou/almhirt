586dd1352981ab1f6337a1ea88a38c95938610b8
Christian Douven <douven@dial.de>
2014-01-20 20:01:00 +0100

    Track the path for error reporting

--- ext/riftwarpx-mongo/src/main/scala/riftwarpx/mongo/FromBsonRematerializer.scala
+++ ext/riftwarpx-mongo/src/main/scala/riftwarpx/mongo/FromBsonRematerializer.scala
@@ -1,6 +1,6 @@
 package riftwarpx.mongo
 
-import org.joda.time.{LocalDateTime, DateTimeZone }
+import org.joda.time.{ LocalDateTime, DateTimeZone }
 import scalaz._
 import scalaz.Scalaz._
 import almhirt.common._
@@ -13,16 +13,16 @@ object FromBsonRematerializer extends Rematerializer[BSONValue] {
   import BsonConverter._
   override val channel = WarpChannels.`rift-bson-reactive-mongo`
   override def rematerialize(what: BSONValue, options: Map[String, Any] = Map.empty): AlmValidation[WarpPackage] =
-    extract(what)
+    extract(what, Nil)
 
-  private def extract(what: BSONValue): AlmValidation[WarpPackage] =
+  private def extract(what: BSONValue, path: List[String]): AlmValidation[WarpPackage] =
     what match {
-      case obj: BSONDocument => extractObject(obj)
-      case arr: BSONArray => extractCollection(arr)
-      case other => extractPrimitive(other)
+      case obj: BSONDocument => extractObject(obj, path)
+      case arr: BSONArray => extractCollection(arr, path)
+      case other => extractPrimitive(other, path)
     }
 
-  private def extractObject(what: BSONDocument): AlmValidation[WarpObject] = {
+  private def extractObject(what: BSONDocument, path: List[String]): AlmValidation[WarpObject] = {
     val elements = scala.collection.mutable.HashMap(what.elements: _*)
     for {
       warpdescriptor <- elements.get(WarpDescriptor.defaultKey) match {
@@ -34,22 +34,22 @@ object FromBsonRematerializer extends Rematerializer[BSONValue] {
         case None =>
           None.success
       }
-      warpElements <- mapWarpElements(elements)
+      warpElements <- mapWarpElements(elements, path)
 
     } yield WarpObject(warpdescriptor, warpElements)
   }
 
-  private def mapWarpElements(bsonElems: scala.collection.mutable.HashMap[String, BSONValue]): AlmValidation[Vector[WarpElement]] =
+  private def mapWarpElements(bsonElems: scala.collection.mutable.HashMap[String, BSONValue], path: List[String]): AlmValidation[Vector[WarpElement]] =
     bsonElems.map(labelAndValue =>
       labelAndValue._2 match {
         case BSONNull => WarpElement(labelAndValue._1, None).success
-        case v => extract(v).map(x => WarpElement(labelAndValue._1, Some(x))).toAgg
+        case v => extract(v, labelAndValue._1 :: path).map(x => WarpElement(labelAndValue._1, Some(x))).toAgg
       }).toVector.sequence
 
-  private def extractCollection(what: BSONArray): AlmValidation[WarpCollection] =
-    what.values.map(item => extract(item).toAgg).sequence.map(x => WarpCollection(x.toVector))
+  private def extractCollection(what: BSONArray, path: List[String]): AlmValidation[WarpCollection] =
+    what.values.map(item => extract(item, path).toAgg).sequence.map(x => WarpCollection(x.toVector))
 
-  private def extractPrimitive(what: BSONValue): AlmValidation[WarpPrimitive] = {
+  private def extractPrimitive(what: BSONValue, path: List[String]): AlmValidation[WarpPrimitive] = {
     what match {
       case BSONBoolean(value) => WarpBoolean(value).success
       case BSONString(value) => WarpString(value).success
@@ -58,10 +58,11 @@ object FromBsonRematerializer extends Rematerializer[BSONValue] {
       case BSONDouble(value) => WarpDouble(value).success
       case BSONTimestamp(value) => WarpLocalDateTime(new LocalDateTime(value, DateTimeZone.UTC)).success
       case BSONBinary(value, Subtype.UuidSubtype) => WarpUuid(BinaryConverter.bytesToUuid(value.readArray(16))).success
-      case BSONBinary(value, st) => 
-        Failure(UnspecifiedProblem(s"""A BSONBinary with subtype ${st.toString()} is not a primitive type."""))
-      case x => 
-        Failure(UnspecifiedProblem(s"""No primitive rematerialization found for "${what.getClass.getName()}""""))
+      case BSONBinary(value, Subtype.OldUuidSubtype) => WarpUuid(BinaryConverter.bytesBigEndianToUuid(value.readArray(16))).success
+      case BSONBinary(value, st) =>
+        Failure(SerializationProblem(s"A BSONBinary with subtype ${st.toString()} is not a primitive type. The path is ${path.reverse.mkString("[", "->", "]")}."))
+      case x =>
+        Failure(SerializationProblem(s"""No primitive rematerialization found for "${what.getClass.getName()}""""))
     }
   }
 }
