b0331e6d3ec77a9c08c03589165414ea22548c9c
Christian Douven <douven@dial.de>
2014-01-27 14:08:39 +0100

    increased version

--- project/Build.scala
+++ project/Build.scala
@@ -3,7 +3,7 @@ import Keys._
 
 object BuildSettings {
   val buildOrganization = "org.almhirt"
-  val buildVersion      = "0.5.189"
+  val buildVersion      = "0.5.190"
   val buildScalaVersion = "2.10.3"
 
   val akkaVersion = "2.2.+"
