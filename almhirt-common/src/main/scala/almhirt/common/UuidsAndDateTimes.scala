d00261d8a1ecd9ef6ea64fc6827b1987d0aa6fbf
Christian Douven <douven@dial.de>
2014-02-06 06:43:59 +0100

    create a nicer unique string, no digits in it

--- almhirt-common/src/main/scala/almhirt/common/UuidsAndDateTimes.scala
+++ almhirt-common/src/main/scala/almhirt/common/UuidsAndDateTimes.scala
@@ -13,16 +13,30 @@ trait CanCreateUuidsAndDateTimes extends CanCreateUuid with CanCreateDateTime
 object CanCreateUuidsAndDateTimes {
   def apply(): CanCreateUuidsAndDateTimes = new CanCreateUuidsAndDateTimes {
     override def getUuid(): java.util.UUID = java.util.UUID.randomUUID()
-    override def getUniqueString(): String = java.util.UUID.randomUUID().toString().filterNot(_ == '-')
+    override def getUniqueString(): String = createUniqueString
     override def getDateTime(): DateTime = new DateTime()
     override def getUtcTimestamp(): LocalDateTime = new LocalDateTime(DateTimeZone.UTC)
   }
 
   def utc(): CanCreateUuidsAndDateTimes = new CanCreateUuidsAndDateTimes {
     override def getUuid(): java.util.UUID = java.util.UUID.randomUUID()
-    override def getUniqueString(): String = java.util.UUID.randomUUID().toString().filterNot(_ == '-')
+    override def getUniqueString(): String = createUniqueString
     override def getDateTime(): DateTime = new DateTime(DateTimeZone.UTC)
     override def getUtcTimestamp(): LocalDateTime = new LocalDateTime(DateTimeZone.UTC)
   }
+
+  def createUniqueString: String =
+    java.util.UUID.randomUUID().toString().view.filterNot(_ == '-').map {
+      case '0' => 'g'
+      case '1' => 'h'
+      case '2' => 'i'
+      case '3' => 'j'
+      case '4' => 'k'
+      case '5' => 'l'
+      case '6' => 'm'
+      case '7' => 'n'
+      case '8' => 'o'
+      case '9' => 'p'
+    }.toString
 
 }
