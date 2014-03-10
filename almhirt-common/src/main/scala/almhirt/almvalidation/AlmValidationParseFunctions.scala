f070b9d38e7ac5c6ef7aeedce76871857a6c080f
douven <douven@dial.de>
2014-03-10 08:15:47 +0100

    Parse many trimmed strings

--- almhirt-common/src/main/scala/almhirt/almvalidation/AlmValidationParseFunctions.scala
+++ almhirt-common/src/main/scala/almhirt/almvalidation/AlmValidationParseFunctions.scala
@@ -187,6 +187,11 @@ trait AlmValidationParseFunctions {
       funs.aggregateProblemsMN[A, Seq, M](parsedItems))
   }
 
+  /** Parses each item of the string toParse separated by sep with parser and applies each String to String.trim */
+  def parseTrimmedToManyAlm[A, M[_] <: Traversable[_]](toParse: String, parse: String => AlmValidation[A], sep: String = ";")(implicit cbf: CanBuildFrom[Seq[A], A, M[A]]): AlmValidation[M[A]] = {
+    parseToManyAlm(toParse, str => parse(str.trim))
+  }
+  
   private def emptyStringIsNone[T](str: String, f: String => AlmValidation[T]) =
     if (str.trim.isEmpty)
       None.success
