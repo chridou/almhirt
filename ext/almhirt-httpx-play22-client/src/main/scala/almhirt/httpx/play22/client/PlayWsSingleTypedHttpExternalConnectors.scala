9ffe5c8fd077bc0a80666c9964add624e521b850
douven <douven@dial.de>
2014-03-18 13:57:25 +0100

    play extensions do not need an actor

--- ext/almhirt-httpx-play22-client/src/main/scala/almhirt/httpx/play22/client/PlayWsSingleTypedHttpExternalConnectors.scala
+++ ext/almhirt-httpx-play22-client/src/main/scala/almhirt/httpx/play22/client/PlayWsSingleTypedHttpExternalConnectors.scala
@@ -7,7 +7,7 @@ import almhirt.serialization.CanDeserializeFromWire
 import almhirt.serialization.CanSerializeToWire
 import almhirt.http.AlmMediaType
 
-abstract class PlayWsSingleTypeHttpPublisher[T](implicit serializer: CanSerializeToWire[T], problemDeserializer: CanDeserializeFromWire[Problem]) extends Actor with ActorLogging with PlayWsHttpExternalConnector with PlayWsRequestsWithEntity with PlayWsHttpExternalPublisher {
+abstract class PlayWsSingleTypeHttpPublisher[T](implicit serializer: CanSerializeToWire[T], problemDeserializer: CanDeserializeFromWire[Problem]) extends PlayWsHttpExternalConnector with PlayWsRequestsWithEntity with PlayWsHttpExternalPublisher {
 
   def acceptAsSuccess: Set[Int]
   def contentMediaType: AlmMediaType
@@ -21,7 +21,7 @@ abstract class PlayWsSingleTypeHttpPublisher[T](implicit serializer: CanSerializ
   }
 }
 
-abstract class PlayWsSingleTypeHttpQuery[U](implicit deserializer: CanDeserializeFromWire[U], problemDeserializer: CanDeserializeFromWire[Problem]) extends Actor with ActorLogging with PlayWsHttpExternalConnector with PlayWsAwaitingEntityResponse with PlayWsHttpExternalQuery {
+abstract class PlayWsSingleTypeHttpQuery[U](implicit deserializer: CanDeserializeFromWire[U], problemDeserializer: CanDeserializeFromWire[Problem]) extends PlayWsHttpExternalConnector with PlayWsAwaitingEntityResponse with PlayWsHttpExternalQuery {
   type ResourceId
 
   def acceptAsSuccess: Set[Int]
@@ -35,7 +35,7 @@ abstract class PlayWsSingleTypeHttpQuery[U](implicit deserializer: CanDeserializ
   }
 }
 
-abstract class PlayWsSingleTypeHttpConversation[T, U](implicit serializer: CanSerializeToWire[T], deserializer: CanDeserializeFromWire[T], problemDeserializer: CanDeserializeFromWire[Problem]) extends Actor with ActorLogging with PlayWsHttpExternalConnector with PlayWsRequestsWithEntity with PlayWsAwaitingEntityResponse with PlayWsHttpExternalConversation {
+abstract class PlayWsSingleTypeHttpConversation[T, U](implicit serializer: CanSerializeToWire[T], deserializer: CanDeserializeFromWire[T], problemDeserializer: CanDeserializeFromWire[Problem]) extends PlayWsHttpExternalConnector with PlayWsRequestsWithEntity with PlayWsAwaitingEntityResponse with PlayWsHttpExternalConversation {
   def acceptAsSuccess: Set[Int]
   def contentMediaType: AlmMediaType
   def acceptMediaTypes: Seq[AlmMediaType]
