ff227e9886ee7f69184ed24ea8b917464dc8b334
douven <douven@dial.de>
2014-04-24 15:31:44 +0200

    Better error message

--- almhirt-common/src/main/scala/almhirt/common/EscalatedProblemException.scala
+++ almhirt-common/src/main/scala/almhirt/common/EscalatedProblemException.scala
@@ -1,7 +1,5 @@
 package almhirt.common
 
-class EscalatedProblemException(val escalatedProblem: Problem) extends Exception(s"""A validation failed and the problem has been escalated: "${escalatedProblem.message}"""") {
-  override def toString(): String = {
-    super.toString + "\nEscalated problem:\n" + escalatedProblem.toString()
-  }
+class EscalatedProblemException(val escalatedProblem: Problem) extends Exception(s"""	|A validation failed and the problem has been escalated:
+																						|${escalatedProblem}""".stripMargin) {
 }
