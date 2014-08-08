87d6199c72d5ddc427b147a65d70fac945116464
douven <douven@dial.de>
2014-08-08 07:54:56 +0200

    better not have event ids ordered...

--- almhirt-common/src/main/scala/almhirt/common/EventId.scala
+++ almhirt-common/src/main/scala/almhirt/common/EventId.scala
@@ -1,5 +1,3 @@
 package almhirt.common
 
-final case class EventId(id: String) extends AnyVal with Ordered[EventId]{
-	def compare(that: EventId) =  this.id.compareTo(that.id) 
-}
+final case class EventId(id: String) extends AnyVal
