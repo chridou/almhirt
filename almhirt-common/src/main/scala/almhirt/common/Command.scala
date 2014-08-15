5f8b898c152b8f18f07aeb8e6de190761f290467
douven <douven@dial.de>
2014-08-15 07:14:20 +0200

    CommandHeader can store metadata

--- almhirt-common/src/main/scala/almhirt/common/Command.scala
+++ almhirt-common/src/main/scala/almhirt/common/Command.scala
@@ -3,13 +3,20 @@ package almhirt.common
 import org.joda.time.LocalDateTime
 import almhirt.aggregates.{ AggregateRootId, AggregateRootVersion }
 
-case class CommandHeader(id: CommandId, timestamp: LocalDateTime)
+case class CommandHeader(id: CommandId, timestamp: LocalDateTime, metadata: Map[String, String])
 
 object CommandHeader {
   def apply()(implicit ccuad: CanCreateUuidsAndDateTimes): CommandHeader =
-    CommandHeader(CommandId(ccuad.getUniqueString), ccuad.getUtcTimestamp)
+    CommandHeader(CommandId(ccuad.getUniqueString), ccuad.getUtcTimestamp, Map.empty)
   def apply(id: CommandId)(implicit ccuad: CanCreateUuidsAndDateTimes): CommandHeader =
-    CommandHeader(id, ccuad.getUtcTimestamp)
+    CommandHeader(id, ccuad.getUtcTimestamp, Map.empty)
+  def apply(id: CommandId, timestamp: LocalDateTime): CommandHeader =
+    CommandHeader(id, timestamp, Map.empty)
+
+  implicit class CommandHeaderOps(self: CommandHeader) {
+    def withMetadata(metadata: Map[String, String]): CommandHeader =
+      self.copy(metadata = metadata)
+  }
 }
 
 trait Command {
