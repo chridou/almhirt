06c79cc51df679c2b5411e30d977778ad2107910
douven <douven@dial.de>
2014-08-18 13:46:14 +0200

    Added an await method

--- almhirt-common/src/main/scala/almhirt/aggregates/AggregateCommandResult.scala
+++ almhirt-common/src/main/scala/almhirt/aggregates/AggregateCommandResult.scala
@@ -6,3 +6,15 @@ trait AggregateCommandResult[T <: AggregateRoot, E <: AggregateEvent]
 
 case class SyncCommandResult[T <: AggregateRoot, E <: AggregateEvent](r: AlmValidation[(AggregateRootLifecycle[T], Seq[E])]) extends AggregateCommandResult[T, E]
 case class AsyncCommandResult[T <: AggregateRoot, E <: AggregateEvent](r: AlmFuture[(AggregateRootLifecycle[T], Seq[E])]) extends AggregateCommandResult[T, E]
+
+object AggregateCommandResult {
+  import scala.concurrent.ExecutionContext
+  import scala.concurrent.duration.FiniteDuration
+  implicit class AggregateCommandResultOps[T <: AggregateRoot, E <: AggregateEvent](self: AggregateCommandResult[T, E]) {
+    def await(atMost: FiniteDuration): AlmValidation[(AggregateRootLifecycle[T], Seq[E])] =
+      self match {
+        case SyncCommandResult(res) => res
+        case AsyncCommandResult(resF) => resF.awaitResult(atMost)
+      }
+  }
+}
