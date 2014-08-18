8a5a169333feb1ebee33fc50351ed887449100f0
douven <douven@dial.de>
2014-08-18 17:17:27 +0200

    Renamed trait

--- almhirt-core/src/main/scala/almhirt/domain/AggregateDroneCommandHandlerAdaptor.scala
+++ almhirt-core/src/main/scala/almhirt/domain/AggregateDroneCommandHandlerAdaptor.scala
@@ -6,7 +6,7 @@ import almhirt.aggregates._
 /** Mix in this trait together with an [[almhirt.aggregates.AggregateRootCommandHandler]] into your
  *  [[AggregateDrone]] to make it use the mixed in [[almhirt.aggregates.AggregateRootCommandHandler]].
  */
-trait AggregateDroneCommandAdaptor[T <: AggregateRoot, E <: AggregateEvent] { self: AggregateDrone[T, E] with AggregateRootCommandHandler[T, E] =>
+trait AggregateDroneCommandHandlerAdaptor[T <: AggregateRoot, E <: AggregateEvent] { self: AggregateDrone[T, E] with AggregateRootCommandHandler[T, E] =>
   final override val handleAggregateCommand: ConfirmationContext[E] ⇒ (AggregateCommand, AggregateRootLifecycle[T]) ⇒ Unit =
     (ctx) => (currentState, nextCommand) => {
       this.handleAggregateCommand(currentState, nextCommand).onComplete {
