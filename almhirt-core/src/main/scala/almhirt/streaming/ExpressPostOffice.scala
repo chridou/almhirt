379c50780a05f1dab337a63ed64a2007eda45cd6
douven <douven@dial.de>
2014-08-26 16:44:48 +0200

    removed some >>postoffice<<< files

--- almhirt-core/src/main/scala/almhirt/streaming/ExpressPostOffice.scala
+++
@@ -1,7 +0,0 @@
-package almhirt.streaming
-
-/** An ExpressPostOffice tries to deliver small packages to a broker. After a package was sent, the sender of the package gets notified. 
- *  The ExpressPostOffice may try to deliver multiple packages concurrently.
- *	Packages might not be sent in the order they are received. After a package has been sent, a DeliveryJobDone is sent.  
- */
-trait ExpressPostOffice[TElement] extends PostOffice[TElement]
