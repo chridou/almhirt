fb5c2bb885b9140c2997e770280405650ee878ce
douven <douven@dial.de>
2014-09-10 14:21:04 +0200

    Added an event log writer

---
+++ almhirt-core/src/main/scala/almhirt/components/EventLogWriter.scala
@@ -0,0 +1,75 @@
+package almhirt.components
+
+import scalaz._, Scalaz._
+import scala.language.postfixOps
+import scala.concurrent.duration._
+import akka.actor._
+import almhirt.common._
+import akka.stream.actor._
+import org.reactivestreams.Subscriber
+
+object EventLogWriter {
+  def props(eventLog: ActorRef, maxWaitForEventLog: FiniteDuration = 1 second, writeAggregateRootEvents: Boolean = false): Props = {
+    Props(new EventLogWriterImpl(eventLog, writeAggregateRootEvents, maxWaitForEventLog))
+  }
+
+  def apply(eventLogWriter: ActorRef): Subscriber[Event] =
+    ActorSubscriber[Event](eventLogWriter)
+}
+
+private[almhirt] class EventLogWriterImpl(val eventLog: ActorRef, writeAggregateRootEvents: Boolean, maxWaitForEventLog: FiniteDuration) extends ActorSubscriber with ActorLogging {
+  import almhirt.eventlog.EventLog
+
+  override val requestStrategy = ZeroRequestStrategy
+
+  private case class PotentialTimeout(eventId: EventId)
+
+  def running(activeEvent: Option[Event]): Receive = {
+    case ActorSubscriberMessage.OnNext(event: Event) =>
+      if (activeEvent.isDefined) {
+        sys.error(s"Only one event may be processed at any time. Currently event with id '${activeEvent.get.eventId.value}' is processed.")
+      }
+
+      if (!event.isInstanceOf[AggregateRootEvent] || writeAggregateRootEvents) {
+        context.system.scheduler.scheduleOnce(maxWaitForEventLog, self, PotentialTimeout(event.eventId))(context.dispatcher)
+        eventLog ! EventLog.LogEvent(event)
+        context.become(running(activeEvent = Some(event)))
+      } else {
+        request(1)
+      }
+
+    case EventLog.EventLogged(id) =>
+      if (activeEvent.map(_.eventId == id) | false) {
+        request(1)
+        context.become(running(activeEvent = None))
+      }
+
+    case EventLog.EventNotLogged(id, problem) =>
+      log.error(s"Could not log event '${id.value}':\n$problem")
+      if (activeEvent.map(_.eventId == id) | false) {
+        request(1)
+        context.become(running(activeEvent = None))
+      }
+
+    case PotentialTimeout(eventId) =>
+      if (activeEvent.map(_.eventId == eventId) | false) {
+        log.warning(s"Writing event '${eventId.value}' timed out. It might have been written or not...")
+        request(1)
+        context.become(running(activeEvent = None))
+      }
+
+    case ActorSubscriberMessage.OnNext(unprocessable) =>
+      if (activeEvent.isDefined) {
+        sys.error(s"Only one event may be processed at any time. Currently event with id '${activeEvent.get.eventId.value}' is processed.")
+      } else {
+        log.warning(s"received unprocessable element $unprocessable")
+        request(1)
+      }
+  }
+
+  override def receive: Receive = running(activeEvent = None)
+
+  override def preStart() {
+    request(1)
+  }
+} 
