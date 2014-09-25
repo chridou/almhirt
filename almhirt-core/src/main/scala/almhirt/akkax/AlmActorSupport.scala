2239eea03d0ba222307860d157d729ed30da2e74
douven <douven@dial.de>
2014-09-25 06:48:24 +0200

    ExceptionCaughtProblem always propagates exeception

--- almhirt-core/src/main/scala/almhirt/akkax/AlmActorSupport.scala
+++ almhirt-core/src/main/scala/almhirt/akkax/AlmActorSupport.scala
@@ -17,14 +17,16 @@ trait AlmActorSupport { me: Actor =>
   def mapRecoverPipeTo[T](what: => AlmFuture[T], map: T => Any, recover: Problem => Any)(receiver: ActorRef)(implicit executionContext: ExecutionContext) {
     what.mapRecoverPipeTo(map, recover)(receiver)
   }
-  
+
   implicit class AlmFuturePipeTo[T](self: AlmFuture[T]) {
     def pipeTo(receiver: ActorRef, unwrapProblem: Boolean = true)(implicit executionContext: ExecutionContext) {
       import almhirt.problem._
       self.onComplete(
         problem =>
           problem match {
-            case IsSingleProblem(ContainsThrowable(throwable)) if unwrapProblem =>
+            case ExceptionCaughtProblem(ContainsThrowable(throwable)) =>
+              receiver ! Status.Failure(throwable)
+            case ContainsThrowable(throwable) if unwrapProblem =>
               receiver ! Status.Failure(throwable)
             case _ =>
               receiver ! Status.Failure(new EscalatedProblemException(problem))
