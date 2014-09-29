d52a25ad6fdcea9127a7182a23e6e54231e86f23
douven <douven@dial.de>
2014-09-29 07:50:06 +0200

    Configurable endpoints

--- ext/almhirt-httpx-spray-client/src/main/scala/almhirt/httpx/spray/client/ElasticSearchEventPublisher.scala
+++ ext/almhirt-httpx-spray-client/src/main/scala/almhirt/httpx/spray/client/ElasticSearchEventPublisher.scala
@@ -5,20 +5,36 @@ import scala.concurrent.duration.FiniteDuration
 import akka.actor._
 import almhirt.common._
 import almhirt.http._
+import almhirt.configuration._
 import spray.http._
 import spray.client.pipelining._
 import akka.stream.actor._
 import org.reactivestreams.Subscriber
+import com.typesafe.config.Config
 
 object ElasticSearchEventPublisher {
-  def props(host: String,
+  def propsRaw(
+    host: String,
     index: String,
     fixedTypeName: Option[String],
     ttl: FiniteDuration)(implicit serializer: HttpSerializer[Event], problemDeserializer: HttpDeserializer[Problem], executionContexts: HasExecutionContexts): Props =
     Props(new ElasticSearchEventPublisherImpl(host, index, fixedTypeName, ttl))
 
+  def props(config: Config, elConfigName: Option[String] = None)(implicit serializer: HttpSerializer[Event], problemDeserializer: HttpDeserializer[Problem], executionContexts: HasExecutionContexts): AlmValidation[Props] = {
+    val path = "almhirt.components.event-publishers.elastic-search-event-publisher" + elConfigName.map("." + _).getOrElse("")
+    for {
+      section <- config.v[com.typesafe.config.Config](path)
+      host <- section.v[String]("host")
+      index <- section.v[String]("index")
+      fixedTypeName <- section.magicOption[String]("index")
+      ttl <- section.v[FiniteDuration]("ttl")
+    } yield propsRaw(host, index, fixedTypeName, ttl)
+  }
+
   def apply(elasticSearchEventPublischer: ActorRef): Subscriber[Event] =
     ActorSubscriber[Event](elasticSearchEventPublischer)
+    
+  val actorname = "elastic-search-event-publisher"
 }
 
 private[almhirt] class ElasticSearchEventPublisherImpl(
--- ext/almhirt-httpx-spray-client/src/main/scala/almhirt/httpx/spray/client/HttpEventPublisher.scala
+++ ext/almhirt-httpx-spray-client/src/main/scala/almhirt/httpx/spray/client/HttpEventPublisher.scala
@@ -5,17 +5,35 @@ import scala.concurrent.duration.FiniteDuration
 import akka.actor._
 import almhirt.common._
 import almhirt.http._
+import almhirt.configuration._
+import almhirt.almvalidation.kit._
 import spray.http._
 import spray.client.pipelining._
-import akka.stream.actor._
 import org.reactivestreams.Subscriber
+import akka.stream.actor._
 import akka.stream.FlowMaterializer
+import com.typesafe.config.Config
 
 object HttpEventPublisher {
-  def props(endpointUri: String,
+  def propsRaw(
+    endpointUri: String,
+    method: HttpMethod,
     contentMediaType: MediaType,
     addEventId: Boolean)(implicit serializer: HttpSerializer[Event], problemDeserializer: HttpDeserializer[Problem], executionContexts: HasExecutionContexts): Props =
-    Props(new HttpEventPublisherImpl(endpointUri, addEventId, contentMediaType))
+    Props(new HttpEventPublisherImpl(endpointUri, addEventId, method, contentMediaType))
+
+  def props(config: Config, configName: Option[String])(implicit serializer: HttpSerializer[Event], problemDeserializer: HttpDeserializer[Problem], executionContexts: HasExecutionContexts): AlmValidation[Props] = {
+    implicit val extr = almhirt.httpx.spray.HttpMethodConfigExtractor
+    val path = "almhirt.components.event-publishers.http-event-publisher" + configName.map("." + _).getOrElse("")
+    for {
+      section <- config.v[com.typesafe.config.Config](path)
+      endpointUri <- section.v[String]("endpoint-uri")
+      method <- section.v[HttpMethod]("method")
+      contentMediaTypeStr <- section.v[String]("content-media-type")
+      mediaType <- inTryCatch { MediaType.custom(contentMediaTypeStr) }
+      addEventId <- section.v[Boolean]("add-event-id")
+    } yield propsRaw(endpointUri, method, mediaType, addEventId)
+  }
 
   def apply(eventPublischer: ActorRef): Subscriber[Event] =
     ActorSubscriber[Event](eventPublischer)
@@ -33,9 +51,9 @@ object HttpEventPublisher {
 private[almhirt] class HttpEventPublisherImpl(
   endpointUri: String,
   addEventId: Boolean,
+  override val method: HttpMethod,
   override val contentMediaType: MediaType)(implicit override val serializer: HttpSerializer[Event], override val problemDeserializer: HttpDeserializer[Problem], executionContexts: HasExecutionContexts) extends ActorConsumerHttpPublisher[Event] {
 
-  override val method: HttpMethod = HttpMethods.PUT
   override val acceptAsSuccess: Set[StatusCode] = Set(StatusCodes.OK, StatusCodes.Accepted)
   override val entityTag = implicitly[ClassTag[Event]]
 
