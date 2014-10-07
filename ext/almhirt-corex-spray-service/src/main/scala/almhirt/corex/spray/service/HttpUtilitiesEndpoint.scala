520fffcc26242d0fc0ba05da6fed1c7bb2f206e4
douven <douven@dial.de>
2014-10-07 15:20:00 +0200

    Configurable flag to allow config exposure

--- ext/almhirt-corex-spray-service/src/main/scala/almhirt/corex/spray/service/HttpUtilitiesEndpoint.scala
+++ ext/almhirt-corex-spray-service/src/main/scala/almhirt/corex/spray/service/HttpUtilitiesEndpoint.scala
@@ -1,42 +1,64 @@
 package almhirt.corex.spray.service
 
 import scala.language.postfixOps
-import spray.routing.Directives
+import scalaz.Validation.FlatMap._
+import akka.actor.Actor
 import almhirt.common._
 import almhirt.httpx.spray.service.AlmHttpEndpoint
-import almhirt.context.HasAlmhirtContext
-import akka.actor.Actor
+import almhirt.context.{ HasAlmhirtContext, AlmhirtContext  }
 import spray.httpx.marshalling.Marshaller
+import spray.http.StatusCodes
+import spray.routing.Directives
 import org.joda.time.LocalDateTime
 import org.joda.time.DateTime
-import almhirt.context.AlmhirtContext
 
 object HttpUtilitiesEndpoint {
-  final case class HttpUtilitiesEndpointMarshallers(
+  final case class HttpUtilitiesEndpointParams(
+    returnConfigEnabled: Boolean,
     localDateTimeMarshaller: Marshaller[LocalDateTime],
     dateTimeMarshaller: Marshaller[DateTime],
     stringMarshaller: Marshaller[String],
     uuidMarshaller: Marshaller[java.util.UUID],
     problemMarshaller: Marshaller[Problem])
 
+  def paramsFactory(implicit ctx: AlmhirtContext): AlmValidation[(Marshaller[LocalDateTime], Marshaller[DateTime], Marshaller[String], Marshaller[java.util.UUID], Marshaller[Problem]) => HttpUtilitiesEndpointParams] = {
+    import com.typesafe.config.Config
+    import almhirt.configuration._
+    import scala.concurrent.duration.FiniteDuration
+    for {
+      section <- ctx.config.v[Config]("almhirt.http.endpoints.utilities-endpoint")
+      returnConfigEnabled <- section.v[Boolean]("return-config-enabled")
+    } yield {
+      (localDateTimeMarshaller: Marshaller[LocalDateTime],
+      dateTimeMarshaller: Marshaller[DateTime],
+      stringMarshaller: Marshaller[String],
+      uuidMarshaller: Marshaller[java.util.UUID],
+      problemMarshaller: Marshaller[Problem]) =>
+        HttpUtilitiesEndpointParams(returnConfigEnabled, localDateTimeMarshaller, dateTimeMarshaller, stringMarshaller, uuidMarshaller, problemMarshaller)
+    }
+  }
+
 }
 
 trait HttpUtilitiesEndpoint extends Directives { me: Actor with AlmHttpEndpoint with HasAlmhirtContext =>
   import HttpUtilitiesEndpoint._
 
-  protected def httpUtilitiesEndpointMarshallers: HttpUtilitiesEndpointMarshallers
+  protected def httpUtilitiesEndpointParams: HttpUtilitiesEndpointParams
 
-  implicit private val problemMarshaller = httpUtilitiesEndpointMarshallers.problemMarshaller
-  implicit private val dateTimeMarshaller = httpUtilitiesEndpointMarshallers.dateTimeMarshaller
-  implicit private val localDateTimeMarshaller = httpUtilitiesEndpointMarshallers.localDateTimeMarshaller
-  implicit private val stringMarshaller = httpUtilitiesEndpointMarshallers.stringMarshaller
-  implicit private val uuidMarshaller = httpUtilitiesEndpointMarshallers.uuidMarshaller
+  implicit private val problemMarshaller = httpUtilitiesEndpointParams.problemMarshaller
+  implicit private val dateTimeMarshaller = httpUtilitiesEndpointParams.dateTimeMarshaller
+  implicit private val localDateTimeMarshaller = httpUtilitiesEndpointParams.localDateTimeMarshaller
+  implicit private val stringMarshaller = httpUtilitiesEndpointParams.stringMarshaller
+  implicit private val uuidMarshaller = httpUtilitiesEndpointParams.uuidMarshaller
 
   val utilitiesTerminator =
     pathPrefix("config") {
       pathEnd {
-        get {
-          complete(almhirtContext.config.root().render())
+        get { ctx =>
+          if (httpUtilitiesEndpointParams.returnConfigEnabled)
+            ctx.complete(almhirtContext.config.root().render())
+          else
+            ctx.complete(StatusCodes.Forbidden, "Sorry...")
         }
       }
     } ~ pathPrefix("date") {
