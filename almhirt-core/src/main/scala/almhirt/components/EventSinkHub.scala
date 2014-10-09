d244577f7a696c2486ee5249a0e9a76a9e2961fe
douven <douven@dial.de>
2014-10-09 07:10:27 +0200

    typo

--- almhirt-core/src/main/scala/almhirt/components/EventSinkHub.scala
+++ almhirt-core/src/main/scala/almhirt/components/EventSinkHub.scala
@@ -90,7 +90,7 @@ private[almhirt] class EventSinksSupervisorImpl(factories: EventSinkHub.EventSin
         case (name, (props, filterOpt)) ⇒
           val actor = context.actorOf(props, name)
           context watch actor
-          log.info(s"""Create subrcriber "$name".""")
+          log.info(s"""Create subscriber "$name".""")
           val subscriber = ActorSubscriber[Event](actor)
           filterOpt match {
             case Some(filter) ⇒
