5e19fd52c6cd66500f175f4fb216e9a0205bfad4
douven <douven@dial.de>
2014-10-15 08:47:02 +0200

    fixed config section and behaviour of ignoring curcuit problems

--- almhirt-core/src/main/scala/almhirt/herder/herdingdogs/FailuresHerdingDog.scala
+++ almhirt-core/src/main/scala/almhirt/herder/herdingdogs/FailuresHerdingDog.scala
@@ -15,16 +15,16 @@ object FailuresHerdingDog {
     val configPath = "almhirt.herder.herding-dogs.failures"
     for {
       section <- ctx.config.v[Config](configPath)
-      ignoreCircuitProblems <- ctx.config.v[Boolean]("ignore-circuit-problems")
-      maxFailuresForSummary <- ctx.config.v[Int]("max-failures-for-summary")
-      unwrapFailures <- ctx.config.v[Boolean]("unwrap-failures")
-    } yield Props(new FailuresHerdingDog(ignoreCircuitProblems, maxFailuresForSummary, unwrapFailures))
+      ignoreConsecutiveCircuitProblems <- section.v[Boolean]("ignore-consecutive-circuit-problems")
+      maxFailuresForSummary <- section.v[Int]("max-failures-for-summary")
+      unwrapFailures <- section.v[Boolean]("unwrap-failures")
+    } yield Props(new FailuresHerdingDog(ignoreConsecutiveCircuitProblems, maxFailuresForSummary, unwrapFailures))
   }
 
   val actorname = "failures-herdingdog"
 }
 
-private[almhirt] class FailuresHerdingDog(ignoreCircuitProblems: Boolean, maxFailuresForSummary: Int, unwrapFailures: Boolean)(implicit override val almhirtContext: AlmhirtContext) extends Actor with HasAlmhirtContext with ActorLogging {
+private[almhirt] class FailuresHerdingDog(ignoreConsecutiveCircuitProblems: Boolean, maxFailuresForSummary: Int, unwrapFailures: Boolean)(implicit override val almhirtContext: AlmhirtContext) extends Actor with HasAlmhirtContext with ActorLogging {
 
   implicit val executor = almhirtContext.futuresContext
 
@@ -32,13 +32,22 @@ private[almhirt] class FailuresHerdingDog(ignoreCircuitProblems: Boolean, maxFai
 
   def receiveRunning: Receive = {
     case HerderMessage.FailureOccured(name, failure, severity, timestamp) =>
-      prepareCause(failure).foreach(p => collectedFailures get name match {
+      val ignore =
+        (for {
+          entry <- collectedFailures.get(name)
+          first <- entry.summaryQueue.headOption
+          ignore <- first match {
+            case CircuitOpenProblem(_) => Some(!ignoreConsecutiveCircuitProblems)
+            case _ => None
+          }
+        } yield ignore).getOrElse(false)
+      prepareCause(failure, ignore).foreach(p => collectedFailures get name match {
         case Some(entry) =>
           collectedFailures + (name -> entry.add(p, severity, timestamp, maxFailuresForSummary))
         case None =>
           collectedFailures + (name -> FailuresEntry().add(p, severity, timestamp, maxFailuresForSummary))
       })
-      
+
     case HerderMessage.ReportFailures =>
       val entries = collectedFailures.toSeq
       sender() ! HerderMessage.ReportedFailures(entries)
@@ -47,7 +56,7 @@ private[almhirt] class FailuresHerdingDog(ignoreCircuitProblems: Boolean, maxFai
       sender() ! HerderMessage.ReportedFailuresFor(name, collectedFailures get name)
   }
 
-  def prepareCause(cause: ProblemCause): Option[ProblemCause] = {
+  def prepareCause(cause: ProblemCause, ignoreCircuitProblems: Boolean): Option[ProblemCause] = {
     val unwrapped = if (unwrapFailures) cause.unwrap() else cause
     unwrapped match {
       case CauseIsProblem(CircuitOpenProblem(_)) if ignoreCircuitProblems => None
