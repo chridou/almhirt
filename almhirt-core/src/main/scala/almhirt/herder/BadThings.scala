460bb5e47129fb69412d519712530d1d74351f9c
chridou <chridou@users.noreply.github.com>
2014-10-16 22:41:35 +0200

    Added container for bad things

--- almhirt-core/src/main/scala/almhirt/herder/BadThings.scala
+++ almhirt-core/src/main/scala/almhirt/herder/BadThings.scala
@@ -6,7 +6,9 @@ import almhirt.collections.CircularBuffer
 
 trait GetsSeverity[T] { def get(from: T): Severity }
 
-class MutableBadThingsStats[T: ClassTag](val maxQueueSize: Int)(implicit getsSeverity: GetsSeverity[T]) {
+final case class BadThings[T](occurencesCount: Int, maxSeverity: Option[Severity], lastOccurences: Vector[T])
+
+class MutableBadThings[T: ClassTag](val maxQueueSize: Int)(implicit getsSeverity: GetsSeverity[T]) {
   private var occurencesCount: Int = 0
   private var maxSeverity: Option[Severity] = None
   private val lastOccurencesQueue = new CircularBuffer[T](maxQueueSize) 
@@ -23,23 +25,27 @@ class MutableBadThingsStats[T: ClassTag](val maxQueueSize: Int)(implicit getsSev
     maxSeverity = None
     lastOccurencesQueue.clear
   }
+  
+  def immutable: BadThings[T] = BadThings(occurencesCount, maxSeverity, lastOccurencesQueue.toVector)
 }
 
-class MutableBadThingsStatsContainer[K, T : GetsSeverity : ClassTag](val maxQueueSize: Int) {
-  private val entries = new scala.collection.mutable.HashMap[K, MutableBadThingsStats[T]]()
+class MutableBadThingsContainer[K, T : GetsSeverity : ClassTag](val maxQueueSize: Int) {
+  private val entries = new scala.collection.mutable.HashMap[K, MutableBadThings[T]]()
   
   def add(key: K, what: T) {
     entries get key match {
       case Some(entry) => 
         entry.add(what)
       case None =>
-        val newEntry = new MutableBadThingsStats[T](maxQueueSize)
+        val newEntry = new MutableBadThings[T](maxQueueSize)
         newEntry.add(what)
         entries.put(key, newEntry)
     }
   }
   
   def get(key: K) = entries get key
+  
+  def all: Vector[(K, BadThings[T])] = entries.map(x => (x._1, x._2.immutable)).toVector
   
   def clear(key: K) { get(key).foreach(_.clear()) }
   
