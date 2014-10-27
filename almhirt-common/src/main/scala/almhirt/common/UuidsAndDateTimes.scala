05a9b7f69143ce5b05414087436c0aad8d6d873e
douven <douven@dial.de>
2014-10-27 07:27:29 +0100

    Timshift DateTime provider

--- almhirt-common/src/main/scala/almhirt/common/UuidsAndDateTimes.scala
+++ almhirt-common/src/main/scala/almhirt/common/UuidsAndDateTimes.scala
@@ -1,9 +1,8 @@
 package almhirt.common
 
 import scalaz._, Scalaz._
-import org.joda.time.DateTime
-import org.joda.time.DateTimeZone
-import org.joda.time.LocalDateTime
+import org.joda.time.{ DateTime, DateTimeZone, LocalDateTime }
+import scala.concurrent.duration.FiniteDuration
 
 trait CanCreateUuid { def getUuid(): java.util.UUID; def getUniqueString(): String }
 
@@ -26,6 +25,15 @@ object CanCreateUuidsAndDateTimes {
     override def getUtcTimestamp(): LocalDateTime = new LocalDateTime(DateTimeZone.UTC)
   }
 
+  def timeShifted(timeShift: FiniteDuration): CanCreateUuidsAndDateTimes = {
+    val timeshiftMillis = timeShift.toMillis.toInt
+    new CanCreateUuidsAndDateTimes {
+      override def getUuid(): java.util.UUID = java.util.UUID.randomUUID()
+      override def getUniqueString(): String = almhirt.converters.MiscConverters.uuidToBase64String(java.util.UUID.randomUUID())
+      override def getDateTime(): DateTime = new DateTime(DateTimeZone.UTC).plusMillis(timeshiftMillis)
+      override def getUtcTimestamp(): LocalDateTime = new LocalDateTime(DateTimeZone.UTC).plusMillis(timeshiftMillis)
+    }
+  }
 
   private val regexStr = """(?:[-\w:@&=+,.!~*'_;]|%\p{XDigit}{2})(?:[-\w:@&=+,.!~*'$_;]|%\p{XDigit}{2})*"""
   private val regex = regexStr.r
