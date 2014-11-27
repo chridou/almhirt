674d641a46c506eebfd5f29211ba22a9bce5b9e9
douven <douven@dial.de>
2014-11-27 11:27:09 +0100

    some more implicits

--- almhirt-i18n/src/main/scala/almhirt/i18n/package.scala
+++ almhirt-i18n/src/main/scala/almhirt/i18n/package.scala
@@ -2,6 +2,7 @@ package almhirt
 
 import scala.language.implicitConversions
 import com.ibm.icu.util._
+import almhirt.i18n.MeasuredLength
 
 /**
  * Stuff for internationalizations.
@@ -12,16 +13,25 @@ package object i18n {
   implicit def uom2IcuMeasurement(self: UnitOfMeasurement): MeasureUnit = self.icu
   implicit def measured2IcuMeasure(self: Measured): Measure = self.icu
 
-  object MeasuredShortImplicits {
-    implicit final class MeasuredShortImplicitsOps(private val d: Double) extends AnyVal {
-      def mm = MeasuredLength(d, UnitsOfMeasurement.Millimeter)
-      def cm = MeasuredLength(d, UnitsOfMeasurement.Centimeter)
-      def m = MeasuredLength(d, UnitsOfMeasurement.Meter)
-      def km = MeasuredLength(d, UnitsOfMeasurement.Kilometer)
+  object MeasuredImplicits {
+    implicit final class MeasuredImplicitsOps(private val d: Double) extends AnyVal {
+      def millimeter = MeasuredLength(d, UnitsOfMeasurement.Millimeter)
+      def centimeter = MeasuredLength(d, UnitsOfMeasurement.Centimeter)
+      def meter = MeasuredLength(d, UnitsOfMeasurement.Meter)
+      def kilometer = MeasuredLength(d, UnitsOfMeasurement.Kilometer)
+      def inch = MeasuredLength(d, UnitsOfMeasurement.Inch)
 
-      def g = MeasuredMass(d, UnitsOfMeasurement.Gram)
-      def kg = MeasuredMass(d, UnitsOfMeasurement.Kilogram)
-      def t = MeasuredMass(d, UnitsOfMeasurement.Ton)
+      def gram = MeasuredMass(d, UnitsOfMeasurement.Gram)
+      def kilogram = MeasuredMass(d, UnitsOfMeasurement.Kilogram)
+      def ton = MeasuredMass(d, UnitsOfMeasurement.Ton)
+    }
+
+    implicit final class MeasuredLengtsOps(private val measured: MeasuredLength) extends AnyVal {
+      def asMillimeter = measured.to(UnitsOfMeasurement.Millimeter)
+      def asCentimeter = measured.to(UnitsOfMeasurement.Centimeter)
+      def asMeter = measured.to(UnitsOfMeasurement.Meter)
+      def asKilometer = measured.to(UnitsOfMeasurement.Kilometer)
+      def asInch = measured.to(UnitsOfMeasurement.Inch)
     }
   }
 }
--- almhirt-i18n/src/test/scala/almhirt/i18n/MeasuredDoubleOpsTests.scala
+++ almhirt-i18n/src/test/scala/almhirt/i18n/MeasuredDoubleOpsTests.scala
@@ -2,17 +2,17 @@ package almhirt.i18n
 
 import scala.language.postfixOps
 
-import MeasuredShortImplicits._
+import MeasuredImplicits._
 import org.scalatest._
 
 class MeasuredDoubleImplicitsTests extends FunSuite with Matchers {
   test("Meter") {
-    val x = 1.0.m
+    val x = 1.0.meter
     x should equal(MeasuredLength(1.0, UnitsOfMeasurement.Meter))
   }
 
   test("xx") {
-    val x = (1.0.m) * 2.0
+    val x = (1.0.meter) * 2.0
     x should equal(MeasuredLength(2.0, UnitsOfMeasurement.Meter))
   }
   
