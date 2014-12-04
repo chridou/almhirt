fa3df0fa27e6382144f8573e42feba949d10a81e
douven <douven@dial.de>
2014-12-04 08:02:38 +0100

    Added a prefixed key that can be made a full key

--- almhirt-i18n/src/main/scala/almhirt/i18n/ResourceKey.scala
+++ almhirt-i18n/src/main/scala/almhirt/i18n/ResourceKey.scala
@@ -1,6 +1,7 @@
 package almhirt.i18n
 
 import almhirt.common._
+import almhirt.almvalidation.kit._
 import com.ibm.icu.util.ULocale
 
 final case class ResourceSection(section: String) {
@@ -14,4 +15,23 @@ final case class ResourceGroup(section: String, group: String) {
 final case class ResourceKey(section: String, group: String, key: String) {
   def lookup(locale: ULocale)(implicit resources: AlmResources): AlmValidation[ResourceValue] = resources.resource(this, locale)
   def find(locale: ULocale)(implicit resources: AlmResources): Option[ResourceValue] = resources.findResource(this, locale)
+}
+
+object ResourceKey {
+  def apply(raw: String): AlmValidation[ResourceKey] =
+    raw.split("\\.") match {
+      case Array(section, group, key) ⇒ scalaz.Success(ResourceKey(section, group, key))
+      case _                          ⇒ scalaz.Failure(ArgumentProblem(s""""$raw" is not a valid resource key."""))
+    }
+
+  def unsafe(raw: String): ResourceKey =
+    ResourceKey(raw).resultOrEscalate
+}
+
+final case class ResourceKeyPrefix(section: String, group: String, prefix: String) {
+  def apply(finalPart: String) = ResourceKey(section, group, s"$prefix$finalPart")
+
+  def appendToPrefix(part: String) = ResourceKeyPrefix(section, group, s"$prefix$part")
+  
+  def prependToPrefix(part: String) = ResourceKeyPrefix(section, group, s"$part$prefix")
 }
