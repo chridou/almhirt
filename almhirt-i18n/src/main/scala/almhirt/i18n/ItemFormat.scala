ba269e037cdc91d5d301784bfa3260d65ad090c3
douven <douven@dial.de>
2014-12-04 09:12:48 +0100

    added a helper method to ItemFormat to work with formatables

--- almhirt-i18n/src/main/scala/almhirt/i18n/ItemFormat.scala
+++ almhirt-i18n/src/main/scala/almhirt/i18n/ItemFormat.scala
@@ -1,8 +1,17 @@
 package almhirt.i18n
 
+import scalaz.Validation.FlatMap._
 import almhirt.common._
 import com.ibm.icu.util.ULocale
 
 trait ItemFormat[T] {
   def prepare(what: T, locale: ULocale, lookUp: ResourceLookup): AlmValidation[CanRenderToString]
+
+  def createKey(what: T): ResourceKey
+
+  protected def withFormatable(what: T, locale: ULocale, lookup: ResourceLookup)(f: (T, Formatable) ⇒ AlmValidation[CanRenderToString]): AlmValidation[CanRenderToString] =
+    for {
+      formatable ← lookup.formatable(createKey(what), locale)
+      renderable ← f(what, formatable)
+    } yield renderable
 }
