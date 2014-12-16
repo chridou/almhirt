c557462f675ec7db17359e44dcaa9c7b98703e8a
douven <douven@dial.de>
2014-12-16 12:23:06 +0100

    removed unused implicits

--- almhirt-i18n/src/main/scala/almhirt/i18n/package.scala
+++ almhirt-i18n/src/main/scala/almhirt/i18n/package.scala
@@ -10,9 +10,6 @@ import almhirt.i18n.MeasuredLength
  * Calculation functions are only to be used for display purposes!
  */
 package object i18n {
-//  implicit def uom2IcuMeasurement(self: UnitOfMeasurement): MeasureUnit = self.icu
-//  implicit def measured2IcuMeasure(self: Measured): Measure = self.icu
-
   implicit object ULocaleMagnet extends LocaleMagnet[ULocale] {
     @inline
     def toULocale(what: ULocale) = what
