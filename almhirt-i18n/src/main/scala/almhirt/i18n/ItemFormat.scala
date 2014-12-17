66606c47f64c6781cb4ec75e4a02b336b246014c
douven <douven@dial.de>
2014-12-17 09:01:54 +0100

    simplified item format trait

--- almhirt-i18n/src/main/scala/almhirt/i18n/ItemFormat.scala
+++ almhirt-i18n/src/main/scala/almhirt/i18n/ItemFormat.scala
@@ -6,12 +6,4 @@ import com.ibm.icu.util.ULocale
 
 trait ItemFormat[T] {
   def appendTo(what: T, locale: ULocale, appendTo: StringBuffer)(implicit lookUp: ResourceLookup): AlmValidation[StringBuffer]
-
-  def createKey(what: T): ResourceKey
-
-  protected def withFormatable(what: T, locale: ULocale, lookup: ResourceLookup)(f: (T, Formatable) ⇒ AlmValidation[StringBuffer]): AlmValidation[StringBuffer] =
-    for {
-      formatable ← lookup.formatable(createKey(what), locale)
-      rendered ← f(what, formatable)
-    } yield rendered
 }
