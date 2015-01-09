131c83d68b6869bbd34be794ea5ffef1da6e2266
douven <douven@dial.de>
2015-01-09 11:16:17 +0100

    made resource keys etc extend function1

--- almhirt-common/src/main/scala/almhirt/common/ResourceKey.scala
+++ almhirt-common/src/main/scala/almhirt/common/ResourceKey.scala
@@ -2,11 +2,13 @@ package almhirt.common
 
 import almhirt.almvalidation.kit._
 
-final case class ResourceSection(section: String) {
+final case class ResourceSection(section: String) extends Function1[String, ResourceGroup] {
+  def apply(group: String) = ResourceGroup(section, group)
   def withGroup(group: String) = ResourceGroup(section, group)
 }
 
-final case class ResourceGroup(section: String, group: String) {
+final case class ResourceGroup(section: String, group: String) extends Function1[String, ResourceKey] {
+  def apply(key: String) = ResourceKey(section, group, key)
   def withKey(key: String) = ResourceKey(section, group, key)
   def withKeyPrefix(prefix: String) = ResourceKeyPrefix(section, group, prefix)
 }
@@ -24,10 +26,10 @@ object ResourceKey {
     ResourceKey(raw).resultOrEscalate
 }
 
-final case class ResourceKeyPrefix(section: String, group: String, prefix: String) {
+final case class ResourceKeyPrefix(section: String, group: String, prefix: String) extends Function1[String, ResourceKey] {
   def apply(finalPart: String) = ResourceKey(section, group, s"$prefix$finalPart")
 
   def append(part: String) = ResourceKeyPrefix(section, group, s"$prefix$part")
-  
+
   def prepend(part: String) = ResourceKeyPrefix(section, group, s"$part$prefix")
 }
