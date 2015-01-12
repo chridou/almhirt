6f82e1a33ef9b835cc7f73f25ae8b8ca2acdad67
douven <douven@dial.de>
2015-01-12 11:53:54 +0100

    display a country in in exploration tests

--- almhirt-i18n/src/test/scala/almhirt/i18n/ULocaleExplorationTests.scala
+++ almhirt-i18n/src/test/scala/almhirt/i18n/ULocaleExplorationTests.scala
@@ -8,12 +8,12 @@ class ULocaleExplorationTests extends FunSuite with Matchers {
     val loc = new ULocale("DE-DE")
     loc.getBaseName should equal("de_DE")
   }
-  
+
   test("the language for DE-DE is Some(de)") {
     val loc = new ULocale("DE-DE")
     loc.language should equal(Some("de"))
   }
-  
+
   test("the country for DE-DE is Some(DE)") {
     val loc = new ULocale("DE-de")
     loc.country should equal(Some("DE"))
@@ -27,5 +27,19 @@ class ULocaleExplorationTests extends FunSuite with Matchers {
   test("the script for sR-LaTn_rS is Some(Latn)") {
     val loc = new ULocale("sR-LaTn_rS")
     loc.script should equal(Some("Latn"))
+  }
+
+  test("the display language for sR-LaTn_rS is") {
+    val loc = new ULocale("sR-LaTn_rS")
+    info(loc.getDisplayName(new ULocale("en-US")))
+    info(loc.getDisplayCountry)
+    info(loc.getDisplayVariant)
+  }
+  
+  test("the display language for en-UK is") {
+    val loc = new ULocale("en-UK")
+    info(loc.getDisplayName(new ULocale("de-DE")))
+    info(loc.getDisplayCountry)
+    info(loc.getDisplayVariant)
   }
 }
