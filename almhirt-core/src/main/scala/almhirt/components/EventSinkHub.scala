1a3b784a2b0f4e03063f7f304763141c4fdb7c21
douven <douven@dial.de>
2015-03-02 09:54:51 +0100

    Adjustments to new akka streams version

--- almhirt-core/src/main/scala/almhirt/components/EventSinkHub.scala
+++ almhirt-core/src/main/scala/almhirt/components/EventSinkHub.scala
@@ -21,7 +21,7 @@ object EventSinkHubMessage {
 
 object EventSinkHub {
   /** [Name, (Props, Option[Filter])] */
-  type EventSinkHubMemberFactories = Map[String, (Props, Option[Flow[Event, Event]])]
+  type EventSinkHubMemberFactories = Map[String, (Props, Option[Flow[Event, Event, Unit]])]
 
   def propsRaw(factories: EventSinkHub.EventSinkHubMemberFactories, buffersize: Option[Int], withBlackHoleIfEmpty: Boolean)(implicit ctx: AlmhirtContext): Props =
     Props(new EventSinksSupervisorImpl(factories, buffersize, withBlackHoleIfEmpty))
--- almhirt-core/src/main/scala/almhirt/streaming/DelegatingSubscriber.scala
+++ almhirt-core/src/main/scala/almhirt/streaming/DelegatingSubscriber.scala
@@ -8,12 +8,20 @@ import almhirt.common._
 
 object DelegatingSubscriber {
   def apply[T](delegateTo: ActorRef): Subscriber[T] =
-    new DelegatingSubscriber[T](delegateTo)
+    new DelegatingSubscriber[T](delegateTo, None)
+
+  def apply[T](delegateTo: ActorRef, onErrorAction: Throwable ⇒ Unit): Subscriber[T] =
+    new DelegatingSubscriber[T](delegateTo, Some(onErrorAction))
+
 }
 
 object DelegatingEventSubscriber {
   def apply[T <: Event](delegateTo: ActorRef): Subscriber[T] =
     DelegatingSubscriber[T](delegateTo)
+
+  def apply[T <: Event](delegateTo: ActorRef, onErrorAction: Throwable ⇒ Unit): Subscriber[T] =
+    DelegatingSubscriber[T](delegateTo, onErrorAction)
+
 }
 
 object DelegatingCommandSubscriber {
@@ -21,12 +29,15 @@ object DelegatingCommandSubscriber {
     DelegatingSubscriber[T](delegateTo)
 }
 
-private[streaming] class DelegatingSubscriber[T](delegateTo: ActorRef) extends Subscriber[T] {
+private[streaming] class DelegatingSubscriber[T](delegateTo: ActorRef, onErrorAction: Option[Throwable ⇒ Unit]) extends Subscriber[T] {
   private[this] var sub: Option[Subscription] = None
 
   private def requestMore() = sub.map(_.request(1))
-  override def onError(cause: Throwable): Unit =
+
+  override def onError(cause: Throwable): Unit = {
+    onErrorAction.foreach(_(cause))
     scala.sys.error(cause.getMessage)
+  }
 
   override def onSubscribe(subscription: Subscription): Unit = {
     sub = Some(subscription)
