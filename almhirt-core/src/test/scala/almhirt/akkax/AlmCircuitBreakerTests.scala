2471ccaa434c5de89ff7a620fc2d37ab4642aa5c
douven <douven@douven-ltp.dialnetz>
2015-04-17 11:15:58 +0200

    Added a test to check whether a circuit breaker is instantiable

--- almhirt-core/src/test/scala/almhirt/akkax/AlmCircuitBreakerTests.scala
+++ almhirt-core/src/test/scala/almhirt/akkax/AlmCircuitBreakerTests.scala
@@ -6,5 +6,6 @@ class AlmCircuitBreakerTests extends FunSuite with Matchers {
 
   test("A CircuitBreaker should be instantiable") {
    val cb = AlmCircuitBreaker(CircuitControlSettings.defaultSettings, null, null)
+   cb should not be(null)
   }
 }
