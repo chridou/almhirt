1efdc0ba67682923a6d602d90b79a9a2ad752b80
chridou <chridou@users.noreply.github.com>
2015-06-26 09:22:43 +0200

    Contract for snapshot repositore

---
+++ almhirt-core/src/main/scala/almhirt/snapshots/SnapshotRepository.scala
@@ -0,0 +1,29 @@
+package almhirt.snapshots
+
+import almhirt.common._
+import almhirt.aggregates._
+
+
+object SnapshotRepository {
+  final case class StoreSnapshot(snapshoot: AggregateRoot)
+  sealed trait StoreSnapshotResponse
+  final case class SnapshotStored(id: AggregateRootId) extends StoreSnapshotResponse
+  final case class StoreSnapshotFailed(id: AggregateRootId, problem: Problem) extends StoreSnapshotResponse
+ 
+  final case class MarkAggregateRootAsDeleted(id: AggregateRootId)
+  sealed trait MarkAggregateRootAsDeletedResponse
+  final case class AggregateRootMarkedAsDeleted(id: AggregateRootId) extends MarkAggregateRootAsDeletedResponse
+  final case class MarkAggregateRootAsDeletedFailed(id: AggregateRootId, problem: Problem) extends MarkAggregateRootAsDeletedResponse
+  
+  final case class DeleteSnapshot(id: AggregateRootId)
+  sealed trait DeleteSnapshotResponse
+  final case class SnapshotDeleted(id: AggregateRootId) extends DeleteSnapshotResponse
+  final case class DeleteSnapshotFailed(id: AggregateRootId, problem: Problem) extends DeleteSnapshotResponse
+  
+  final case class FindSnapshot(id: AggregateRootId)
+  sealed trait FindSnapshotResponse
+  final case class FoundSnapshot(snapshoot: AggregateRoot) extends FindSnapshotResponse
+  final case class SnapshotNotFound(id: AggregateRootId) extends FindSnapshotResponse
+  final case class AggregateRootDeleted(id: AggregateRootId) extends FindSnapshotResponse
+  final case class FindSnapshotFailed(id: AggregateRootId, problem: Problem) extends FindSnapshotResponse
+}
