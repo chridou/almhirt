48083d8cb0e431a4b5d7e7fbaf446aa543a6f1e1
chridou <chridou@users.noreply.github.com>
2015-07-20 13:33:19 +0200

    moved a method to extension methods

--- ext/almhirt-reactivemongox/src/main/scala/almhirt/reactivemongox/MongoConnectionSettings.scala
+++ ext/almhirt-reactivemongox/src/main/scala/almhirt/reactivemongox/MongoConnectionSettings.scala
@@ -2,9 +2,13 @@ package almhirt.reactivemongox
 
 import reactivemongo.api.{ MongoDriver, MongoConnection, MongoConnectionOptions }
 
-final case class MongoConnectionSettings(nodes: List[String], numChannelsPerNode: Int) {
-  def createConnection(drv: MongoDriver): MongoConnection = {
-    val opts = MongoConnectionOptions(nbChannelsPerNode = numChannelsPerNode)
-    drv.connection(nodes = nodes, options = opts)
+final case class MongoConnectionSettings(nodes: List[String], numChannelsPerNode: Int)
+
+object MongoConnectionSettings {
+  implicit class MongoConnectionSettingsOps(val self: MongoConnectionSettings) extends AnyVal {
+    def createConnection(drv: MongoDriver): MongoConnection = {
+      val opts = MongoConnectionOptions(nbChannelsPerNode = self.numChannelsPerNode)
+      drv.connection(nodes = self.nodes, options = opts)
+    }
   }
 }
