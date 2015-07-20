6ac08f4dbe85f5b088c7fa7f886639ec3e3fcaf3
chridou <chridou@users.noreply.github.com>
2015-07-20 13:31:21 +0200

    fixed mongo connectionSettings

--- ext/almhirt-reactivemongox/src/main/scala/almhirt/reactivemongox/MongoConnectionSettings.scala
+++ ext/almhirt-reactivemongox/src/main/scala/almhirt/reactivemongox/MongoConnectionSettings.scala
@@ -1,7 +1,10 @@
 package almhirt.reactivemongox
 
-import reactivemongo.api.{ MongoDriver, MongoConnection }
+import reactivemongo.api.{ MongoDriver, MongoConnection, MongoConnectionOptions }
 
-final case class MongoConnectionSettings(hosts: List[String], numChannelsPerNode: Int) {
-  def createConnection(drv: MongoDriver): MongoConnection = drv.connection(hosts, nbChannelsPerNode = numChannelsPerNode)
+final case class MongoConnectionSettings(nodes: List[String], numChannelsPerNode: Int) {
+  def createConnection(drv: MongoDriver): MongoConnection = {
+    val opts = MongoConnectionOptions(nbChannelsPerNode = numChannelsPerNode)
+    drv.connection(nodes = nodes, options = opts)
+  }
 }
