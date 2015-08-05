dca75e0d9033aa4b7b13231c209b33b32c9407ce
chridou <chridou@users.noreply.github.com>
2015-08-05 14:49:25 +0200

    just put the properties...

--- almhirt-common/src/main/scala/almhirt/security/JsseSslSettings.scala
+++ almhirt-common/src/main/scala/almhirt/security/JsseSslSettings.scala
@@ -9,18 +9,24 @@ final case class JsseSslSettings(
 
 object JsseSslSettings {
   private def launderPath(p: String): String = p.replace("~", System.getProperty("user.home"))
-    
-  
+
   implicit class JSSESettingsOps(val self: JsseSslSettings) extends AnyVal {
     def properties(): java.util.Properties = {
       val props = new java.util.Properties()
       props.setProperty("javax.net.ssl.keyStore", launderPath(self.keyStore))
-      self.keyStorePassword.foreach(v => props.setProperty("javax.net.ssl.keyStorePassword", v))
-      self.trustStore.foreach(v => props.setProperty("javax.net.ssl.trustStore", launderPath(v)))
-      self.trustStorePassword.foreach(v => props.setProperty("javax.net.ssl.trustStorePassword", launderPath(v)))
+      self.keyStorePassword.foreach(v ⇒ props.setProperty("javax.net.ssl.keyStorePassword", v))
+      self.trustStore.foreach(v ⇒ props.setProperty("javax.net.ssl.trustStore", launderPath(v)))
+      self.trustStorePassword.foreach(v ⇒ props.setProperty("javax.net.ssl.trustStorePassword", launderPath(v)))
       props
     }
-    
-    def setSystemProperties(): Unit = System.setProperties(properties())
+
+    def setSystemProperties(): Unit = {
+      val systemProps = System.getProperties
+      systemProps.put("javax.net.ssl.keyStore", launderPath(self.keyStore))
+      self.keyStorePassword.foreach(v ⇒ systemProps.put("javax.net.ssl.keyStorePassword", v))
+      self.trustStore.foreach(v ⇒ systemProps.put("javax.net.ssl.trustStore", launderPath(v)))
+      self.trustStorePassword.foreach(v ⇒ systemProps.put("javax.net.ssl.trustStorePassword", launderPath(v)))
+      System.setProperties(systemProps)
+    }
   }
 }
