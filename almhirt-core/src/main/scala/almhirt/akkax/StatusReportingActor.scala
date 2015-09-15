9fa65fc1e12c4b25ba8561c46ae3291fb7daaa25
chridou <chridou@users.noreply.github.com>
2015-09-15 13:55:54 +0200

    added some building blocks for status reporting

--- almhirt-core/src/main/scala/almhirt/akkax/StatusReportingActor.scala
+++ almhirt-core/src/main/scala/almhirt/akkax/StatusReportingActor.scala
@@ -8,18 +8,54 @@ import almhirt.common._
 import almhirt.almfuture.all._
 import almhirt.herder.HerderMessages.StatusReportMessages
 import almhirt.herder.HerderMessages
+import almhirt.akkax.reporting.StatusReport
 
 trait StatusReportingActor { me: AlmActor ⇒
-  final def registerStatusReporter(reporter: almhirt.herder.StatusReporter)(implicit cnp: ActorComponentIdProvider): Unit =
+  final def registerExplicitStatusReporter(reporter: almhirt.herder.StatusReporter)(implicit cnp: ActorComponentIdProvider): Unit =
     almhirtContext.tellHerder(StatusReportMessages.RegisterStatusReporter(cnp.componentId, reporter))
 
   final def registerStatusReporterPF[T: ClassTag](requestReportMsg: Any, extractReportPF: PartialFunction[T, AlmValidation[almhirt.akkax.reporting.StatusReport]])(implicit cnp: ActorComponentIdProvider, executor: ExecutionContext): Unit = {
     val reporter = almhirt.herder.StatusReporter(getReport = () ⇒ {
       (self ? requestReportMsg)(5.seconds).mapCastTo[T].mapV(extractReportPF)
     })
-    this.registerStatusReporter(reporter)(cnp)
+    this.registerExplicitStatusReporter(reporter)(cnp)
+  }
+
+  final def registerStatusReporter(timeout: FiniteDuration = 5.seconds)(implicit cnp: ActorComponentIdProvider, executor: ExecutionContext): Unit = {
+    val reporter = almhirt.herder.StatusReporter(getReport = () ⇒ {
+      (self ? ActorMessages.ReportStatus)(timeout).mapCastTo[ActorMessages.ReportStatusRsp].mapV {
+        case ActorMessages.CurrentStatusReport(report) ⇒ scalaz.Success(report)
+        case ActorMessages.ReportStatusFailed(cause)   ⇒ scalaz.Failure(cause.toProblem)
+      }
+    })
+    this.registerExplicitStatusReporter(reporter)(cnp)
   }
 
   final def deregisterStatusReporter()(implicit cnp: ActorComponentIdProvider): Unit =
     almhirtContext.tellHerder(StatusReportMessages.DeregisterStatusReporter(cnp.componentId))
+
+  implicit class StatusReportingActorRecieveOps(val self: Receive) {
+    def termininateStatusReporting(onReportRequested: () ⇒ AlmValidation[StatusReport]) =
+      self orElse ({
+        case ActorMessages.ReportStatus ⇒
+          onReportRequested().fold(
+            fail ⇒ sender() ! ActorMessages.ReportStatusFailed(fail),
+            succ ⇒ sender ! ActorMessages.CurrentStatusReport(succ))
+      }: Receive)
+
+    def termininateStatusReportingF(onReportRequested: () ⇒ AlmFuture[StatusReport])(implicit executor: ExecutionContext) =
+      self orElse ({
+        case ActorMessages.ReportStatus ⇒
+          onReportRequested().mapOrRecoverThenPipeTo(
+            map = ActorMessages.CurrentStatusReport(_),
+            recover = ActorMessages.ReportStatusFailed(_))(sender())
+      }: Receive)
+  }
+
+  def reportsStatus(onReportRequested: () ⇒ AlmValidation[StatusReport])(receive: Receive) =
+    receive.termininateStatusReporting(onReportRequested)
+
+  def reportsStatusF(onReportRequested: () ⇒ AlmFuture[StatusReport])(receive: Receive)(implicit executor: ExecutionContext) =
+    receive.termininateStatusReportingF(onReportRequested)
+
 }
