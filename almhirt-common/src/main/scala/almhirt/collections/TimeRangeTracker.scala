3e7400f59d0f3e17a6e35db46ab3de6465c39655
Stefan Röhrbein <roehrbein@dial.de>
2015-10-27 08:13:22 +0100

    added tests for TimeRangeTracker

--- almhirt-common/src/main/scala/almhirt/collections/TimeRangeTracker.scala
+++ almhirt-common/src/main/scala/almhirt/collections/TimeRangeTracker.scala
@@ -12,16 +12,16 @@ class TimeRangeTrackerImpl(numberOfBuckets: Int, bucketSpan: Duration, startTime
   case class TimeRange(begin: LocalDateTime, end: LocalDateTime)
   case class OccurencesInTimeRange(timeRange: TimeRange, var count: Long)
 
-  val buckets = (1 to numberOfBuckets) map (index ⇒ {
+  val buckets = ((1 to numberOfBuckets) map (index ⇒ {
     val beginOffset = (index - 1) * bucketSpan.toNanos
     val endOffset = index * bucketSpan.toNanos
-    val begin = startTime.plusNanos(beginOffset.toLong)
-    val end = index match {
-      case `numberOfBuckets` ⇒ startTime.plusNanos(endOffset.toLong)
-      case _                 ⇒ startTime.plusNanos(endOffset.toLong - 1L)
+    val begin = index match {
+      case 1 ⇒ startTime.plusNanos(beginOffset.toLong)
+      case _ ⇒ startTime.plusNanos(beginOffset.toLong + 1L)
     }
+    val end =  startTime.plusNanos(endOffset.toLong)
     OccurencesInTimeRange(TimeRange(begin, end), 0L)
-  })
+  })).toSeq
 
   def add(occurrence: LocalDateTime): Unit = buckets foreach (bucket ⇒ {
     if (occurrence.isAfter(bucket.timeRange.begin.minusNanos(1L)) && occurrence.isBefore(bucket.timeRange.end.plusNanos(1L)))
@@ -29,9 +29,10 @@ class TimeRangeTrackerImpl(numberOfBuckets: Int, bucketSpan: Duration, startTime
   })
 
   def coveredRange: (LocalDateTime, LocalDateTime) = (buckets.head.timeRange.begin, buckets.last.timeRange.end)
+
   def occurences(from: LocalDateTime, to: LocalDateTime): Long = buckets
-//    .filter(bucket ⇒
-//    bucket.timeRange.begin.isAfter(from.minusNanos(1L)) && bucket.timeRange.end.isBefore(to.plusNanos(1L)))
+    .filter(bucket =>
+      bucket.timeRange.begin.isAfter(from.minusNanos(1L)) && bucket.timeRange.end.isBefore(to.plusNanos(1L)))
     .map(_.count).sum
 
 }
--- almhirt-common/src/test/scala/almhirt/collections/TimeRangeTrackerTest.scala
+++ almhirt-common/src/test/scala/almhirt/collections/TimeRangeTrackerTest.scala
@@ -14,13 +14,33 @@ class TimeRangeTrackerTest extends FunSuite with Matchers {
     coveredDuration.toMinutes should equal(2)
   }
 
-  test("must have 10 occurences") {
+  test("must have 10 occurences in 60 seconds") {
     val duration = Duration.ofMinutes(1L)
     val numberOfBuckets = 2
     val startTime = LocalDateTime.now
     val tracker = new TimeRangeTrackerImpl(numberOfBuckets, duration, startTime)
     (1 to 10) foreach(index => tracker.add(startTime.plusSeconds(index)))
-    val count = tracker.occurences(startTime, startTime.plusSeconds(10))
+    val count = tracker.occurences(startTime, startTime.plusSeconds(60))
+    count should equal(10)
+  }
+  
+  test("must have 6 occurences") {
+    val duration = Duration.ofMinutes(1L)
+    val numberOfBuckets = 2
+    val startTime = LocalDateTime.now
+    val tracker = new TimeRangeTrackerImpl(numberOfBuckets, duration, startTime)
+    (1 to 10) foreach(index => tracker.add(startTime.plusSeconds(index*10)))
+    val count = tracker.occurences(startTime, startTime.plusSeconds(60))
+    count should equal(6)
+  }
+  
+  test("must have 10 occurences in 120 seconds") {
+    val duration = Duration.ofMinutes(1L)
+    val numberOfBuckets = 2
+    val startTime = LocalDateTime.now
+    val tracker = new TimeRangeTrackerImpl(numberOfBuckets, duration, startTime)
+    (1 to 10) foreach(index => tracker.add(startTime.plusSeconds(index*10)))
+    val count = tracker.occurences(startTime, startTime.plusSeconds(120))
     count should equal(10)
   }
 
