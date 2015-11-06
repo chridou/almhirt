de87c8670ff4256a1ffbb841ba474fb39b48505f
chridou <chridou@users.noreply.github.com>
2015-11-06 08:27:31 +0100

    reporting converters for agg id and version

--- almhirt-core/src/main/scala/almhirt/akkax/reporting/RValueConverter.scala
+++ almhirt-core/src/main/scala/almhirt/akkax/reporting/RValueConverter.scala
@@ -4,8 +4,17 @@ import java.time.{ LocalDateTime, ZonedDateTime }
 import almhirt.problem.ProblemCause
 import almhirt.akkax.ComponentState
 import ezreps.util.EzValueConverter
+import almhirt.aggregates._
 
 trait AlmEzValueConverters {
+  implicit val RValueAggregateRootIdInst: EzValueConverter[AggregateRootId] = new EzValueConverter[AggregateRootId] {
+    def convert(value: AggregateRootId): ezreps.ast.EzValue = ezreps.ast.EzString(value.value)
+  }
+
+  implicit val RValueAggregateRootVersionInst: EzValueConverter[AggregateRootVersion] = new EzValueConverter[AggregateRootVersion] {
+    def convert(value: AggregateRootVersion): ezreps.ast.EzValue = ezreps.ast.EzInteger(value.value)
+  }
+
   implicit val RValueConverterComponentStateInst: EzValueConverter[ComponentState] = new EzValueConverter[ComponentState] {
     def convert(value: ComponentState): ezreps.ast.EzValue = ezreps.ast.EzString(value.parsableString)
   }
@@ -17,7 +26,7 @@ trait AlmEzValueConverters {
   }
 }
 
-trait AlmEzValueOptionConverters { self: AlmEzValueConverters  ⇒
+trait AlmEzValueOptionConverters { self: AlmEzValueConverters ⇒
   private def createOptionSomeConverterWrapperInst[T: EzValueConverter]: EzValueConverter[Option[T]] = new EzValueConverter[Option[T]] {
     def convert(value: Option[T]): ezreps.ast.EzValue =
       value match {
