a5019df1e3916adf100de5d5bede80e2926e0e0c
Stefan Röhrbein <roehrbein@dial.de>
2016-04-15 13:24:30 +0200

    updated dependencies

--- project/Build.scala
+++ project/Build.scala
@@ -10,7 +10,7 @@ object BuildSettings {
   val buildOrganization = "org.almhirt"
   val buildScalaVersion = "2.11.7"
 
-  val akkaVersion = "2.4.3"
+  val akkaVersion = "2.4.4"
   val scalatestVersion = "2.2.6"
   val sprayVersion = "1.3.3"
   val scalazVersion = "7.2.1"
@@ -62,7 +62,7 @@ object Dependencies {
 
 	lazy val scalaz       = "org.scalaz" %% "scalaz-core" % BuildSettings.scalazVersion
 
-	lazy val play2_iteratees   = "com.typesafe.play" %% "play-iteratees" % "2.5.1"
+	lazy val play2_iteratees   = "com.typesafe.play" %% "play-iteratees" % "2.5.2"
 
   lazy val json4s   = "org.json4s" %% "json4s-native" % json4sVersion % "compile"
 
